ff48ca60d5266aecf758794b4a09d5d8cc58535e
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-09-17 23:50:55 +0100

    Update what-is-emoney.php

--- EvolvingMoney/cryptographicMoney/what-is-emoney.php
+++ EvolvingMoney/cryptographicMoney/what-is-emoney.php
@@ -1,9 +1,5 @@
-
-               
 <!DOCTYPE html>
-
 <html>
-
 <head>		
 		<title>E-Money</title>
 	 	<link type="text/css" rel="stylesheet" href = "../css/learningPage.css"/>
@@ -19,35 +15,11 @@
 </head>
 
 <body>
-
-	<div id="page">			 
-
-		<div class="imgbox" id="logo">
-    		<img class="center-fit" src="../Images/EM_logo.GIF" alt="Evolving Money" />	
-		</div>
-
-	<ul id="navigation">	 
-
-		<li><a href="../index.html" class="on">Home</a></li>
-  		<li><a href="../about-us.html">About</a></li>
-  		<li><a href="../InvIdeas/index.html">Contrarian</a></li>
-	  	<li><a href="../MacroIdeas/index.html">Macro</a></li>
-		<li><a href="../cryptographicMoney/what-is-crypto.html">E-Money</a></li>
-	 	<li><a href="../PerToPer/index.html">P2P</a></li>	 
-	  	<li><a href="../Further_Reading/index.html">G&T</a></li>	
-  		<li><a href="../contact.html">Contact</a></li>	 
-  	</ul>
-
-	<ul id="navigation">	 
-  		<li><a href="what-is-crypto.html" class="on">E-Money></a></li>
-  		<li><a href="crypto.html">Crypto</a></li>
-  		<li><a href="banking.html">Banking</a></li>
-     	</ul>
-  
+	<?php include("../includes/header.html");?>
+	<br>
 	<div class="imgbox">
     		<img class="center-fit" src="../Images/EMoney_logo.GIF">
 	</div>
-	
 
 		<p> One of the most exciting and least understood areas in finance is online purchasing and how it's still changing. Commerce has moved online, and those that have made 
 			e-commerce "frictionless" (<strong>Paypal, Amazon, Alibaba</strong>) have slaughtered the competition. It hasn't stopped with <strong>Amazon</strong> and <strong>Paypal</strong>, innovations continue. Payments systems like <strong>Transferwise</strong> 
@@ -62,7 +34,9 @@ over the next 5-10 years. Many of these new entrants to global e-commerce are cu
 		<p> It's a strange thought, but in manufacturing it's a story that's played out before, riches to rust belt for some, and new wealth to others. In a similar way, 
 finance that doesn't sufficiently innovate, could suffer a similar fate.   </p>
 
-	</div>  	
+	</div>
+	
+  	<?php include("../includes/footer.html");?>  
 </body> 
 	
 </html> 
