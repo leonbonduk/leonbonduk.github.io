834e8f4343850e2e24c8a134f6ca25b7719f6783
leonbonduk <leonbonduk@gmail.com>
2019-09-17 19:06:39 -0400

    rename php

---
+++ EvolvingMoney/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+   	 <head>		
+       <title>Evolving Money</title>
+
+       <link href="css/learningPage.css" type="text/css" rel="stylesheet" />
+
+       <meta name="description"
+             content="Guide to new types of investments and ways of managing money, along with some macro and contrarian investment opportunites" />
+       <meta name="keywords"
+             content="investments, new, macro, crypto, contrarian, p2p, banking" />
+       <meta name="robots"
+             content="follow" />
+       <meta http-equiv="author"
+             content="Leon Bond" />
+       	 
+             
+  </head>
+<body>
+	<?php include("../includes/header.html");?>
+    <br>
+    <div class="imgbox">
+    <img class="center-fit" src="Images/EM_circ_logo.jpg">
+    </div>
+  	<?php include("../includes/footer.html");?>  
+ </body> 
+</html> 
