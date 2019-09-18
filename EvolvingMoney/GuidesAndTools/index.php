68d47851047ccd3c3d4286686c8e6a4020a2da38
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-09-18 02:26:56 +0100

    Update index.php

--- EvolvingMoney/GuidesAndTools/index.php
+++ EvolvingMoney/GuidesAndTools/index.php
@@ -3,7 +3,7 @@
    	 <head>		
        <title>Guides and Tools</title>
 
-       <link href="css/learningPage.css" type="text/css" rel="stylesheet" />
+       <link href="..css/learningPage.css" type="text/css" rel="stylesheet" />
 
        <meta name="description"
              content="Guide to new types of investments and ways of managing money, along with some macro and contrarian investment opportunites" />
@@ -17,15 +17,21 @@
              
   </head>
 <body>
-	<?php include("includes/header.html");?>
-
+	<?php include("..includes/header.html");?>
+	<ul id="navigation">	 
+		<li><a href="index.html" class="on">G&T></a></li>
+  		<li><a href="glossary.html">Glossary</a></li>
+  		<li><a href="books.html">Books</a></li>
+  		<li><a href="media.html">Media</a></li>
+		<li><a href="websites.html">Websites</a></li>
+ 	</ul>
 <div class="imgbox">
     <img class="center-fit" src="../Images/G&T_logo.GIF">
 </div>
 <p> Sorry no alcohol here! Bit hopefully you find this Guides and Tools section useful. A mix of books and websites we have found useful in learning the world of investment oppportunities. We have even collated a glossary of terms to help decipher all the confusing terminology you may come across!
 </p>
 </div>
-  	<?php include("includes/footer.html");?>  
+  	<?php include("..includes/footer.html");?>  
   </body> 
 </html> 
 
