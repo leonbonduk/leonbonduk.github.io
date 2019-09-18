5dc1b0166d8fcdf4d8d53c1559554a92d52d68d8
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-09-18 01:34:23 +0100

    Update contact.php

--- EvolvingMoney/contact.php
+++ EvolvingMoney/contact.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>		
-		<title>E-Money</title>
+		<title>Contact Us</title>
 	 	<link type="text/css" rel="stylesheet" href = "css/learningPage.css"/>
 
        		<meta name="description"
@@ -16,26 +16,16 @@
 
 <body>
 	<?php include("includes/header.html");?>
-	<br>
-	<div class="imgbox">
-    		<img class="center-fit" src="Images/EMoney_logo.GIF">
-	</div>
-
-		<p> One of the most exciting and least understood areas in finance is online purchasing and how it's still changing. Commerce has moved online, and those that have made 
-			e-commerce "frictionless" (<strong>Paypal, Amazon, Alibaba</strong>) have slaughtered the competition. It hasn't stopped with <strong>Amazon</strong> and <strong>Paypal</strong>, innovations continue. Payments systems like <strong>Transferwise</strong> 
-			compete with <strong>Western Union</strong>;  even <strong>Uber</strong> and <strong>AirBnB</strong> can in some sense be considered as frictionless payments systems. </p>		 
-		<p> The nature of the e-commerce market is changing, and the money we use may very well change alongside it.</p>
-		<br>
-		<p> One of the biggest changes on the horizon, is that currently the are roughly 3.2 billion people who are internet connected, but that number is predicted to double 
-over the next 5-10 years. Many of these new entrants to global e-commerce are currently unbanked/underbanked and based in the developing world. They may choose to get bank accounts and credit cards, and setup paypal; but if it's easier and cheaper to do something else, then up to half of those on the internet could be doing something new in the future. </p>
-		<p> If that something new is easier and less hassle than what we have now, we may all end up using it in the future.<p>
-		<br>
-		<p> The other side of this story, is that we know where would be a good place to look for where new innovation is taking hold, emerging markets.</p>
-		<p> It's a strange thought, but in manufacturing it's a story that's played out before, riches to rust belt for some, and new wealth to others. In a similar way, 
-finance that doesn't sufficiently innovate, could suffer a similar fate.   </p>
-
-	</div>
-	
+<br>	
+<h2> <img src="Images/ContactUs_logo.GIF" alt="Contact Us" /> </h2>
+<br>	
+<p>If you would like to get in touch with us then please drop either of us an email: </p>
+<br>
+<a href="mailto:leon@evolving.money">Email: Leon</a>
+<br>
+<br>
+<a href="mailto:james_graham_day@yahoo.co.uk">Email: James</a>
+<br>
   	<?php include("includes/footer.html");?>  
 </body> 
 	
