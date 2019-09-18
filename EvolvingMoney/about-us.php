b7185e9f7f7410bc92533379be86d1fb5fdd6f63
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-09-18 01:36:30 +0100

    Update about-us.php

--- EvolvingMoney/about-us.php
+++ EvolvingMoney/about-us.php
@@ -1,24 +1,22 @@
-<<html>
-  
-  	 <head>		
-       <title>About Us</title>	
+<!DOCTYPE html>
+<html>
+<head>		
+		<title>E-Money</title>
+	 	<link type="text/css" rel="stylesheet" href = "../css/learningPage.css"/>
 
-       <link href="css/learningPage.css" type="text/css" rel="stylesheet" />
+       		<meta name="description"
+             		content="Guide to new types of investments and ways of managing money, along with some macro and contrarian investment opportunites" />
+       		<meta name="keywords"
+             		content="investments, new, macro, crypto, contrarian, p2p, banking" />
+       		<meta name="robots"
+             		content="follow" />
+       		<meta http-equiv="author"
+             		content="James Day" />
+</head>
 
-       <meta name="description"
-             content="Guide to new types of investments and ways of managing money, along with some macro and contrarian investment opportunites" />
-       <meta name="keywords"
-             content="investments, new, macro, crypto, contrarian, p2p, banking" />
-       <meta name="robots"
-             content="follow" />
-       <meta http-equiv="author"
-             content="Leon Bond" />
-       	 
-             
-  </head>
 <body>
-	<?php include("includes/header.html");?>
-
+	<?php include("../includes/header.html");?>
+	<br>
 <h1>			 <img src="Images/AboutUs_logo.GIF"					 alt="Evolving Money" />		 </h1>
 <br>
 <p> The world of most interesting investments we can find, an ecllectic mix of contrarian or macro economic opportunties mixed in with some reasoned cryptocurrency plays and other new opprtunites coming out of the fintech space. We may throw in some low risk ways to manage or grow your wealth that we come across and think worth sharing. <p>		 
@@ -27,6 +25,8 @@
 
 <p> We are here to be your guides to the world of new types of investments, payments technologies, and even changes in money itself. More than that we are here to break it down by region, different parts of the world are doing different things, innovation tends to happen at the edges and move to the centre. We aim to be at the frontier giving you the story as it unfolds.
 <br>
-  	<?php include("includes/footer.html");?>  
-  </body> 
+	
+  	<?php include("../includes/footer.html");?>  
+</body> 
+	
 </html> 
