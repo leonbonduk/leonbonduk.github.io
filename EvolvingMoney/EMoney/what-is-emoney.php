054a5e46692bd4d2907b16944f400208e8700347
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-09-19 09:23:28 +0100

    Update what-is-emoney.php

--- EvolvingMoney/EMoney/what-is-emoney.php
+++ EvolvingMoney/EMoney/what-is-emoney.php
@@ -15,16 +15,19 @@
 </head>
 
 <body>
-	<?php include("../includes/header.html");?>
-	<ul id="navigation">	 
-  		<li><a href="what-is-emoney.php" class="on">E-Money></a></li>
-  		<li><a href="crypto.php">Crypto</a></li>
-  		<li><a href="EBanking/challenger-comp.php">Banking</a></li>
-     	</ul>
-	<br>
-	<div class="imgbox">
-    		<img class="center-fit" src="../Images/EMoney_logo.GIF">
-	</div>
+
+    <div id="page-container">
+        <div id="content-wrap">
+		<?php include("../includes/header.html");?>
+		<ul id="navigation">	 
+  			<li><a href="what-is-emoney.php" class="on">E-Money></a></li>
+  			<li><a href="crypto.php">Crypto</a></li>
+  			<li><a href="EBanking/challenger-comp.php">Banking</a></li>
+     		</ul>
+		<br>
+		<div class="imgbox">
+    			<img class="center-fit" src="../Images/EMoney_logo.GIF">
+		</div>
 
 		<p> One of the most exciting and least understood areas in finance is online purchasing and how it's still changing. Commerce has moved online, and those that have made 
 			e-commerce "frictionless" (<strong>Paypal, Amazon, Alibaba</strong>) have slaughtered the competition. It hasn't stopped with <strong>Amazon</strong> and <strong>Paypal</strong>, innovations continue. Payments systems like <strong>Transferwise</strong> 
@@ -32,18 +35,18 @@
 		<p> The nature of the e-commerce market is changing, and the money we use may very well change alongside it.</p>
 		<br>
 		<p> One of the biggest changes on the horizon, is that currently the are roughly 3.2 billion people who are internet connected, but that number is predicted to double 
-over the next 5-10 years. Many of these new entrants to global e-commerce are currently unbanked/underbanked and based in the developing world. They may choose to get bank accounts and credit cards, and setup paypal; but if it's easier and cheaper to do something else, then up to half of those on the internet could be doing something new in the future. </p>
+		over the next 5-10 years. Many of these new entrants to global e-commerce are currently unbanked/underbanked and based in the developing world. They may choose to get bank accounts and credit cards, and setup paypal; but if it's easier and cheaper to do something else, then up to half of those on the internet could be doing something new in the future. </p>
 		<p> If that something new is easier and less hassle than what we have now, we may all end up using it in the future.<p>
 		<br>
 		<p> The other side of this story, is that we know where would be a good place to look for where new innovation is taking hold, emerging markets.</p>
 		<p> It's a strange thought, but in manufacturing it's a story that's played out before, riches to rust belt for some, and new wealth to others. In a similar way, 
-finance that doesn't sufficiently innovate, could suffer a similar fate.   </p>
+		finance that doesn't sufficiently innovate, could suffer a similar fate.   </p>
 
 	</div>
-
-        <footer id="footer">	
-  		<?php include("../includes/footer.html");?>  
- 	</footer>
+      		 <footer id="footer">	
+  			<?php include("../includes/footer.html");?>  
+ 		</footer>
+    </div>
 </body> 
 	
 </html> 
