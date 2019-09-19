891ea42482ba15ae819291d7cbf7acd0be8d3a61
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-09-19 08:40:21 +0100

    Update challenger-comp.php

--- EvolvingMoney/EMoney/EBanking/challenger-comp.php
+++ EvolvingMoney/EMoney/EBanking/challenger-comp.php
@@ -1,38 +1,258 @@
 <!DOCTYPE html>
-<html>
+<html lang="en" class="no-js">
 <head>		
+		<meta charset="UTF-8">
 		<title>Comapre the Challenger Banks</title>
-	 	<link type="text/css" rel="stylesheet" href = "../../css/learningPage.css"/>
+		<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="../../css/reset.css"> <!-- CSS reset -->
+		<link rel="stylesheet" href="../../css/style.css"> <!-- Resource style -->
+		<script src="../../js/modernizr.js"></script> <!-- Modernizr -->
 
        		<meta name="description"
              		content="Guide to comparing the new challenger banks." />
        		<meta name="keywords"
-             		content="icompare, challenger banks, banking, fintech" />
+             		content="compare, challenger banks, banking, fintech" />
        		<meta name="robots"
              		content="follow" />
        		<meta http-equiv="author"
-             		content="James Day" />
+             		content="Leon Bond" />
 </head>
 
 <body>
 	<?php include("../../includes/header.html");?>
 	<ul id="navigation">	 
-  		<li><a href="what-is-crypto.php" class="on">E-Money></a></li>
+  		<li><a href="what-is-emoney.php" class="on">E-Money></a></li>
   		<li><a href="crypto.php">Crypto</a></li>
-  		<li><a href="banking.php">Banking</a></li>
+  		<li><a href="EBanking/challenger-comp.php">Banking</a></li>
      	</ul>
 	<br>
 	<div class="imgbox">
     		<img class="center-fit" src="../Images/Banking_logo.GIF">
-	</div>
+	<section class="cd-intro">
+		<h1>Products Comparison Table</h1>
+	</section> <!-- .cd-intro -->
+	<section class="cd-products-comparison-table">
+		<header>
+			<h2>Compare Models</h2>
 
-		<p>Comparison tool for all the newly formed challenger banks</p>		 
+			<div class="actions">
+				<a href="#0" class="reset">Reset</a>
+				<a href="#0" class="filter">Filter</a>
+			</div>
+		</header>
 
-	</div>
-	
-        <footer id="footer">
-  	      <?php include("../../includes/footer.html");?>  
-        </footer>
-</body> 
-	
+		<div class="cd-products-table">
+			<div class="features">
+				<div class="top-info">Models</div>
+
+				<ul class="cd-features-list">
+					<li>Price</li>
+					<li>Customer Rating</li>
+					<li>Resolution</li>
+					<li>Screen Type</li>
+					<li>Display Size</li>
+					<li>Refresh Rate</li>
+					<li>Model Year</li>
+					<li>Tuner Technology</li>
+					<li>Ethernet Input</li>
+					<li>USB Input</li>
+					<li>Scart Input</li>
+				</ul>
+			</div> <!-- .features -->
+			
+
+			<div class="cd-products-wrapper">
+				<ul class="cd-products-columns">
+					<li class="product">
+						<div class="top-info">
+							<div class="check"></div>
+							<img src="../../Images/product.png" alt="product image">
+							<h3>Sumsung Series 6 J6300</h3>
+						</div> <!-- .top-info -->
+
+						<ul class="cd-features-list">
+							<li>$600</li>
+							<li class="rate"><span>5/5</span></li>
+							<li>1080p</li>
+							<li>LED</li>
+							<li>47.6 inches</li>
+							<li>800Hz</li>
+							<li>2015</li>
+							<li>mpeg4</li>
+							<li>1 Side</li>
+							<li>3 Port</li>
+							<li>1 Rear</li>
+						</ul>
+					</li> <!-- .product -->
+
+					<li class="product">
+						<div class="top-info">
+							<div class="check"></div>
+							<img src="../../Images/product.png" alt="product image">
+							<h3>Sumsung Series 6 J6300</h3>
+						</div> <!-- .top-info -->
+
+						<ul class="cd-features-list">
+							<li>$600</li>
+							<li class="rate"><span>5/5</span></li>
+							<li>1080p</li>
+							<li>LED</li>
+							<li>47.6 inches</li>
+							<li>800Hz</li>
+							<li>2015</li>
+							<li>mpeg4</li>
+							<li>1 Side</li>
+							<li>3 Port</li>
+							<li>1 Rear</li>
+						</ul>
+					</li> <!-- .product -->
+
+					<li class="product">
+						<div class="top-info">
+							<div class="check"></div>
+							<img src="../../Images/product.png" alt="product image">
+							<h3>Sumsung Series 6 J6300</h3>
+						</div> <!-- .top-info -->
+
+						<ul class="cd-features-list">
+							<li>$600</li>
+							<li class="rate"><span>5/5</span></li>
+							<li>1080p</li>
+							<li>LED</li>
+							<li>47.6 inches</li>
+							<li>800Hz</li>
+							<li>2015</li>
+							<li>mpeg4</li>
+							<li>1 Side</li>
+							<li>3 Port</li>
+							<li>1 Rear</li>
+						</ul>
+					</li> <!-- .product -->
+
+					<li class="product">
+						<div class="top-info">
+							<div class="check"></div>
+							<img src="../../Images/product.png" alt="product image">
+							<h3>Sumsung Series 6 J6300</h3>
+						</div> <!-- .top-info -->
+
+					<ul class="cd-features-list">
+							<li>$600</li>
+							<li class="rate"><span>5/5</span></li>
+							<li>1080p</li>
+							<li>LED</li>
+							<li>47.6 inches</li>
+							<li>800Hz</li>
+							<li>2015</li>
+							<li>mpeg4</li>
+							<li>1 Side</li>
+							<li>3 Port</li>
+							<li>1 Rear</li>
+						</ul>
+					</li> <!-- .product -->
+
+				<li class="product">
+						<div class="top-info">
+							<div class="check"></div>
+							<img src="../../Images/product.png" alt="product image">
+							<h3>Sumsung Series 6 J6300</h3>
+						</div> <!-- .top-info -->
+
+						<ul class="cd-features-list">
+							<li>$600</li>
+							<li class="rate"><span>5/5</span></li>
+							<li>1080p</li>
+							<li>LED</li>
+							<li>47.6 inches</li>
+							<li>800Hz</li>
+							<li>2015</li>
+							<li>mpeg4</li>
+							<li>1 Side</li>
+							<li>3 Port</li>
+							<li>1 Rear</li>
+						</ul>
+					</li> <!-- .product -->
+
+					<li class="product">
+						<div class="top-info">
+							<div class="check"></div>
+							<img src="../../Images/product.png" alt="product image">
+							<h3>Sumsung Series 6 J6300</h3>
+						</div> <!-- .top-info -->
+
+						<ul class="cd-features-list">
+							<li>$600</li>
+							<li class="rate"><span>5/5</span></li>
+							<li>1080p</li>
+							<li>LED</li>
+							<li>47.6 inches</li>
+							<li>800Hz</li>
+							<li>2015</li>
+							<li>mpeg4</li>
+							<li>1 Side</li>
+							<li>3 Port</li>
+							<li>1 Rear</li>
+						</ul>
+					</li> <!-- .product -->
+
+				<li class="product">
+						<div class="top-info">
+							<div class="check"></div>
+							<img src="../../Images/product.png" alt="product image">
+							<h3>Sumsung Series 6 J6300</h3>
+						</div> <!-- .top-info -->
+
+						<ul class="cd-features-list">
+							<li>$600</li>
+							<li class="rate"><span>5/5</span></li>
+							<li>1080p</li>
+							<li>LED</li>
+							<li>47.6 inches</li>
+							<li>800Hz</li>
+							<li>2015</li>
+							<li>mpeg4</li>
+							<li>1 Side</li>
+							<li>3 Port</li>
+							<li>1 Rear</li>
+						</ul>
+					</li> <!-- .product -->
+
+					<li class="product">
+						<div class="top-info">
+							<div class="check"></div>
+							<img src="../../Images/product.png" alt="product image">
+							<h3>Sumsung Series 6 J6300</h3>
+						</div> <!-- .top-info -->
+
+						<ul class="cd-features-list">
+							<li>$600</li>
+							<li class="rate"><span>5/5</span></li>
+							<li>1080p</li>
+							<li>LED</li>
+							<li>47.6 inches</li>
+							<li>800Hz</li>
+							<li>2015</li>
+							<li>mpeg4</li>
+							<li>1 Side</li>
+							<li>3 Port</li>
+							<li>1 Rear</li>
+						</ul>
+					</li> <!-- .product -->
+				</ul> <!-- .cd-products-columns -->
+			</div> <!-- .cd-products-wrapper -->
+			
+			<ul class="cd-table-navigation">
+				<li><a href="#0" class="prev inactive">Prev</a></li>
+				<li><a href="#0" class="next">Next</a></li>
+			</ul>
+		</div> <!-- .cd-products-table -->
+	</section> <!-- .cd-products-comparison-table -->
+<script src="../../js/jquery-2.1.4.js"></script>
+<script src="../../js/main.js"></script> <!-- Resource jQuery -->
+
+       <footer id="footer">
+ 	      <?php include("../../includes/footer.html");?>  
+       </footer>
+/body> 
+
 </html> 
