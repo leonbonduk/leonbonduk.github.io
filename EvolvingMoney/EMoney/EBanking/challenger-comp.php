91248905354eab5dd0677978ce8c15c78ac66f0d
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-09-19 08:09:51 +0100

    Create challenger-comp.php

---
+++ EvolvingMoney/EMoney/EBanking/challenger-comp.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>		
+		<title>Comapre the Challenger Banks</title>
+	 	<link type="text/css" rel="stylesheet" href = "../../css/learningPage.css"/>
+
+       		<meta name="description"
+             		content="Guide to comparing the new challenger banks." />
+       		<meta name="keywords"
+             		content="icompare, challenger banks, banking, fintech" />
+       		<meta name="robots"
+             		content="follow" />
+       		<meta http-equiv="author"
+             		content="James Day" />
+</head>
+
+<body>
+	<?php include("../../includes/header.html");?>
+	<ul id="navigation">	 
+  		<li><a href="what-is-crypto.php" class="on">E-Money></a></li>
+  		<li><a href="crypto.php">Crypto</a></li>
+  		<li><a href="banking.php">Banking</a></li>
+     	</ul>
+	<br>
+	<div class="imgbox">
+    		<img class="center-fit" src="../Images/Banking_logo.GIF">
+	</div>
+
+		<p>Comparison tool for all the newly formed challenger banks</p>		 
+
+	</div>
+	
+        <footer id="footer">
+  	      <?php include("../../includes/footer.html");?>  
+        </footer>
+</body> 
+	
+</html> 
