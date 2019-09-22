e12b811d089c50688c366ae34ff2e41ef45ca77f
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-09-22 01:22:31 +0100

    Update challenger-comp.php

--- EvolvingMoney/EMoney/EBanking/challenger-comp.php
+++ EvolvingMoney/EMoney/EBanking/challenger-comp.php
@@ -80,7 +80,7 @@
 							<li>London, 2015</li>
 							<li>7,000,000+</li>
 							<li>£0.00</li>
-							<li>Yes</li>
+							<li><tick>Yes<tick></li>
 							<li>Yes</li>
 							<li>Yes</li>
 							<li>Up to £200pm</li>
