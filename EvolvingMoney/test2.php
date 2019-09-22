54aa9f974899deb4293e6187e146af4982651aa0
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-09-22 23:54:16 +0100

    Update test2.php

--- EvolvingMoney/test2.php
+++ EvolvingMoney/test2.php
@@ -276,8 +276,8 @@
 
 
 <script src="https://www.evolving.money/navbar-nodejs/main.js"></script>
-<script>var _gaq = _gaq || []; _gaq.push(['_setAccount', 'UA-7572727-1']); _gaq.push(['_trackPageview']); (function() { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })();</script>
-
+<!--<script>var _gaq = _gaq || []; _gaq.push(['_setAccount', 'UA-7572727-1']); _gaq.push(['_trackPageview']); (function() { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })();</script>
+!-->
 
 
 
