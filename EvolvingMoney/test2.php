dcc3d3592f2ffedaac3639797712631de9327f1d
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-09-26 23:04:02 +0100

    Update test2.php

--- EvolvingMoney/test2.php
+++ EvolvingMoney/test2.php
@@ -16,14 +16,66 @@
 	<meta name="viewport" content="width=device-width,initial-scale=1" />
 	<link rel="canonical" href="https://www.evolving.money/">
 	<link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" />
-
+	<link rel="stylesheet" type="text/css" href="960-12-col.css" />
+	
+	<style>
+		/* Column margins */
+		.col_12, /* full width */
+		.col_9, /* 3/4 width */
+		.col_8, /* 2/3 width */
+		.col_6, /* half width */
+		.col_4, /* 1/3 width */
+		.col_3 /* 1/4 width */
+		{
+			height: 50px;
+			margin-top:10px;
+			margin-bottom:10px;
+			text-align:center;
+			display:table-cell;
+			vertical-align:central;
+		}
+	</style>
 </head>
+	
 
 <body>
 
 
 <?php include("includes/new-header.html");?>
 
+
+<div class="row">
+	<div class="col_9 first" style="background-color:#3F6"><p>Here’s a 3/4 width column</p></div>
+	<div class="col_3 last" style="background-color:#6CC"><p>And it’s 1/4 width counterpart</p></div>
+</div>
+	
+<div class="row">
+	<div class="col_8 first" style="background-color:#CF0"><p>Here’s a 2/3 width column</p></div>
+	<div class="col_4 last" style="background-color:#9CC"><p>And it’s 1/3 width counterpart.</p></div>
+</div>
+	
+<div class="row">
+	<div class="col_4 first" style="background-color:#9CC"><p>I’m a 1/3 width first</p></div>
+	<div class="col_8 last" style="background-color:#CF0"><p>And I’m a 2/3 width last</p></div>
+</div>
+	
+<div class="row">
+	<div class="col_6 first" style="background-color:#9CC"><p>Want to go halfsies?</p></div>
+	<div class="col_6 last" style="background-color:#CF0"><p>Sure, I’ll go halfsies!</p></div>
+</div>
+	
+<div class="row">
+	<div class="col_3 first" style="background-color:#CF0"><p>Here’s a 1/3 width column</p></div>
+	<div class="col_3" style="background-color:#9CC"><p>I’m a 1/3 width column, too!</p></div>
+	<div class="col_3 last" style="background-color:#9CC"><p>I’m the last 1/3 width :(</p></div>
+</div>
+	
+<div class="row">
+	<div class="col_4 first" style="background-color:#CF0"><p>Can we do 1/4 column</p></div>
+	<div class="col_4" style="background-color:#9CC"><p>Sure we can do 1/4!</p></div>
+	<div class="col_4" style="background-color:#9CC"><p>I echo that sentiment!</p></div>
+	<div class="col_4 last" style="background-color:#9CC"><p>Why am I always last? :(</p></div>
+</div>
 
  <?php include("includes/footer.html");?>
 
