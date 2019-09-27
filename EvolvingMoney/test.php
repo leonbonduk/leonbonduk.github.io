d0683a55769460fe2bebf141db623c7c6001af5a
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-09-27 22:57:37 +0100

    Update test.php

--- EvolvingMoney/test.php
+++ EvolvingMoney/test.php
@@ -1,27 +1,57 @@
 <!DOCTYPE html>
-<html>
-   	 <head>		
+
+<html lang="en">
+
+<head>
+	<meta charset="utf-8" />
        <title>Evolving Money</title>
-
    <!--    <link href="css/learningPage.css" type="text/css" rel="stylesheet" /> !-->
-
-       <meta name="description"
-             content="Guide to new types of investments and ways of managing money, along with some macro and contrarian investment opportunites" />
-       <meta name="keywords"
-             content="investments, new, macro, crypto, contrarian, p2p, banking" />
-       <meta name="robots"
-             content="follow" />
-       <meta http-equiv="author"
-             content="Leon Bond" />
-       	 
-             
-  </head>
+       <meta name="description" content="Guide to new types of investments and ways of managing money, along with some macro and contrarian investment opportunites" />
+       <meta name="keywords" content="investments, new, macro, crypto, contrarian, p2p, banking" />
+       <meta name="robots" content="follow" />
+       <meta http-equiv="author"content="Leon Bond" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="description" content="Demo of Drop-Down Navigation: Touch-Friendly and Responsive" />
+	<meta name="robots" content="all">
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<link rel="canonical" href="https://www.evolving.money/">
+	<link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" />
+	<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/960-12-col.css" />
+	
+	<style>
+		/* Column margins */
+		 .col_full,
+      .col_12, /* full width */
+		.col_9, /* 3/4 width */
+		.col_8, /* 2/3 width */
+		.col_6, /* half width */
+		.col_4, /* 1/3 width */
+		.col_3 /* 1/4 width */
+		{
+			height: auto;
+			margin-top:10px;
+			margin-bottom:10px;
+			text-align:center;
+			display:table-cell;
+			vertical-align:central;
+		}
+	</style>
+</head>
 <body>
+	
 	<?php include("includes/test-header.html");?>
-    <br>
-    <div class="imgbox">
-    <img class="center-fit" src="Images/EM_circ_logo.jpg">
-    </div>
-  	<?php include("includes/footer.html");?>  
- </body> 
-</html> 
+	
+ <div class="row">
+	<div class="col_8 first" style="background-color:#CF0"><p>Compare the Challenger banks</p></div>
+	<div class="col_4 last" style="background-color:#9CC"><div class="imgbox"><img class="center-fit" src="Images/EM_circ_logo.jpg"></div></div>
+</div>
+			
+<div class="row">
+	<div class="col_full”>
+<?php include("includes/footer.html");?>
+</div>
+</div>
+
+</footer>
+
+</html>
