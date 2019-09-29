581e805e241f0636af9f27734870af1e9a9e5923
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-09-29 10:21:17 +0100

    Update about-us.php

--- EvolvingMoney/about-us.php
+++ EvolvingMoney/about-us.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>		
-		<title>E-Money</title>
+		<title>Evolving Money - About Us</title>
 	 	<link type="text/css" rel="stylesheet" href = "css/learningPage.css"/>
 
        		<meta name="description"
@@ -9,31 +9,73 @@
        		<meta name="keywords"
              		content="investments, new, macro, crypto, contrarian, p2p, banking" />
        		<meta name="robots"
-             		content="follow" />
-       		<meta http-equiv="author"
-             		content="James Day" />
+           <meta name="robots" content="follow" />
+       <meta http-equiv="author"content="Leon Bond" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="description" content="Demo of Drop-Down Navigation: Touch-Friendly and Responsive" />
+	<meta name="robots" content="all">
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<link rel="canonical" href="https://www.evolving.money/">
+	<link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" />
+	<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/960-12-col.css" />
+	
+	<style>
+		/* Column margins */
+		 .col_full,
+      .col_12, /* full width */
+		.col_9, /* 3/4 width */
+		.col_8, /* 2/3 width */
+		.col_6, /* half width */
+		.col_4, /* 1/3 width */
+		.col_3 /* 1/4 width */
+		{
+			height: auto;
+			margin-top:10px;
+			margin-bottom:10px;
+			text-align:center;
+			display:table-cell;
+			vertical-align:central;
+		}
+	</style>
 </head>
-
 <body>
-    <div id="page-container">
-        <div id="content-wrap">
-            <?php include("includes/header.html");?>
+	
+	<?php include("includes/test-header.html");?>
+
 	        <br>
-	        <div class="imgbox">
+<div class="row">
+		<div class="col_12 first" style="background-color:#CF0">
+			<div class="imgbox">
 			    <img src="Images/AboutUs_logo.GIF"					 alt="Evolving Money" />
-			</div>
-        <br>
+		</div>
+</div>
+
+<div class="clear"></div>
+
+<div class="row">
+		<div class="col_9 first" style="background-color:#fff">
+
+<br>
         <p> The world of most interesting investments we can find, an ecllectic mix of contrarian or macro economic opportunties mixed in with some reasoned cryptocurrency plays and other new opprtunites coming out of the fintech space. We may throw in some low risk ways to manage or grow your wealth that we come across and think worth sharing. <p>		 
 
         <p> It is truely an exciting time in the world of money finance. There is a sense that change is on the horizon, and that the future is uncertain. Well the future is what we make it, and crisis is opportunity. What is certainly true, is that the world is changing fast, and the world is a very different place today, compared to when much of our current financial system was architected. The ways we do business are changing fast, who we do business is changing faster still. Our methods and systems for payments, investments, and rights of ownership will change too... in deed the very nature of money may change alongside. 
 
         <p> We are here to be your guides to the world of new types of investments, payments technologies, and even changes in money itself. More than that we are here to break it down by region, different parts of the world are doing different things, innovation tends to happen at the edges and move to the centre. We aim to be at the frontier giving you the story as it unfolds.
-        <br>
-	    </div>
-        <footer id="footer">
-  	        <?php include("includes/footer.html");?>  
-  	    </footer>
-  	</div>
+<br>
+
+	</div>
+
+<div class="col_3 last" style="background-color:#9CC"><div class="imgbox"><img class="center-fit" src="Images/EM_circ_logo.jpg"></div>
+	</div>
+
+<div class="clear"></div>
+</div>
+
+	  
+       
+<?php include("includes/footer.html");?>  
+  
+ 
 </body> 
 	
 </html> 
