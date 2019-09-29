d4c0e38fa7a8ea7f3c7e59484565d50e6d0dcf6b
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-09-30 00:04:32 +0100

    Update index.php

--- EvolvingMoney/index.php
+++ EvolvingMoney/index.php
@@ -12,7 +12,7 @@
 	<meta name="robots" content="all" />
 	<meta name="viewport" content="width=device-width,initial-scale=1" />
 	<link rel="canonical" href="https://www.evolving.money/">
-	<!-- <link href="css/learningPage.css" type="text/css" rel="stylesheet" /> !-->
+	<link href="css/styles.css" type="text/css" rel="stylesheet" />
 	<link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" />
 	<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/960-12-col.css" />
 	
