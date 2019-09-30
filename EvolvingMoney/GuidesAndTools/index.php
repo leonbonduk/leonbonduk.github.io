b491f74a8e21ccc516555db44ec015b9f87a72ae
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-10-01 00:47:11 +0100

    Update index.php

--- EvolvingMoney/GuidesAndTools/index.php
+++ EvolvingMoney/GuidesAndTools/index.php
@@ -1,37 +1,53 @@
 <!DOCTYPE html>
-<html>
-   	 <head>		
-       <title>Guides and Tools</title>
-
-       <link href="../css/learningPage.css" type="text/css" rel="stylesheet" />
-
-       <meta name="description"
-             content="Guide to new types of investments and ways of managing money, along with some macro and contrarian investment opportunites" />
-       <meta name="keywords"
-             content="investments, new, macro, crypto, contrarian, p2p, banking" />
-       <meta name="robots"
-             content="follow" />
-       <meta http-equiv="author"
-             content="Leon Bond" />
-       	 
+<html lang="en">
+<head>
+	<meta charset="utf-8" /
+       	<title>Evolving Money - Guides and Tools</title>
+       	<meta name="description" content="Guide to new types of investments and ways of managing money, along with some macro and contrarian investment opportunites" />
+       	<meta name="keywords" content="investments, new, macro, crypto, contrarian, p2p, banking" />
+       	<meta http-equiv="author" content="Leon Bond" />
+       	<meta name="robots" content="all">
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<link href="css/styles.css" type="text/css" rel="stylesheet" />
+	<link rel="canonical" href="https://www.evolving.money/">
+	<link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" />
+	<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/960-12-col.css" />
              
-  </head>
-<body>
-	<?php include("../includes/header.html");?>
-	<ul id="navigation">	 
-		<li><a href="index.html" class="on">G&T></a></li>
-  		<li><a href="glossary.html">Glossary</a></li>
-  		<li><a href="books.html">Books</a></li>
-  		<li><a href="media.html">Media</a></li>
-		<li><a href="websites.html">Websites</a></li>
- 	</ul>
-<div class="imgbox">
-    <img class="center-fit" src="../Images/G&T_logo.GIF">
-</div>
-<p> Sorry no alcohol here! Bit hopefully you find this Guides and Tools section useful. A mix of books and websites we have found useful in learning the world of investment oppportunities. We have even collated a glossary of terms to help decipher all the confusing terminology you may come across!
-</p>
-</div>
-  	<?php include("../includes/footer.html");?>  
-  </body> 
-</html> 
+</head>
 
+<body>
+
+	<?php include("https://www.evolving.money/includes/test-header.html");?>
+
+<div>
+	<div class="row" margin 0 0;>
+		<div class="col_12 first" style="background-color:#fff">
+			<div class="imgbox">
+    				<img class="left-fit-page-heading" src="../Images/G&T_logo.GIF">
+			</div>
+		</div>
+	</div> 
+
+	<div class="clear"></div>
+
+	<div class="row">
+		<div class="col_9 first" style="background-color:#fff">
+
+		<p> Sorry no alcohol here! Bit hopefully you find this Guides and Tools section useful. A mix of books and websites we have found useful in learning the world of investment oppportunities. We have even collated a glossary of terms to help decipher all the confusing terminology you may come across!
+		</p>
+	</div>
+
+		<div class="col_3 last" style="background-color:#fff">
+			<div class="imgbox"><img class="center-fit" src="https://www.evolving.money/Images/EM_circ_logo.jpg"></div>
+		</div>
+
+	</div>
+
+	<div class="clear"></div>
+
+</div>
+
+<?php include("https://www.evolving.money/includes/footer.html");?>
+
+</body>
+</html>
