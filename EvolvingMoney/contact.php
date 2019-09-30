99433940924af2ae2763192b4676276381435b15
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-10-01 00:19:38 +0100

    Update contact.php

--- EvolvingMoney/contact.php
+++ EvolvingMoney/contact.php
@@ -1,45 +1,60 @@
 <!DOCTYPE html>
-<html>
-<head>		
-		<title>Contact Us</title>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />	
+		<title>Evolving Money - Contact Us</title>
 	 	<link type="text/css" rel="stylesheet" href = "css/learningPage.css"/>
-
-       		<meta name="description"
-             		content="Guide to new types of investments and ways of managing money, along with some macro and contrarian investment opportunites" />
-       		<meta name="keywords"
-             		content="investments, new, macro, crypto, contrarian, p2p, banking" />
-       		<meta name="robots"
-             		content="follow" />
-       		<meta http-equiv="author"
-             		content="James Day" />
+       		<meta name="description" content="Guide to new types of investments and ways of managing money, along with some macro and contrarian investment opportunites" />
+       		<meta name="keywords" content="investments, new, macro, crypto, contrarian, p2p, banking" />
+		<meta http-equiv="author"content="Leon Bond" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="robots" content="all">
+		<meta name="viewport" content="width=device-width,initial-scale=1" />
+		<link href="css/styles.css" type="text/css" rel="stylesheet" />
+		<link rel="canonical" href="https://www.evolving.money/">
+		<link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" />
+		<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/960-12-col.css" />
 </head>
 
 <body>
 
+	<?php include("includes/test-header.html");?>
 
-    <div id="page-container">
-        <div id="content-wrap">
-            	
-            <?php include("includes/header.html");?>
-            <br>
-                <div class="imgbox">
-                    <img src="Images/ContactUs_logo.GIF" alt="Contact Us" />
-	            </div>
-            <br>	
+<div>
+	<div class="row" margin 0 0;>
+		<div class="col_12 first" style="background-color:#fff">
+			<div class="imgbox">         		
+				<img class="left-fit-page-heading" src="Images/ContactUs_logo.GIF" alt="Contact Us" />
+			</div>
+		</div>
+	</div> 
+
+	<div class="clear"></div>
+
+	<div class="row">
+		<div class="col_9 first" style="background-color:#fff">
             <p>If you would like to get in touch with us then please drop either of us an email: </p>
             <br>
-            <a href="mailto:leon@evolving.money">Email: Leon</a>
+            <a href="mailto:leon@evolving.money">leon@evolving.money</a>
             <br>
+            <a href="mailto:james_graham_day@yahoo.co.uk">james_graham_day@yahoo.co.uk</a>
             <br>
-            <a href="mailto:james_graham_day@yahoo.co.uk">Email: James</a>
-            <br>
-            <br>
-            <br>
-        </div>
-        <footer id="footer">
-  	        <?php include("includes/footer.html");?>  
-  	    </footer>
-  	</div>
-</body> 
-	
-</html> 
+
+		</div>
+
+		<div class="col_3 last" style="background-color:#fff">
+
+			<div class="imgbox"><img class="center-fit" src="Images/EM_circ_logo.jpg"></div>
+
+		</div>
+
+	</div>
+
+	<div class="clear"></div>
+
+</div>
+
+<?php include("includes/footer.html");?>
+
+</body>
+</html>
