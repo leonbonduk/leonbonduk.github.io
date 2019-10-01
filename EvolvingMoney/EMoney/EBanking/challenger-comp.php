25ab7256fe4d2ceecf6a7388c560b0788f1e9227
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-10-01 01:02:59 +0100

    Update challenger-comp.php

--- EvolvingMoney/EMoney/EBanking/challenger-comp.php
+++ EvolvingMoney/EMoney/EBanking/challenger-comp.php
@@ -3,39 +3,54 @@
 <head>		
 		<meta charset="UTF-8">
 		<title>Comapre the Challenger Banks</title>
+		<script src="https://evolving.money/table-nodejs/modernizr.js"></script> <!-- Modernizr -->
+       		<meta name="description" content="Guide to comparing the new challenger banks." />
+       		<meta name="keywords" content="compare, challenger banks, banking, fintech" />
+       		<meta http-equiv="author" content="Leon Bond" />
+	       	<meta name="robots" content="all">
+		<meta name="viewport" content="width=device-width,initial-scale=1" />
 		<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" href="../../css/reset.css"> <!-- CSS reset -->
-	 	<!-- <link type="text/css" rel="stylesheet" href = "../css/learningPage.css"/> !-->
 		<link rel="stylesheet" href="../../css/style-comp-table.css"> <!-- Resource style -->
-		<script src="https://evolving.money/table-nodejs/modernizr.js"></script> <!-- Modernizr -->
-
-       		<meta name="description"
-             		content="Guide to comparing the new challenger banks." />
-       		<meta name="keywords"
-             		content="compare, challenger banks, banking, fintech" />
-       		<meta name="robots"
-             		content="follow" />
-       		<meta http-equiv="author"
-             		content="Leon Bond" />
+		<link href="css/styles.css" type="text/css" rel="stylesheet" />
+		<link rel="canonical" href="https://www.evolving.money/">
+		<link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" />
+		<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/960-12-col.css" />
 </head>
 
 <body>
-	<?php include("../../includes/header.html");?>
-	<ul id="navigation">	 
-  		<li><a href="what-is-emoney.php" class="on">E-Money></a></li>
-  		<li><a href="crypto.php">Crypto</a></li>
-  		<li><a href="EBanking/challenger-comp.php">Banking</a></li>
-     	</ul>
-	<br>
-	<div class="imgbox">
-    		<img class="center-fit" src="../Images/Banking_logo.GIF">
+
+	<?php include("https://www.evolving.money/includes/test-header.html");?>
+
+<div>
+	<div class="row" margin 0 0;>
+		<div class="col_12 first" style="background-color:#fff">
+			<div class="imgbox">
+    				<img class="left-fit-page-heading" src="../Images/Banking_logo.GIF">
+			</div>
+		</div>
+	</div> 
+
+	<div class="clear"></div>
+	<div class="row">
+		<div class="col_9 first" style="background-color:#fff">
+		<p> With multiple challenger banks to choose from now, each offering a plethora of features and quite varying fee structures it can be a tough decision as to which will best meet you needs. Each are regularly adding new features to their offerings in a fast-developing market and in doing so are shaking up the banking market with the legacy banking giants starting wake up to the need to develop their own offerings. The challenger banks though, being small and nimble due to their newer cloud-based systems, still seem to have the edge in developing new features over the legacy, slower moving banks with their relatively antiquated banking systems. Below is detailed comparison of all features and fees of each challenger bank so you can compare each banks offering side-by-side. Select several that interest you most and hit the filter button to help make the comparison easier.</p>
+		<br>
+		<p>In summary none of the challenger banks offer any significant switching or sign on incentives that a lot of the legacy banks in the UK are offering. The challengers are looking to compete on service and features instead.
+		</p>
+	</div>
+		<div class="col_3 last" style="background-color:#fff">
+			<div class="imgbox"><img class="center-fit" src="https://www.evolving.money/Images/EM_circ_logo.jpg"></div>
+		</div>
+	</div>
+	<div class="clear"></div>
+
+</div>
 	<section class="cd-intro">
-		<h1>Challenger Bank Comparison Table 1</h1>
+		<h1>Challenger Bank Comparison Table</h1>
 	</section> <!-- .cd-intro -->
 	<section class="cd-products-comparison-table">
 		<header>
-			<h2>Compare Banks</h2>
-
 			<div class="actions">
 				<a href="#0" class="reset">Reset</a>
 				<a href="#0" class="filter">Filter</a>
@@ -217,10 +232,8 @@
 <script src="https://evolving.money/table-nodejs/jquery-2.1.4.js"></script>
 <script src="https://evolving.money/table-nodejs/main.js"></script> <!-- Resource jQuery -->
 
-       <footer id="footer">
- 	      <?php include("../../includes/footer.html");?>  
-       </footer>
-/body> 
+<?php include("https://www.evolving.money/includes/footer.html");?>
 
-</html> 
+</body>
 
+</html>
