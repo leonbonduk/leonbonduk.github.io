cc945fe6b0d57ab06606d4e55824b3cf8945d21e
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-10-04 00:20:59 +0100

    Update BT-Sept19.php

--- EvolvingMoney/InvIdeas/BT-Sept19.php
+++ EvolvingMoney/InvIdeas/BT-Sept19.php
@@ -1,44 +1,50 @@
-<html>
-  
-  	 <head>		
-       <title> BT a contrarian buy?</title>	
-  </head>
-<body>	
-<h1>			 <img src="Images/EM_logo.GIF"					 alt="Evolving Money" />		 </h1>	 
-<br>	 
-<h1> BT a contrarian buy?</h1>		 
- <h2>Image 1 sized and left aligned</hl>
-   <p><img src="../Images/BT_up.jpeg" alt="BT Tower" title="Is BT low share price a contrarian buy?" width=“100” height=“200” align=“left” /> Price: 167p <br>Date: 2 Sept 2019 <br> <br> With such a significant drop in share price over recent months, down 32% for the year, to PE of 6 and dividend yield of 9% this could well be an unmissable buying opportunity! <p>
-<br>
-<p>BT has a new CEO and the company is now beating analysts expectations
-<p>Related news and coverage:<p>
-<br>
-<ul>		 <li><a href="https://citywire.co.uk/wealth-manager/news/the-expert-view-bt-tesco-and-itv/a1264157#i=2">Citywire article 3 Sept 2019</a></li>
-</ul>
-<br>
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>		
+	<meta charset="UTF-8">
+       	<title>Evolving Money - BT a contrarian buy?</title>	
+ 	<meta name="description" content="IIs BT a contrarian buy?" />
+   	<meta name="keywords" content="BT, buy, share tip, contrarian" />
+       	<meta http-equiv="author" content="Leon Bond" />
+	<meta name="robots" content="all">
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<link href="https://www.evolving.money//css/styles.css" type="text/css" rel="stylesheet" />
+	<link rel="canonical" href="https://www.evolving.money/">
+	<link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" />
+	<link rel="stylesheet" type="text/css" href="https://www.evolving.money//css/960-12-col.css" />
 
-<h2>Global Map</h2>		 
-<p>	 <ul>		 <li><a href="index.html">Home</a></li>
-  <li><a href="about-us.html">About</a></li>
-  
-  <li><a href="InvIdeas/index.html">Investment Ideas</a></li>
-  
-  <!-- <li><a href="glossary.html">Glossary</a></li>
-  <li><a href="brokers.html">Brokers</a></li>	
-  <li><a href="property.html">Property</a></li>	
-  <li><a href="online-banking.html">Online Banking</a></li>	-->
-  
-  <li><a href="PerToPer/index.html">P2P</a></li>	 
-  
-  <li><a href="Further_Reading/index.html">Further Reading</a></li>	
-  
-  <li><a href="contact.html">Contact</a></li>	 
-  </ul> </p>m
- 
-  
-  <hr/>
-  
-  </body> 
+
+
+<body>
+
+
+
+	<?php include("../../includes/test-header.html");?>
+
+	
+<div class="row">
+
+	<div class="col_9 first" style="background-color:#fff">	
+		<br>	 
+		<h1> BT a contrarian buy?</h1>		 
+		<p><img src="../Images/BT_up.jpeg" alt="BT Tower" title="Is BT low share price a contrarian buy?" width=“100” height=“200” align=“left” /> Price: 167p <br>Date: 2 Sept 2019 <br> <br> With such a significant drop in share price over recent months, down 32% for the year, to PE of 6 and dividend yield of 9% this could well be an unmissable buying opportunity! <p>
+		<br>
+		<p>BT has a new CEO and the company is now beating analysts expectations
+		<p>Related news and coverage:<p>
+		<br>
+		<ul> <li><a href="https://citywire.co.uk/wealth-manager/news/the-expert-view-bt-tesco-and-itv/a1264157#i=2">Citywire article 3 Sept 2019</a></li>
+		</ul>
+		<br>
+	</div>
+
+	<div class="col_3 last" style="background-color:#fff">
+		<div class="imgbox"><img class="center-fit" src="https://www.evolving.money/Images/EM_circ_logo.jpg"></div>
+	</div>
+</div>
+
+<div class="clear"></div>
+
+<?php include("../../includes/footer.html");?>
+
+</body>
 </html> 
-
-		 
