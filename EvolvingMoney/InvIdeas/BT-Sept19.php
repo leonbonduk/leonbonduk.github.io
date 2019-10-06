a1e6155ff3d5b93725587a916a36117236d5c624
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-10-06 23:00:14 +0100

    Update BT-Sept19.php

--- EvolvingMoney/InvIdeas/BT-Sept19.php
+++ EvolvingMoney/InvIdeas/BT-Sept19.php
@@ -8,7 +8,7 @@
        	<meta http-equiv="author" content="Leon Bond" />
 	<meta name="robots" content="all">
 	<meta name="viewport" content="width=device-width,initial-scale=1" />
-	<link href="https://www.evolving.money/css/styles.css" type="text/css" rel="stylesheet" />
+	<link href="../css/styles.css" type="text/css" rel="stylesheet" />
 	<link rel="canonical" href="https://www.evolving.money/">
 	<link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" />
 	<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/960-12-col.css" />
@@ -22,7 +22,8 @@
 
 	<div class="col_9 first" style="background-color:#fff">	
 		<br>	 
-		<h1>Is BT a contrarian buy?</h1>		 
+		<h1>Is BT a contrarian buy?</h1>
+		<h2>Leon Bond, 6th October 2019</h2>
 		<p><img src="https://www.evolving.money/Images/BT_up.jpeg" alt="BT Tower" title="Is BT low share price a contrarian buy?" width=“100” height=“200” align=“left” /> Price: 167p <br>Date: 2 Sept 2019 <br> <br> With such a significant drop in share price over recent months, down 32% for the year, to PE of 6 and dividend yield of 9% this could well be an unmissable buying opportunity!
 		<br><br>
 			BT has a new CEO and the company is now beating analysts expectations</p>
@@ -57,24 +58,8 @@
 		</div>
 		<!-- TradingView Widget END -->
 		
-		<!-- TradingView Widget BEGIN -->
-<div class="tradingview-widget-container">
-  <div class="tradingview-widget-container__widget"></div>
-  <div class="tradingview-widget-copyright"><a href="https://uk.tradingview.com/symbols/NASDAQ-AAPL/" rel="noopener" target="_blank"><span class="blue-text">AAPL Fundamental Data</span></a> by TradingView</div>
-  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-financials.js" async>
-  {
-  "symbol": "NASDAQ:AAPL",
-  "colorTheme": "light",
-  "isTransparent": false,
-  "largeChartUrl": "",
-  "displayMode": "regular",
-  "width": "600",
-  "height": "830",
-  "locale": "uk"
-}
-  </script>
-</div>
-<!-- TradingView Widget END -->
+		<a href="https://www.evolving.money/EMoney/EBanking/challenger-comp.php"><div class="imgbox">
+				<img class="center-fit" src="Images/Comp_chall_banks.png"></div></a>
 		
 	</div>
 
@@ -82,23 +67,23 @@
 	
 	<div class="col_9 first" style="background-color:#fff" height: 550px >	
 		<!-- TradingView Widget BEGIN -->
-<div class="tradingview-widget-container">
-  <div class="tradingview-widget-container__widget"></div>
-  <div class="tradingview-widget-copyright"><a href="https://uk.tradingview.com/symbols/LSE-BT.A/" rel="noopener" target="_blank"><span class="blue-text">BT.A Fundamental Data</span></a> by TradingView</div>
-  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-financials.js" async>
-  {
-  "symbol": "LSE:BT.A",
-  "colorTheme": "light",
-  "isTransparent": false,
-  "largeChartUrl": "",
-  "displayMode": "compact",
-  "width": "100%",
-  "height": "550",
-  "locale": "uk"
-}
-  </script>
-</div>
-<!-- TradingView Widget END -->
+		<div class="tradingview-widget-container">
+  		<div class="tradingview-widget-container__widget"></div>
+  		<div class="tradingview-widget-copyright"><a href="https://uk.tradingview.com/symbols/LSE-BT.A/" rel="noopener" target="_blank"><span class="blue-text">BT.A Fundamental Data</span></a> by TradingView</div>
+  		<script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-financials.js" async>
+  		{
+  		"symbol": "LSE:BT.A",
+  		"colorTheme": "light",
+  		"isTransparent": false,
+  		"largeChartUrl": "",
+  		"displayMode": "compact",
+  		"width": "100%",
+  		"height": "550",
+  		"locale": "uk"
+		}
+  		</script>
+		</div>
+		<!-- TradingView Widget END -->
 	</div>
 	
 	<div class="clear"></div>
