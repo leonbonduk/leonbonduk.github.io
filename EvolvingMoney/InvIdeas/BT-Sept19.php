eb3b1a41d456180c044f75d2b73865cdf32b5fd1
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-10-06 22:14:56 +0100

    Update BT-Sept19.php

--- EvolvingMoney/InvIdeas/BT-Sept19.php
+++ EvolvingMoney/InvIdeas/BT-Sept19.php
@@ -56,6 +56,26 @@
   		</script>
 		</div>
 		<!-- TradingView Widget END -->
+		
+		<!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container">
+  <div class="tradingview-widget-container__widget"></div>
+  <div class="tradingview-widget-copyright"><a href="https://uk.tradingview.com/symbols/NASDAQ-AAPL/" rel="noopener" target="_blank"><span class="blue-text">AAPL Fundamental Data</span></a> by TradingView</div>
+  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-financials.js" async>
+  {
+  "symbol": "NASDAQ:AAPL",
+  "colorTheme": "light",
+  "isTransparent": false,
+  "largeChartUrl": "",
+  "displayMode": "regular",
+  "width": "600",
+  "height": "830",
+  "locale": "uk"
+}
+  </script>
+</div>
+<!-- TradingView Widget END -->
+		
 	</div>
 
 	<div class="clear"></div>
