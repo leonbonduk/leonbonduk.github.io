5c7227803c76d69317f04fad10ca7db6fc825765
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-10-06 21:44:37 +0100

    Update BT-Sept19.php

--- EvolvingMoney/InvIdeas/BT-Sept19.php
+++ EvolvingMoney/InvIdeas/BT-Sept19.php
@@ -60,6 +60,27 @@
 	</div>
 
 	<div class="clear"></div>
+	
+	<div class="col_9 first" style="background-color:#fff">	
+		<!-- TradingView Widget BEGIN -->
+		<div class="tradingview-widget-container">
+ 		<div class="tradingview-widget-container__widget"></div>
+  		<div class="tradingview-widget-copyright"><a href="https://uk.tradingview.com/symbols/LSE-BT.A/" rel="noopener" target="_blank"><span class="blue-text">BT.A Fundamental Data</span></a> by TradingView</div>
+  		<script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-financials.js" async>
+  		{
+  		"symbol": "LSE:BT.A",
+  		"colorTheme": "light",
+  		"isTransparent": false,
+  		"largeChartUrl": "",
+  		"displayMode": "compact",
+  		"width": "480",
+  		"height": "830",
+  		"locale": "uk"
+		}
+  		</script>
+	</div>
+	<!-- TradingView Widget END -->
+	</div>
 </div>
 	
 <?php include("../includes/footer.html");?>
