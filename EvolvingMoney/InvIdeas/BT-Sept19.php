367c661e13151bdc9f305b217db280d361c5618a
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-10-07 01:01:11 +0100

    Update BT-Sept19.php

--- EvolvingMoney/InvIdeas/BT-Sept19.php
+++ EvolvingMoney/InvIdeas/BT-Sept19.php
@@ -24,7 +24,7 @@
 		<br>	 
 		<h1>Is BT a contrarian buy?</h1>
 		<h2>Leon Bond, 2nd September 2019, Price £1.67</h2>
-		<p><img src="https://www.evolving.money/Images/BT_up.jpeg" alt="BT Tower" title="Is BT low share price a contrarian buy?" width=“100” height=“200” align=“left” /> Price: 167p <br>Date: 2 Sept 2019 <br> <br> With such a significant drop in share price over recent months, down 32% for the year, to PE of 6 and dividend yield of 9% this could well be an unmissable buying opportunity!
+		<p><img src="https://www.evolving.money/Images/BT_up.jpeg" alt="BT Tower" title="Is BT low share price a contrarian buy?" style="width:300px;height:400px;float: left;"/> <br> With such a significant drop in share price over recent months, down 32% for the year, to PE of 6 and dividend yield of 9% this could well be an unmissable buying opportunity!
 		<br><br>
 			BT has a new CEO and the company is now beating analysts expectations</p>
 		<p>Related news and coverage:</p>
@@ -32,31 +32,31 @@
 	</div>
 
 	<div class="col_4 last" style="background-color:#fff">
-		<!-- TradingView Widget BEGIN -->
-		<div class="tradingview-widget-container">
-  		<div id="tv-medium-widget"></div>
-  		<div class="tradingview-widget-copyright"><a href="https://uk.tradingview.com/symbols/LSE-BT/" rel="noopener" target="_blank"><span class="blue-text">BT Quotes</span></a> by TradingView</div>
-  		<script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
-  		<script type="text/javascript">
-  		new TradingView.MediumWidget(
-  		{
-  		"container_id": "tv-medium-widget",
-  		"symbols": [
-    		"LSE:BT.A|60m"
-  		],
-  		"greyText": "Quotes by",
-  		"gridLineColor": "#e9e9ea",
-  		"fontColor": "#83888D",
-  		"underLineColor": "#dbeffb",
-  		"trendLineColor": "#4bafe9",
-  		"width": "600px",
-  		"height": "800px",
-  		"locale": "uk"
-		}
-  		);
-  		</script>
-		</div>
-		<!-- TradingView Widget END -->
+<!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container">
+  <div id="tv-medium-widget"></div>
+  <div class="tradingview-widget-copyright"><a href="https://uk.tradingview.com/symbols/LSE-BT/" rel="noopener" target="_blank"><span class="blue-text">BT Quotes</span></a> by TradingView</div>
+  <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+  <script type="text/javascript">
+  new TradingView.MediumWidget(
+  {
+  "container_id": "tv-medium-widget",
+  "symbols": [
+    "LSE:BT.A|60m"
+  ],
+  "greyText": "Quotes by",
+  "gridLineColor": "#e9e9ea",
+  "fontColor": "#83888D",
+  "underLineColor": "#dbeffb",
+  "trendLineColor": "#4bafe9",
+  "width": "100%",
+  "height": "100%",
+  "locale": "uk"
+}
+  );
+  </script>
+</div>
+<!-- TradingView Widget END -->
 		
 		<a href="https://www.evolving.money/EMoney/EBanking/challenger-comp.php"><div class="imgbox">
 				<img class="center-fit" src="../Images/Comp_chall_banks.png"></div></a>
@@ -65,7 +65,7 @@
 
 	<div class="clear"></div>
 	
-	<div class="col_9 first" style="background-color:#fff" height: 550px >	
+	<div class="col_9 first" style="background-color:#fff" >	
 		<!-- TradingView Widget BEGIN -->
 		<div class="tradingview-widget-container">
   		<div class="tradingview-widget-container__widget"></div>
