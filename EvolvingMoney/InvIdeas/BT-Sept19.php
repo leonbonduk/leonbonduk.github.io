064ee51fa3c96d3a52f6cc04cc9142573fd20558
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-10-09 00:46:14 +0100

    Update BT-Sept19.php

--- EvolvingMoney/InvIdeas/BT-Sept19.php
+++ EvolvingMoney/InvIdeas/BT-Sept19.php
@@ -23,10 +23,26 @@
 	<div class="col_8 first" style="background-color:#fff">	
 		<br>	 
 		<h1 class="h1_article">Is BT a contrarian buy?</h1>
-		<h2>Leon Bond, 2nd September 2019, Price £1.67</h2>
-		<p><img src="https://www.evolving.money/Images/BT_up.jpeg" alt="BT Tower" title="Is BT low share price a contrarian buy?" style="width:300px;height:400px;float: left; margin: 1%; padding: 1%;"/> <br> With such a significant drop in share price over recent months, down 32% for the year, to PE of 6 and dividend yield of 9% this could well be an unmissable buying opportunity!
-		<br><br>
-			BT has a new CEO and the company is now beating analysts expectations</p>
+		<h2>Leon Bond, 2nd September 2019 (updated 17th September 2019), Price £1.67</h2>
+		<p><img src="https://www.evolving.money/Images/BT_up.jpeg" alt="BT Tower" title="Is BT low share price a contrarian buy?" style="width:300px;height:400px;float: left; margin: 1%; padding: 1%;"/> </p>
+		<p>With the share price now at 167p, this is significantly down from the heady heights of 500p back in 2015, that’s almost 67% down from it’s peak and 30% down for the year to date. With a forecast PE ratio of 6 (it was around 16 back in 2015!) and a dividend yield of 9% could this now be an unmissable buying opportunity?.</p> 
+		<h3>What went wrong?</h3>
+		<p>After a dramatic rise in the share price in the years up to 2015, from the lows after the last financial crisis, the price has fallen significantly since the EE acquisition back in January 2016. EE has been a relative bright spot in BT’s results since acquisition but other various headwind pressures have battered the share price including:
+		<ul>
+			<li>A fall in revenues and earnings driven by regulated prices at Openreach, a fall in voice revenues as VOIP grows more popular, as well as falls in the international Global Services division.</li>
+			<li>An accounting scandal in the Italian division resulted in a charge of £530mil early 2017.</li>
+			<li>Cashflow pressures to cover capital expenditure needs for the roll out of full fibre, 5G and significant pension liabilities on the historic defined contribution schemes all threaten the perceived ability of the company to continue to pay it’s dividend at the same levels.</li>
+			<li>Growth in debt to fund the pension top-up payments.</li>
+			<li>Potential significant costs for new 5G spectrum and football rights.</li>
+			<li>A rocky relationship with the regulator along with little clarity as to whether Openreach should be spun out as separate company or not.</li>
+		</ul>	
+		<h3>The way ahead</h3>
+		<p>BT’s revenue’s and earnings are forecast to still fall slightly in the coming year but the hire of the new CEO, Philip Jansen, at the start of the year and his reputation for restructuring should hopefully start to bare fruit in the next few years. He has been doing the rounds of government departments in recent weeks plugging an accelerated roll-out of full fibre services, which should be music to the ears of Boris Johnson’s government who appear to be putting more focus on such a vision to drive economic growth for  the country in a post-Brexit world. Along with a change in personnel at the regulator Ofgem, relations should be thawing for BT and hopefully Philip has enough bargaining power that increased rates will be rewarded in return for an accelerated roll out of fibre.</p>
+		<p>Although earlier this month Bank of America Merrill Lynch suggested a 40% cut in the dividend was likely this may not be such a bad thing if and when it does happen. The dividend would still be a not unreasonable 5.4% and the extra cash would allow BT to accelerate the roll out of full fibre and so provide the modern network required to drive revenues and financial growth in the coming years. It will also finally put an end to market speculation around such a cut and so the market can move on to future returns that can be driven from forthcoming capital expenditure.</p>
+		<p>The pressures from the costs of football rights costs should hopefully ease now that there is the reciprocal deal with Sky to provide each other’s matches. Under the new CEO’s tenure there also appears to be shift away from this expensive distraction towards a focus on BT’s core strengths as an infrastructure company.</p>
+		<p>The Global Services division has also been pruned and refocused to higher margin services and so hopefully providing a sounder footing for growth for this segment of the Group.</p>
+		<p>Philip also now has a lot of skin in the game owing £3.6million of BT shares (having bought a further £1mil worth on 16th September), quite a significant purchase from a director, and showing his faith in the future prospect of the group.</p>
+		<p>With a forecast PE ratio of just over 6 the market appears to have already factored in an excessive worst case scenario of costs and cashflow restrictions and does not give any value to the potential future growth in returns from an accelerated roll out of the full fibre service and those of other of the other restructuring activities being enacted . Surely at such a lowly valuation the risk is now to the upside and any positive news and signs that Philip’s plans are starting to deliver should initiate a return to a growth in the share price. Therefore I think on balance, the company’s shares are a “Buy”.</p>
 		<p>Related news and coverage:</p>
 		<br>
 	</div>
