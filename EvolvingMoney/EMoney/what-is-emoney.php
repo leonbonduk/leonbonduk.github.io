dfcef1d65c3732f3e1f32aa4a558a93b1987f4f0
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-10-09 21:02:00 +0100

    Update what-is-emoney.php

--- EvolvingMoney/EMoney/what-is-emoney.php
+++ EvolvingMoney/EMoney/what-is-emoney.php
@@ -10,7 +10,7 @@
 		<meta name="viewport" content="width=device-width,initial-scale=1" />
 		<link href=""https://www.evolving.money/css/styles.css" type="text/css" rel="stylesheet" />
 		<link rel="canonical" href="https://www.evolving.money/">
-		<link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" />
+		<!-- <link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" /> !-->
 		<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/960-12-col.css" />
 </head>
 
