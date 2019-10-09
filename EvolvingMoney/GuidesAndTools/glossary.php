596ff718564ab50b29a9fd5f5a5872d8da402d02
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-10-09 01:08:44 +0100

    Update glossary.php

--- EvolvingMoney/GuidesAndTools/glossary.php
+++ EvolvingMoney/GuidesAndTools/glossary.php
@@ -1,33 +1,70 @@
 <!DOCTYPE html>
-<html>
-   	 <head>		
-       <title>Glossary</title>
- 
-   <link href="../css/learningPage.css" type="text/css" rel="stylesheet" />
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+       	<title>Evolving Money - Glossary</title>
+       	<meta name="description" content="Glossary of some key finacial terms explained in easy to udnerstand terms" />
+       	<meta name="keywords" content="finacial terms, glossary, investments, new, macro, crypto, contrarian, p2p, banking" />
+       	<meta http-equiv="author" content="Leon Bond" />
+       	<meta name="robots" content="all">
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<link href="https://www.evolving.money/css/styles.css" type="text/css" rel="stylesheet" />
+	<link rel="canonical" href="https://www.evolving.money/">
+	<link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" />
+	<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/960-12-col.css" />
 
-       <meta name="description"
-             content="Guide to new types of investments and ways of managing money, along with some macro and contrarian investment opportunites" />
-       <meta name="keywords"
-             content="investments, new, macro, crypto, contrarian, p2p, banking" />
-       <meta name="robots"
-             content="follow" />
-       <meta http-equiv="author"
-             content="Leon Bond" />
-       	 
-             
-  </head>
+</head>
+
 <body>
-	<?php include("../includes/header.html");?>
-	<ul id="navigation">	 
-		<li><a href="index.html" class="on">G&T></a></li>
-  		<li><a href="glossary.html">Glossary</a></li>
-  		<li><a href="books.html">Books</a></li>
-  		<li><a href="media.html">Media</a></li>
-		<li><a href="websites.html">Websites</a></li>
- 	</ul>
-<div class="imgbox">
-    <img class="center-fit" src="../Images/Glossary_logo.GIF">
+	<?php include("../includes/test-header.html");?>
+
+<div>
+	<div class="row" margin 0 0;>
+		<div class="col_12 first" style="background-color:#fff">
+			<div class="imgbox">
+    				<img class="left-fit-page-heading" src="https://www.evolving.money/Images/Glossary_logo.gif">
+			</div>
+		</div>
+	</div> 
+	
+	<div class="clear"></div>
+
+	<div class="row">
+		<div class="col_9 first" style="background-color:#fff">
+			<h1>Glossary of financial terms</h1>
+			<p> A useful collection of terms to help decipher all the confusing terminology you may come across!</p>
+			<dl>	 
+			<dt><h2>Challenger bank</h2></dt>	 
+			<dd>One of the new wave of start up companies providing banking services ! usually just over the internet or through an app on your phone. Butt here are one two smaller high street banks classed as such as well such as Metro Bank, TSB and Virgin.</dd>
+			<dt><h2>Crypto</h2></dt>	 
+			<dd>Short for cryptocurrency, an oline currency usually utilising blockchain technology to provide a method to transact whilst avoiding the usual banking institutions and their fees.</dd>	 
+			<dt><h2>ETF</h2></dt>	 
+			<dd>Exchange Traded Fund, usually a very low cost way of trading funds. Typically these are passive funds such that there is minimal input from a manager in determining what to invest in. Passive funds normally invest based on a pre determined methodology such as following market index. They can be a very useful investment to trade quickly and cheaply into a market trend or theme.</dd>
+			<dt><h2>Growth investing</h2></dt>	 
+			<dd>Investing in shares of companies that are growing their revenues and hopefully profits above industry trends. Typically these are new tech companies.</dd>
+			<dt><h2>P2P</h2></dt>	 
+			<dd>P2P is short for Person to Person lending. This is where any member of the public can lend their iwn money directly to another member of the public. The intermediary website takes a small fixed cut but the rest if the risk and reward resides with the person providing the funds. Therefore potentially this form of investment can provide better rates to both the lender and the holder of the loan.</dd> 
+			<dt><h2>PB ratio (price to book ratio)</h2></dt>
+			<dd>A commonly used methodology to determine the fair valuation of a company. It is determined as the ratio of the companies market value (or share price) as a proportion of the net assets of the company. It is commonly used to find value opportunities, wher bu a ratio of over one could indicate such an opportunity as the companies assets are potentially worth more than the market valuation of the company.</dd> 
+			<dt><h2>PE ratio (price to earnings ratio)</h2></dt>	 
+			<dd>This a commonly used methodology to determine whether a company is under or overvalued. It is determined as the ratio of the share price of the company as a proportion of its earnings or profit after tax. The S&P 500 has typically traded in a range from 10 to 20. So typically a ratio under 10 could indicate that a company is undervalued but may also indicate that the company is going through a rough patch such that its results may come in lower than last time or it may be about to cut its dividend or make a cash call on investors. A value above 20 could indicate that a conpany is overvalued but high growth companies can typically hold such valuations for long periods of time also.</dd>
+			<dt><h2>Value investing</h2></dt>	 
+			<dd>Investing in shares that hopefully the market has overlooked and undervalued. Typically this is in more mature companies which may have had recent challenges to their business model but have managed stabilise the business for future steady returns to investors.</dd>
+			</dl>
+		</div>
+		<div class="col_3 last" style="background-color:#fff">
+			<div class="imgbox"><img class="center-fit" src="https://www.evolving.money/Images/EM_circ_logo.jpg"></div>
+		</div>
+
+	</div>
+
+	<div class="clear"></div>
 </div>
+
+<?php include("../includes/footer.html");?>
+
+</body>
+</html>
  
 <p> A useful collection of terms to help decipher all the confusing terminology you may come across!
 </p>
@@ -61,5 +98,3 @@
   	<?php include("../includes/footer.html");?>  
   </body> 
 </html> 
- 
- 
