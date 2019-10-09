50ac25db3f25a79acbbe5dcc4153d3484bda0a0f
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-10-10 00:06:40 +0100

    Update challenger-comp.php

--- EvolvingMoney/EMoney/EBanking/challenger-comp.php
+++ EvolvingMoney/EMoney/EBanking/challenger-comp.php
@@ -12,9 +12,8 @@
 		<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" href="../../css/reset.css"> <!-- CSS reset -->
 		<link rel="stylesheet" href="../../css/style-comp-table.css"> <!-- Resource style -->
-		<link href="../../css/styles.css" type="text/css" rel="stylesheet" />
-		<link rel="canonical" href="https://www.evolving.money/">
-		<link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" />
+		<!-- <link href="../../css/styles.css" type="text/css" rel="stylesheet" /> !-->
+		<!-- <link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" /> !-->
 		<link rel="stylesheet" type="text/css" href="../../css/960-12-col.css" />
 </head>
 
