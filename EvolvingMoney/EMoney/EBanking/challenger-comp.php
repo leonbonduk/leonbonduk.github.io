69449d6a3b804c2c039962f137a88a567f4e5d79
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-10-11 12:52:06 +0100

    Update challenger-comp.php

--- EvolvingMoney/EMoney/EBanking/challenger-comp.php
+++ EvolvingMoney/EMoney/EBanking/challenger-comp.php
@@ -1,244 +1,26 @@
 <!DOCTYPE html>
 <html lang="en" class="no-js">
 <head>		
-		<meta charset="UTF-8">
-		<title>Compare the Challenger Banks</title>
-	<!--	<script src="https://evolving.money/table-nodejs/modernizr.js"></script> <!-- Modernizr -->
-       		<meta name="description" content="Guide to comparing the new challenger banks." />
-       		<meta name="keywords" content="compare, challenger banks, banking, fintech, Revolut, Monese, Starling, N26, Monzo, Cashplus, Monzo plus, Cashplus Activeplus, Cashplus Deluxe, Monese Simple, Monese classic, Monese premium, N26 black, N26 metal, Revolut metal, Revolut premium" />
-       		<meta name="author" content="Leon Bond" />
-	       	<meta name="robots" content="all">
-		<meta name="viewport" content="width=device-width,initial-scale=1" />
-	<!--	<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600' rel='stylesheet' type='text/css'>
-		<!-- <link rel="stylesheet" href="../../css/reset.css">  CSS reset -->
-		<!-- <link rel="stylesheet" href="../../css/style-comp-table.css">  Resource style -->
-		<link href="../../css/styles.css" type="text/css" rel="stylesheet" />
-		<!-- <link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" /> !-->
-		<link rel="stylesheet" type="text/css" href="https://evolving.money/css/960-12-col.css" />
+	<meta charset="UTF-8">
+       	<title>Evolving Money - BT a contrarian buy?</title>	
+ 	<meta name="description" content="Is BT a contrarian buy?" />
+   	<meta name="keywords" content="BT, buy, share tip, contrarian" />
+       	<meta name="author" content="Leon Bond" />
+	<meta name="robots" content="all">
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<link href="../css/styles.css" type="text/css" rel="stylesheet" />
+	<!-- <link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" /> !-->
+	<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/960-12-col.css" />
 </head>
 
 <body>
 
-	<?php include("../../includes/test-header.html");?>
-<!--
-<div>
-	<div class="row">
-		<div class="col_12 first" style="background-color:#fff">
-			<div class="imgbox">
-    				<img class="left-fit-page-heading" src="https://www.evolving.money/Images/Banking_logo.GIF" alt="Banking.Money">
-			</div>
-		</div>
-	</div> 
-	<div class="clear"></div>
-	<div class="row">
-		<div class="col_12 first" style="background-color:#fff">
-			<h1>Which is the best challenger bank?</h1>
-		</div>
-	</div> 
+	<?php include("../includes/test-header.html");?>
 	
-	<div class="clear"></div>
-	<div class="row">
-		<div class="col_9 first" style="background-color:#fff">
-		<p> With multiple challenger banks to choose from now, each offering a plethora of features and quite varying fee structures it can be a tough decision as to which will best meet you needs. Each are regularly adding new features to their offerings in a fast-developing market and in doing so are shaking up the banking market with the legacy banking giants starting wake up to the need to develop their own offerings. The challenger banks though, being small and nimble due to their newer cloud-based systems, still seem to have the edge in developing new features over the legacy, slower moving banks with their relatively antiquated banking systems. Below is detailed comparison of all features and fees of each challenger bank so you can compare each banks offering side-by-side. Select several that interest you most and hit the filter button to help make the comparison easier.</p>
-		<br>
-		<p>In summary none of the challenger banks offer any significant switching or sign on incentives that a lot of the legacy banks in the UK are offering. The challengers are looking to compete on service and features instead.
-		</p>
-	</div>
-		<div class="col_3 last" style="background-color:#fff">
-			<div class="imgbox"><img class="center-fit" src="https://www.evolving.money/Images/EM_circ_logo.jpg" alt="Evolving.Money logo"></div>
-		</div>
-	</div>
-	<div class="clear"></div>
+	<?php include("../includes/footer.html");?>
 
-</div>
-	<section class="cd-intro">
-		<h1>Challenger Bank Comparison Table</h1>
-	</section> <!-- .cd-intro -->
-<!--	<section class="cd-products-comparison-table">
-		<header>
-			<div class="actions">
-				<a href="#0" class="reset">Reset</a>
-				<a href="#0" class="filter">Filter</a>
-			</div>
-		</header>
 
-		<div class="cd-products-table">
-			<div class="features">
-				<div class="top-info">Account</div>
-
-				<ul class="cd-features-list">
-					<li>Card</li>
-					<li>EM rating</li>
-					<li>Founded</li>
-					<li>Number of customers</li>
-					<li>Monthly fee</li>
-					<li>Free UK Account</li>
-					<li>Free Euro Account</li>
-					<li>FX at inter-bank rate</li>
-					<li>Fee free ATM withdrawl</li>
-					<li>Banking liscense</li>
-					<li>Apple Pay</li>
-					<li class="cd-features-subheading">Security features</li>
-					<li>Transaction notification</li>
-				<li>Virtual cards</li>
-					<li>Freeze card in app</li>
-					<li>Magstripe disable</li>
-					<li>Online 3D secure</li>
-					<li>Refunds for fraudulent trans</li>
-					<li>Location based</li>
-					<li>Contactless disable</li>
-					<li>ATM disable</li>
-					<li>E commerce disable</li>
-				</ul>
-			</div> <!-- .features -->
-			
-
-	<!--		<div class="cd-products-wrapper">
-				<ul class="cd-products-columns">
-					<li class="product">
-						<div class="top-info">
-							<div class="check"></div>
-							<img class="col-fit-img" src="https://evolving.money/Images/Revolut_card_logo_EM.png" alt="product image">
-							<h3>Revolut</h3>
-						</div> <!-- .top-info -->
-
-		<!--				<ul class="cd-features-list">
-							<li>xxx</li>
-							<li class="rate"><span>4/5</span></li>
-							<li>London, 2015</li>
-							<li>7,000,000+</li>
-							<li>£0.00</li>
-							<li class="tick"><span>5/5</span></li>
-							<li>Yes</li>
-							<li>Yes</li>
-							<li>Up to £200pm</li>
-							<li>Received</li>
-							<li>Yes</li>
-							<li class="cd-features-subheading"><br></li>
-							<li><img  src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-							<li>TBC</li>
-							<li>TBC</li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-						</ul>
-					</li> <!-- .product -->
-
-<!--					<li class="product">
-						<div class="top-info">
-							<div class="check"></div>
-							<img class="col-fit-img" src="https://evolving.money/Images/Revolut_prem_logo_EM.png" alt="product image">
-							<h3>Revolut Premium</h3>
-						</div> <!-- .top-info -->
-<!--
-						<ul class="cd-features-list">
-							<li>xxx</li>
-							<li class="rate"><span>5/5</span></li>
-							<li>London, 2015</li>
-							<li>7,000,000+</li>
-							<li>£6.99</li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image" height = "14" width = "14"></li>
-							<li><img src="https://evolving.money/Images/cancel.png" alt="product image" height = "14" width = "14"> Up to £400pm</li>
-							<li>Received</li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image" height = "14" width = "14"></li>
-							<li class="cd-features-subheading"><br></li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-							<li>TBC</li>
-							<li>TBC</li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-						</ul>
-					</li> <!-- .product -->
-
-	<!--				<li class="product">
-						<div class="top-info">
-							<div class="check"></div>
-							<img class="col-fit-img" src="https://evolving.money/Images/Revolut_metal_logo_EM.png" alt="product image">
-							<h3>Revolut Metal</h3>
-						</div> <!-- .top-info -->
-
-	<!--					<ul class="cd-features-list">
-							<li>xxx</li>
-							<li class="rate"><span>5/5</span></li>
-							<li>London, 2015</li>
-							<li>7,000,000+</li>
-							<li>£12.99</li>
-							<li>Yes</li>
-							<li>Yes</li>
-							<li>Yes</li>
-							<li>Up to £600pm</li>
-							<li>Received</li>
-							<li>Yes</li>
-							<li class="cd-features-subheading"><br></li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-							<li>TBC</li>
-							<li>TBC</li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-						</ul>
-					</li> <!-- .product -->
-	<!--				<li class="product">
-						<div class="top-info">
-							<div class="check"></div>
-							<img class="col-fit-img" src="https://evolving.money/Images/Starling_card_logo_EM.png" alt="product image">
-							<h3>Starling</h3>
-						</div> <!-- .top-info -->
-
-		<!--				<ul class="cd-features-list">
-							<li>xxx</li>
-							<li class="rate"><span>5/5</span></li>
-							<li>London, 2015</li>
-							<li>2,000,000+</li>
-							<li>£0.00</li>
-							<li>Yes</li>
-							<li>Yes</li>
-							<li>Yes</li>
-							<li>Up to £600pm</li>
-							<li>Received</li>
-							<li>Yes</li>
-							<li class="cd-features-subheading"><br></li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-							<li>TBC</li>
-							<li>TBC</li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-							<li><img src="https://evolving.money/Images/checked.png" alt="product image"  height = "14" width = "14"></li>
-						</ul>
-					</li> <!-- .product -->
-				</ul> <!-- .cd-products-columns -->
-			</div> <!-- .cd-products-wrapper -->
-			
-	<!--		<ul class="cd-table-navigation">
-				<li><a href="#0" class="prev inactive">Prev</a></li>
-				<li><a href="#0" class="next">Next</a></li>
-			</ul>
-		</div> <!-- .cd-products-table -->
-	</section> <!-- .cd-products-comparison-table -->
-<!--<script src="https://evolving.money/table-nodejs/jquery-2.1.4.js"></script>
-<script src="https://evolving.money/table-nodejs/main.js"></script> <!-- Resource jQuery -->
-
-<?php include("../../includes/footer.html");?>
 
 </body>
 
-</html>
+</html> 
