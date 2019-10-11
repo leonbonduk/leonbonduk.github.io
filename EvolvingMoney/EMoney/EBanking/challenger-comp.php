aa191947ce83b78db831880f4b0c1861ea3fb372
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-10-11 12:53:23 +0100

    Update challenger-comp.php

--- EvolvingMoney/EMoney/EBanking/challenger-comp.php
+++ EvolvingMoney/EMoney/EBanking/challenger-comp.php
@@ -8,16 +8,16 @@
        	<meta name="author" content="Leon Bond" />
 	<meta name="robots" content="all">
 	<meta name="viewport" content="width=device-width,initial-scale=1" />
-	<link href="../css/styles.css" type="text/css" rel="stylesheet" />
+	<link href="../../css/styles.css" type="text/css" rel="stylesheet" />
 	<!-- <link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" /> !-->
-	<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/960-12-col.css" />
+	<link rel="stylesheet" type="text/css" href="../../css/960-12-col.css" />
 </head>
 
 <body>
 
-	<?php include("../includes/test-header.html");?>
+	<?php include("../../includes/test-header.html");?>
 	
-	<?php include("../includes/footer.html");?>
+	<?php include("../../includes/footer.html");?>
 
 
 
