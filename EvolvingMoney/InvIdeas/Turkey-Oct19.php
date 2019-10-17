7d8531b740972775b73dff226f3958c2cbf24747
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-10-18 00:17:03 +0100

    Create Turkey-Oct19.php

---
+++ EvolvingMoney/InvIdeas/Turkey-Oct19.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>		
+	<meta charset="UTF-8">
+       	<title>Evolving Money - Turkish Delight</title>	
+ 	<meta name="description" content="Is BT a contrarian buy?" />
+   	<meta name="keywords" content="Turkey, buy, share tip, etf, contrarian, buying opportunity" />
+       	<meta name="author" content="Leon Bond" />
+	<meta name="robots" content="all">
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<link href="../css/styles.css" type="text/css" rel="stylesheet" />
+	<!-- <link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" /> !-->
+	<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/960-12-col.css" />
+
+</head>
+
+<body>
+	<?php include("../includes/test-header.html");?>
+	
+<div class="row">
+
+	<div class="col_8 first" style="background-color:#fff">	
+		<br>	 
+		<h1 class="h1_article">Turkish Delight</h1>
+		<h3>Leon Bond, 17th October 2019, Price £1.67</h3>
+		<p><img src="https://www.evolving.money/Images/turkey-flag-wrinkled.jpg" alt="Wrinkled Turkish Flag" title="Turkish Delight" style="width:300px;height:auto;float: left; margin: 1%; padding: 1%;"/> </p>
+		<p>With the share price now at 167p, this is significantly down from the heady heights of 500p back in 2015, that’s almost 67% down from it’s peak and 30% down for the year to date. With a forecast PE ratio of 6 (it was around 16 back in 2015!) and a dividend yield of 9% could this now be an unmissable buying opportunity?.</p> 
+		<h2>What went wrong?</h2>
+		<p>After a dramatic rise in the share price in the years up to 2015, from the lows after the last financial crisis, the price has fallen significantly since the EE acquisition back in January 2016. EE has been a relative bright spot in BT’s results since acquisition but other various headwind pressures have battered the share price including:</p>
+		<ul class="Glossary">
+			<li>A fall in revenues and earnings driven by regulated prices at Openreach, a fall in voice revenues as VOIP grows more popular, as well as falls in the international Global Services division.</li>
+			<li>An accounting scandal in the Italian division resulted in a charge of £530mil early 2017.</li>
+			<li>Cashflow pressures to cover capital expenditure needs for the roll out of full fibre, 5G and significant pension liabilities on the historic defined contribution schemes all threaten the perceived ability of the company to continue to pay it’s dividend at the same levels.</li>
+			<li>Growth in debt to fund the pension top-up payments.</li>
+			<li>Potential significant costs for new 5G spectrum and football rights.</li>
+			<li>A rocky relationship with the regulator along with little clarity as to whether Openreach should be spun out as separate company or not.</li>
+		</ul>	
+		<h2>The way ahead</h2>
+		<p>BT’s revenue’s and earnings are forecast to still fall slightly in the coming year but the hire of the new CEO, Philip Jansen, at the start of the year and his reputation for restructuring should hopefully start to bear fruit in the next few years. He has been doing the rounds of government departments in recent weeks plugging an accelerated roll-out of full fibre services, which should be music to the ears of Boris Johnson’s government who appear to be putting more focus on such a vision to drive economic growth for  the country in a post-Brexit world. Along with a change in personnel at the regulator Ofgem, relations should be thawing for BT and hopefully Philip has enough bargaining power that increased rates will be rewarded in return for an accelerated roll out of fibre.</p>
+		<p>Although earlier this month Bank of America Merrill Lynch suggested a 40% cut in the dividend was likely this may not be such a bad thing if and when it does happen. The dividend would still be a not unreasonable 5.4% and the extra cash would allow BT to accelerate the roll out of full fibre and so provide the modern network required to drive revenues and financial growth in the coming years. It will also finally put an end to market speculation around such a cut and so the market can move on to future returns that can be driven from forthcoming capital expenditure.</p>
+		<p>The pressures from the costs of football rights costs should hopefully ease now that there is the reciprocal deal with Sky to provide each other’s matches. Under the new CEO’s tenure there also appears to be shift away from this expensive distraction towards a focus on BT’s core strengths as an infrastructure company.</p>
+		<p>The Global Services division has also been pruned and refocused to higher margin services and so hopefully providing a sounder footing for growth for this segment of the Group.</p>
+		<p>Philip also now has a lot of skin in the game owing £3.6million of BT shares (having bought a further £1mil worth on 16th September), quite a significant purchase from a director, and showing his faith in the future prospect of the group.</p>
+		<p>With a forecast PE ratio of just over 6 the market appears to have already factored in an excessive worst case scenario of costs and cashflow restrictions and does not give any value to the potential future growth in returns from an accelerated roll out of the full fibre service and those of other of the other restructuring activities being enacted . Surely at such a lowly valuation the risk is now to the upside and any positive news and signs that Philip’s plans are starting to deliver should initiate a return to a growth in the share price. Therefore I think on balance, the company’s shares are a “Buy”.</p>
+		<br>
+	</div>
+
+	<div class="col_4 last" style="background-color:#fff">
+			
+		<div>
+			<p>Latest ETF price:</p>
+		</div>
+	
+		<div class="chart_container">
+			<div class="chart_chart">
+<!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container">
+  <div id="tv-medium-widget"></div>
+  <div class="tradingview-widget-copyright"><a href="https://uk.tradingview.com/symbols/LSE-HTRY/" rel="noopener" target="_blank"><span class="blue-text">HTRY Quotes</span></a> by TradingView</div>
+  <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+  <script type="text/javascript">
+  new TradingView.MediumWidget(
+  {
+  "container_id": "tv-medium-widget",
+  "symbols": [
+    "LSE:HTRY|60m"
+  ],
+  "greyText": "Quotes by",
+  "gridLineColor": "#e9e9ea",
+  "fontColor": "#83888D",
+  "underLineColor": "#dbeffb",
+  "trendLineColor": "#4bafe9",
+  "width": "100%",
+  "height": "100%",
+  "locale": "uk"
+}
+  );
+  </script>
+</div>
+<!-- TradingView Widget END -->
+			</div>
+		</div>
+	
+		<div>
+			<a href="https://www.evolving.money/EMoney/EBanking/challenger-comp.php"><div class="imgbox">
+				<img class="center-fit" src="../Images/Comp_chall_banks.png", alt="bank cards"></div></a>
+		
+		</div>
+	
+	</div>
+	
+	<div class="clear"></div>
+	
+	<div class="col_8 first" style="background-color:#8a8a8a" >
+<p>How to trade:<br><br>The best and cheapest way to play a short term market opportunity is via an ETF, which are usually passively managed and so have low management fees. As they are traded on stock exchange they usually also have a smaller spread when buying or selling than standard unit trust fund. It is best to make sure a reputable provider is choosen also to ensure counter party risk is minimised, i.e. risk that they will go bust is minimised. Our favourrite way to play the Turkish market is the HSBC's ETF "HSBC ETFs plc MSCI Turkey (HTRY)" which attempts to replciate the performance of the MSCI Turkey Index and charges a management fee of only 0.6%. MSCI, once part of Morgan Stanley, have provided global equity indexes since 1969 are a widely used benchmarks for various markets. This ETF attempts to track the index through physcialreplication, in other words it directly holds the securties that make up the index, thismethod helps reduce risks compared synthetic replication where there is increased counter party risk due to synthetic replication involving derviatives with other parties. <br><br>You should be able to buy this ETF through most online brokers in a similar manner to a normal company share, just search for the ticker HTRY on your broker's trading screen.   
+</p>
+	</div>
+	
+	<div class="clear"></div>
+</div>
+	
+<?php include("../includes/footer.html");?>
+
+</body>
+</html> 
