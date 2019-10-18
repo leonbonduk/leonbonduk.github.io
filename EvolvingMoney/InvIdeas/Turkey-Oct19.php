c7eb27489f008383ebbd92946b6fe78b0bd3bd31
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-10-18 01:09:03 +0100

    Update Turkey-Oct19.php

--- EvolvingMoney/InvIdeas/Turkey-Oct19.php
+++ EvolvingMoney/InvIdeas/Turkey-Oct19.php
@@ -22,27 +22,14 @@
 	<div class="col_8 first" style="background-color:#fff">	
 		<br>	 
 		<h1 class="h1_article">Turkish Delight</h1>
-		<h3>Leon Bond, 17th October 2019, Price £1.67</h3>
-		<p><img src="https://www.evolving.money/Images/turkey-flag-wrinkled.jpg" alt="Wrinkled Turkish Flag" title="Turkish Delight" style="width:300px;height:auto;float: left; margin: 1%; padding: 1%;"/> </p>
-		<p>With the share price now at 167p, this is significantly down from the heady heights of 500p back in 2015, that’s almost 67% down from it’s peak and 30% down for the year to date. With a forecast PE ratio of 6 (it was around 16 back in 2015!) and a dividend yield of 9% could this now be an unmissable buying opportunity?.</p> 
-		<h2>What went wrong?</h2>
-		<p>After a dramatic rise in the share price in the years up to 2015, from the lows after the last financial crisis, the price has fallen significantly since the EE acquisition back in January 2016. EE has been a relative bright spot in BT’s results since acquisition but other various headwind pressures have battered the share price including:</p>
-		<ul class="Glossary">
-			<li>A fall in revenues and earnings driven by regulated prices at Openreach, a fall in voice revenues as VOIP grows more popular, as well as falls in the international Global Services division.</li>
-			<li>An accounting scandal in the Italian division resulted in a charge of £530mil early 2017.</li>
-			<li>Cashflow pressures to cover capital expenditure needs for the roll out of full fibre, 5G and significant pension liabilities on the historic defined contribution schemes all threaten the perceived ability of the company to continue to pay it’s dividend at the same levels.</li>
-			<li>Growth in debt to fund the pension top-up payments.</li>
-			<li>Potential significant costs for new 5G spectrum and football rights.</li>
-			<li>A rocky relationship with the regulator along with little clarity as to whether Openreach should be spun out as separate company or not.</li>
-		</ul>	
-		<h2>The way ahead</h2>
-		<p>BT’s revenue’s and earnings are forecast to still fall slightly in the coming year but the hire of the new CEO, Philip Jansen, at the start of the year and his reputation for restructuring should hopefully start to bear fruit in the next few years. He has been doing the rounds of government departments in recent weeks plugging an accelerated roll-out of full fibre services, which should be music to the ears of Boris Johnson’s government who appear to be putting more focus on such a vision to drive economic growth for  the country in a post-Brexit world. Along with a change in personnel at the regulator Ofgem, relations should be thawing for BT and hopefully Philip has enough bargaining power that increased rates will be rewarded in return for an accelerated roll out of fibre.</p>
-		<p>Although earlier this month Bank of America Merrill Lynch suggested a 40% cut in the dividend was likely this may not be such a bad thing if and when it does happen. The dividend would still be a not unreasonable 5.4% and the extra cash would allow BT to accelerate the roll out of full fibre and so provide the modern network required to drive revenues and financial growth in the coming years. It will also finally put an end to market speculation around such a cut and so the market can move on to future returns that can be driven from forthcoming capital expenditure.</p>
-		<p>The pressures from the costs of football rights costs should hopefully ease now that there is the reciprocal deal with Sky to provide each other’s matches. Under the new CEO’s tenure there also appears to be shift away from this expensive distraction towards a focus on BT’s core strengths as an infrastructure company.</p>
-		<p>The Global Services division has also been pruned and refocused to higher margin services and so hopefully providing a sounder footing for growth for this segment of the Group.</p>
-		<p>Philip also now has a lot of skin in the game owing £3.6million of BT shares (having bought a further £1mil worth on 16th September), quite a significant purchase from a director, and showing his faith in the future prospect of the group.</p>
-		<p>With a forecast PE ratio of just over 6 the market appears to have already factored in an excessive worst case scenario of costs and cashflow restrictions and does not give any value to the potential future growth in returns from an accelerated roll out of the full fibre service and those of other of the other restructuring activities being enacted . Surely at such a lowly valuation the risk is now to the upside and any positive news and signs that Philip’s plans are starting to deliver should initiate a return to a growth in the share price. Therefore I think on balance, the company’s shares are a “Buy”.</p>
-		<br>
+		<h3>Leon Bond, 17th October 2019, HSBC ETFs plc MSCI Turkey (HTRY), Price £1.69</h3>
+		<p>Since Turkey's incursion into Syria a few days ago  Turkey's stock market has fallen to fresh lows with the MSCI Turky index down 18% since 30th September! It remains 13% higher than the lows seen in May this year however.</p> 
+		<p>With a ceasefie now negotiated by Trump as of today, and the Kurds having allowed the Syrian government forces up to protect the border the markets could now drift back up to their annual highs since just a couple of weeks ago.</p>
+		<h2>Other market details</h2>
+		<p>Turkey's market was already very cheap before this retracement, due in most part to the political turmoil of the Turkish president's regime, President Erdoğan. The Turkish MSCI index was following simlar market trends to the general MSCI EMerging Markets Index from 2004 right up to 2016. Since shortly before the attempted coup in 2016, however, the Turkish market has diverged considerably from the rest of the emerging markets, diverging by approx 50%.</p>
+		<p>At 30th September the MSCI Turkey index had a P/E ratio of 8.2 and a forward P/E ratio of 6.3! This compares to the MSCI EMerging Markets index which had P/E ratio of 13.2 and a forward P/E ratio of 11.9. Therefore the Turkish market is trading at apprximately half the value of other emrging markets...</p>
+		<h2>High risk opportunity</h2>
+		<p>Although this is obviously a high risk play given the current Syrian situation, the market valuations provides some comfort that even if short term bet does not pan out, if you can hold for longer term then this should at least return to slightly more sensible valuations although coming fully back in line with the other emerging markets is unlikely for the foreseeable future until the internal political situation in Turkey settles down also.</p>
 	</div>
 
 	<div class="col_4 last" style="background-color:#fff">
@@ -63,7 +50,7 @@
   {
   "container_id": "tv-medium-widget",
   "symbols": [
-    "LSE:HTRY|60m"
+    "LSE:HTRY|12m"
   ],
   "greyText": "Quotes by",
   "gridLineColor": "#e9e9ea",
@@ -92,7 +79,7 @@
 	<div class="clear"></div>
 	
 	<div class="col_8 first" style="background-color:#8a8a8a" >
-<p>How to trade:<br><br>The best and cheapest way to play a short term market opportunity is via an ETF, which are usually passively managed and so have low management fees. As they are traded on stock exchange they usually also have a smaller spread when buying or selling than standard unit trust fund. It is best to make sure a reputable provider is choosen also to ensure counter party risk is minimised, i.e. risk that they will go bust is minimised. Our favourrite way to play the Turkish market is the HSBC's ETF "HSBC ETFs plc MSCI Turkey (HTRY)" which attempts to replciate the performance of the MSCI Turkey Index and charges a management fee of only 0.6%. MSCI, once part of Morgan Stanley, have provided global equity indexes since 1969 are a widely used benchmarks for various markets. This ETF attempts to track the index through physcialreplication, in other words it directly holds the securties that make up the index, thismethod helps reduce risks compared synthetic replication where there is increased counter party risk due to synthetic replication involving derviatives with other parties. <br><br>You should be able to buy this ETF through most online brokers in a similar manner to a normal company share, just search for the ticker HTRY on your broker's trading screen.   
+<p>How to trade:<br><br>The best and cheapest way to play a short term market opportunity like this is via an ETF, which are usually passively managed and so have low management fees. As they are traded on a stock exchange they usually also have a smaller spread when buying or selling compared to a standard unit trust fund. It is also best to make sure a reputable provider is choosen to ensure counter party risk is minimised, i.e. risk that they will go bust is minimised. Our favourite way to play the Turkish market is through HSBC's ETF "HSBC ETFs plc MSCI Turkey (HTRY)" which attempts to replciate the performance of the MSCI Turkey Index and charges a management fee of only 0.6%.<br><br>MSCI, once part of Morgan Stanley, have provided global equity indexes since 1969 and are widely used as benchmarks for various markets by financial professionals. This ETF attempts to track the index through physcial replication, in other words it directly holds the securties that make up the index. This method helps reduce risks compared to synthetic replication where there is increased counter party risk due to synthetic replication involving derviatives with other parties. <br><br>You should be able to buy this ETF through most online brokers in a similar manner to a normal company share, just search for the ticker HTRY on your broker's trading screen.   
 </p>
 	</div>
 	
