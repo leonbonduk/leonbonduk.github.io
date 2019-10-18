667a58c95f7911428be9f3477b7ba425acb6166b
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-10-18 01:24:32 +0100

    Update challenger-comp.php

--- EvolvingMoney/EMoney/EBanking/challenger-comp.php
+++ EvolvingMoney/EMoney/EBanking/challenger-comp.php
@@ -10,16 +10,16 @@
 	       	<meta name="robots" content="all">
 		<meta name="viewport" content="width=device-width,initial-scale=1" />
 		<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600' rel='stylesheet' type='text/css'>
-		<link rel="stylesheet" href="../../css/reset.css">  <!-- CSS reset -->
-		<link rel="stylesheet" href="../../css/style-comp-table.css">  <!-- Resource style -->
-		<link href="../../css/styles.css" type="text/css" rel="stylesheet" />
-		<link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" /> !-->
+		<link rel="stylesheet" href="css/reset.css">  <!-- CSS reset -->
+		<link rel="stylesheet" href="css/style-comparison-table.css">  <!-- Resource style -->
+		<link href="css/styles.css" type="text/css" rel="stylesheet" />
+		<!-- <link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" /> !-->
 		<link rel="stylesheet" type="text/css" href="https://evolving.money/css/960-12-col.css" />
 </head>
 
 <body>
 
-	<?php include("../../includes/test-header.html");?>
+	<?php include("includes/test-header.html");?>
 
 <div>
 	<div class="row">
@@ -52,6 +52,7 @@
 
 </div>
 	<section class="cd-intro">
+		
 		<h1>Challenger Bank Comparison Table</h1>
 	</section> <!-- .cd-intro -->
 	<section class="cd-products-comparison-table">
@@ -64,8 +65,7 @@
 
 		<div class="cd-products-table">
 			<div class="features">
-				<div class="top-info">Account</div>
-
+				<div class="top-info">Models</div>
 				<ul class="cd-features-list">
 					<li>Card</li>
 					<li>EM rating</li>
@@ -80,7 +80,7 @@
 					<li>Apple Pay</li>
 					<li class="cd-features-subheading">Security features</li>
 					<li>Transaction notification</li>
-				<li>Virtual cards</li>
+					<li>Virtual cards</li>
 					<li>Freeze card in app</li>
 					<li>Magstripe disable</li>
 					<li>Online 3D secure</li>
@@ -92,39 +92,38 @@
 				</ul>
 			</div> <!-- .features -->
 			
-
 			<div class="cd-products-wrapper">
 				<ul class="cd-products-columns">
 					<li class="product">
 						<div class="top-info">
 							<div class="check"></div>
 							<img class="col-fit-img" src="https://evolving.money/Images/Revolut_card_logo_EM.png" alt="product image">
-							<p class="table_title">Revolut</p>
+							<h3 class="comp_title">Revolut</h3>
 						</div> <!-- .top-info -->
 
 						<ul class="cd-features-list">
 							<li>xxx</li>
-							<li class="rate"><span style="width: 9.9rem;">4.5/5</span></li>
+							<li class="rate"><span>5/5</span></li>
 							<li>2015</li>
 							<li>7mil+</li>
 							<li>£0.00</li>
-							<li class="tick"><span>1/1</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
 							<li>Yes</li>
 							<li>Yes</li>
-							<li>Up to £200pm</li>
+							<li><£200pm</li>
 							<li>Received</li>
 							<li>Yes</li>
 							<li class="cd-features-subheading"><br></li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
-							<li class="tick"><span style="width: 2.0rem;">2/2</span></li>
-							<li class="tick"><span style="width: 2.0rem;">3/3</span></li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
 							<li>TBC</li>
 							<li>TBC</li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
 						</ul>
 					</li> <!-- .product -->
 
@@ -132,32 +131,32 @@
 						<div class="top-info">
 							<div class="check"></div>
 							<img class="col-fit-img" src="https://evolving.money/Images/Revolut_prem_logo_EM.png" alt="product image">
-							<p class="table_title">Revolut Premium</p>
+							<h3 class="comp_title">Revolut Premium</h3>
 						</div> <!-- .top-info -->
 
 						<ul class="cd-features-list">
 							<li>xxx</li>
-							<li class="rate"><span style="width: 9.9rem;">4.5/5</span></li>
+							<li class="rate"><span>5/5</span></li>
 							<li>2015</li>
 							<li>7mil+</li>
 							<li>£6.99</li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
 							<li>Received</li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
 							<li class="cd-features-subheading"><br></li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
 							<li>TBC</li>
 							<li>TBC</li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
 						</ul>
 					</li> <!-- .product -->
 
@@ -165,66 +164,68 @@
 						<div class="top-info">
 							<div class="check"></div>
 							<img class="col-fit-img" src="https://evolving.money/Images/Revolut_metal_logo_EM.png" alt="product image">
-							<p class="table_title">Revolut Metal</p>
+							<h3 class="comp_title">Revolut Metal</h3>
 						</div> <!-- .top-info -->
 
 						<ul class="cd-features-list">
 							<li>xxx</li>
-							<li class="rate"><span style="width: 9.9rem;">4.5/5</span></li>
+							<li class="rate"><span>5/5</span></li>
 							<li>2015</li>
 							<li>7mil+</li>
 							<li>£12.99</li>
 							<li>Yes</li>
 							<li>Yes</li>
 							<li>Yes</li>
-							<li>Up to £600pm</li>
+							<li><£600pm</li>
 							<li>Received</li>
 							<li>Yes</li>
 							<li class="cd-features-subheading"><br></li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
 							<li>TBC</li>
 							<li>TBC</li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
 						</ul>
 					</li> <!-- .product -->
+
 					<li class="product">
 						<div class="top-info">
 							<div class="check"></div>
 							<img class="col-fit-img" src="https://evolving.money/Images/Starling_card_logo_EM.png" alt="product image">
-							<p class="table_title">Starling</p>
+							<h3 class="comp_title">Starling</h3>
 						</div> <!-- .top-info -->
 
 						<ul class="cd-features-list">
 							<li>xxx</li>
-							<li class="rate"><span style="width: 8.8rem;">4/5</span></li>
+							<li class="rate"><span style="width: 99px;">4.5/5</span></li>
 							<li>2015</li>
 							<li>2mil+</li>
 							<li>£0.00</li>
 							<li>Yes</li>
 							<li>Yes</li>
 							<li>Yes</li>
-							<li>Up to £600pm</li>
+							<li><£600pm</li>
 							<li>Received</li>
 							<li>Yes</li>
 							<li class="cd-features-subheading"><br></li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
 							<li>TBC</li>
 							<li>TBC</li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
-							<li class="tick"><span style="width: 2.0rem;">1/1</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
+							<li class="tick"><span style="width: 22px;">Yes</span></li>
 						</ul>
 					</li> <!-- .product -->
+					
 				</ul> <!-- .cd-products-columns -->
 			</div> <!-- .cd-products-wrapper -->
 			
@@ -236,7 +237,8 @@
 	</section> <!-- .cd-products-comparison-table -->
 <script src="https://evolving.money/table-nodejs/jquery-2.1.4.js"></script>
 <script src="https://evolving.money/table-nodejs/main.js"></script> <!-- Resource jQuery -->
-
-<?php include("../../includes/footer.html");?>
+	
+<?php include("includes/footer.html");?>
 
 </body>
+</html>
