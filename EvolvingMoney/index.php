d42013b00f6ef9fc5d05704869eeebf4480cf4f0
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-10-19 22:19:17 +0100

    Update index.php

--- EvolvingMoney/index.php
+++ EvolvingMoney/index.php
@@ -11,10 +11,12 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="robots" content="all" />
 	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<script src="https://evolving.money/table-nodejs/slideshow.js"></script> <!-- SLideshow js -->
 	<link rel="canonical" href="https://www.evolving.money/">
 	<link href="css/styles.css" type="text/css" rel="stylesheet" />
 	<!-- <link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" /> !-->
 	<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/960-12-col.css" />
+	<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/slideshow.css" />
 	
 </head>
 <body>
@@ -24,10 +26,26 @@
 
  		<div class="row">
 			<div class="col_8 first" style="border-bottom-width: 5px; border-bottom-style: solid; border-bottom-color: #274168;">
-				<a href="https://www.evolving.money/EMoney/EBanking/challenger-comp.php">
-					<div class="imgbox"><img class="center-fit" src="Images/Comp_chall_banks.png"></div>
-	
-			</div>
+				 <div class="slideshow-container">
+					 <div class="slideshow-inner">
+              					<div class="mySlides fade">
+                  					<a href="https://www.evolving.money/EMoney/EBanking/challenger-comp.php">
+					            		<div class="imgbox"><img class="center-fit" src="Images/Comp_chall_banks.png"></div>
+              					</div>
+              					<div class="mySlides fade">
+                   					<a href="https://www.evolving.money/InvIdeas/index.php">
+					            		<div class="imgbox"><img class="center-fit" src="Images/investment_ideas.png"></div>
+              					</div>
+              					<a class="prev" onclick='plusSlides(-1)'>&#10094;</a>
+              					<a class="next" onclick='plusSlides(1)'>&#10095;</a>
+         			 	</div>
+        			<br/>
+				<div style='text-align: center;'>
+  					<span class="dot" onclick='currentSlide(1)'></span>
+  					<span class="dot" onclick='currentSlide(2)'></span>
+				</div>
+				</div>
+    			</div>
 			<div class="col_4 last" style="background-color:#fff"><div class="imgbox"><img class="center-fit" src="Images/EM_circ_logo.jpg"></div>
 	
 		</div>
