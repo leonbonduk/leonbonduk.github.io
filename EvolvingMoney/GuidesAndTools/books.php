41c028185aa31d28a1c55c6d8ca3b7e9b0c8cc45
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-11-03 21:48:39 +0000

    Update books.php

--- EvolvingMoney/GuidesAndTools/books.php
+++ EvolvingMoney/GuidesAndTools/books.php
@@ -25,7 +25,7 @@
 	<div class="row" margin 0 0;>
 		<div class="col_12 first" style="background-color:#fff">
 			<div class="imgbox">
-    				<img class="left-fit-page-heading" src="../Images/Media_logo.GIF">
+    				<img class="left-fit-page-heading" src="../Images/Books_logo.gif">
 			</div>
 		</div>
 	</div> 
@@ -34,12 +34,15 @@
  
 	<div class="row">
 		<div class="col_9 first" style="background-color:#fff">
-<p>Here’s a list of some of our favourite books we find or have found useful in developing our knowledge of the world of money and investments: </p>
-<ul>
-<li>
-Coming soon...</li>
-</ul>
-</div>
+			<p>Here’s a list of some of our favourite books we have found useful in developing our knowledge of the world of money and investments: </p>
+			<h2><a target="_blank" href="https://www.amazon.co.uk/gp/product/0857197819/ref=as_li_tl?ie=UTF8&camp=1634&creative=6738&creativeASIN=0857197819&linkCode=as2&tag=evolvingmoney-21&linkId=bf01685b9ebdcce869bde10b5f83ddec">The Naked Trader: How anyone can make money trading shares, 5th edition</a><img src="//ir-uk.amazon-adsystem.com/e/ir?t=evolvingmoney-21&l=am2&o=2&a=0857197819" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" /></h2> 
+				<a target="_blank"  href="https://www.amazon.co.uk/gp/product/0857197819/ref=as_li_tl?ie=UTF8&camp=1634&creative=6738&creativeASIN=0857197819&linkCode=as2&tag=evolvingmoney-21&linkId=a1f377b9f05a68e431669df06fe43ae3"><img border="0" src="//ws-eu.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=GB&ASIN=0857197819&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=evolvingmoney-21" ></a><img src="//ir-uk.amazon-adsystem.com/e/ir?t=evolvingmoney-21&l=am2&o=2&a=0857197819" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
+				<p>Not your average stale investing guide, Robbie Burns puts life and interest into learning various trading strategies. Most of his strategies are largely momentum style plays. And he avoids technical analysis, instead concentrating on market timing, looking at market volumes and current share price proximity to recent highs and lows. He also discusses how he researches the health of companies he buys into.</p>
+				<p>His strategies are explained simply such that someone new to investing can readily follow and replicate without too much trouble. Via his online blog he also provides regular updates on his trades and provides the full history of all his historic trades which gives further insights in to how he is playing current market conditions such that you can hopefully learn further from the almost real-time situations in the market! He does however delay putting his trades up on his website by a couple of weeks, so it is probably not wise to blindly follows his trades in most cases.</p>
+				<p>This book is so easy to read and follow that it will give you the confidence to jump straight in on some of the strategies, unlike a lot of other investing books where in most instances you feel like you are missing a few key details.</p>
+				<br>					
+			<h2> More Coming soon...</h2>
+		</div>
  
 		<div class="col_3 last" style="background-color:#fff">
 			<div class="imgbox"><img class="center-fit" src="https://www.evolving.money/Images/EM_circ_logo.jpg"></div>
