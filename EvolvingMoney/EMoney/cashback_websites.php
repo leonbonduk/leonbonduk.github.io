ac8f098f61e50bb615b8da45bd1bc3b1478d3e13
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-12-27 00:13:34 +0000

    Create cashback_websites.php

---
+++ EvolvingMoney/EMoney/cashback_websites.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>		
+	<meta charset="UTF-8">
+       	<title>Cashback Websites</title>	
+ 	<meta name="description" content="Cashback websites" />
+   	<meta name="keywords" content="Cashback, Quidco, TopCashback" />
+       	<meta name="author" content="Leon Bond" />
+	<meta name="robots" content="all">
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<link href="../css/styles.css" type="text/css" rel="stylesheet" />
+	<!-- <link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" /> !-->
+	<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/960-12-col.css" />
+
+</head>
+
+<body>
+	<?php include("../includes/test-header.html");?>
+	
+<div class="row">
+
+	<div class="col_8 first" style="background-color:#fff">	
+		<br>	 
+		<h1 class="h1_article">Cashback Websites</h1>
+		<h3>Leon Bond, 27th December 2019</h3>
+		<p><img src="https://www.evolving.money/Images/BT_up.jpeg" alt="BT Tower" title="Is BT low share price a contrarian buy?" style="width:300px;height:400px;float: left; margin: 1%; padding: 1%;"/> </p>
+		<p>xxxxxx</p> 
+		<h2>What xxx?</h2>
+		<p>xxx</p>
+		<h2>Links</h2>
+		<ul>
+      <li>    <a href="https://www.topcashback.co.uk/ref/leonbond"</a> </li>
+      <li>    <a href="https://www.quidco.com/raf/9781321/"</a> </li>
+    </ul>
+    
+		<br>
+	</div>
+
+	<div class="col_4 last" style="background-color:#fff">
+			
+		
+	
+		<div>
+			<a href="https://www.evolving.money/EMoney/EBanking/challenger-comp.php"><div class="imgbox">
+				<img class="center-fit" src="../Images/Comp_chall_banks.png", alt="bank cards"></div></a>
+		
+		</div>
+	
+	</div>
+	
+	<div class="clear"></div>
+	
+
+	<div class="clear"></div>
+</div>
+	
+<?php include("../includes/footer.html");?>
+
+</body>
+</html> 
