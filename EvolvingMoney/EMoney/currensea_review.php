3c5da95267f27680582e0e5c137bf70e6ba0d479
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2019-12-28 21:28:21 +0000

    Create currensea_review.php

---
+++ EvolvingMoney/EMoney/currensea_review.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>		
+	<meta charset="UTF-8">
+       	<title>Seemless low FX rates with Currensea</title>	
+ 	<meta name="description" content="Get seemless low fx rates with Currensea on online and overseas transactions. No need to prefund like challenger banks, draws funds directly from standard bank account." />
+   	<meta name="keywords" content="Currensea, fx ,low fx rates, travel, overseas fx, foreign exchange" />
+       	<meta name="author" content="Leon Bond" />
+	<meta name="robots" content="all">
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<link href="../css/styles.css" type="text/css" rel="stylesheet" />
+	<!-- <link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" /> !-->
+	<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/960-12-col.css" />
+
+</head>
+
+<body>
+	<?php include("../includes/test-header.html");?>
+	
+<div class="row">
+
+	<div class="col_8 first" style="background-color:#fff">	
+		
+	</div>
+
+	<div class="col_4 last" style="background-color:#fff">
+	
+		<div>
+			<a href="https://www.evolving.money/EMoney/EBanking/challenger-comp.php"><div class="imgbox">
+				<img class="center-fit" src="../Images/Comp_chall_banks.png", alt="bank cards"></div></a>
+		
+		</div>
+	
+	</div>
+	
+	<div class="clear"></div>
+	
+
+	<div class="clear"></div>
+</div>
+	
+<?php include("../includes/footer.html");?>
+
+</body>
+</html> 
