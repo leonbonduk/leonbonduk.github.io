c60bc488dbea5b5e96ffb763d85cef329c333e44
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2020-01-05 21:54:25 +0000

    Update OpenSourceHedgeFund.php

--- EvolvingMoney/EMoney/OpenSourceHedgeFund.php
+++ EvolvingMoney/EMoney/OpenSourceHedgeFund.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
        	<title>Open Source Hedge Funds</title>	
  	<meta name="description" content="summary." />
-   	<meta name="keywords" content="eMoney, technical analysis, paymenst systems, fintech" />
+   	<meta name="keywords" content="Numerai, Open Source Hedge Fund, eMoney, technical analysis, payments systems, fintech" />
        	<meta name="author" content="James Day" />
 	<meta name="robots" content="all">
 	<meta name="viewport" content="width=device-width,initial-scale=1" />
@@ -23,31 +23,24 @@
 		<br>	 
 		<h1 class="h1_article">Open Source Hedge Funds!</h1>
 		<h3>James Day, 5th January 2020</h3>
-		<h2> <strong>NumerAi</strong> the open-source hedge fund  </h2>
-
-                <p> Open source is a term we here from software, where the source code is freely available, but what does this have to do with hedge funds? <p>
-
-                <p> In hedge funds quants generate models for technical analysis of stock prices and these models, (algorithms) are what tend to do the larger proprtion of the trading.
-                    These algorithms are kept closely guarded, and are not something a normal hedge fund would share, but in the case of NumerAI that it is precisily these tools
+		<p><img src="https://www.evolving.money/Images/numerai.jpg" alt="Numerai hexagonal logo at top of three skyscrapers" title="Numerai" style="width:300px;height:auto;float: left; margin: 1%; padding: 1%;"/> </p>
+		<h2> <strong>NumerAi</strong> the open-source hedge fund </h2>
+                <p> Open source is a term we hear from software, where the source code is freely available, but what does this have to do with hedge funds? <p>
+                <p> In quant hedge funds algorithms generate models for technical analysis of stock prices and these models. Quants tend to do the larger proportion of the trading rather than a human trader.
+                    These algorithms are kept closely guarded, and are not something a normal hedge fund would share, but in the case of NumerAI that it is precisely these tools
                     that are shared. <p>		 
-
-		<p> The reason that  <strong>NumerAi</strong> do this is to take advanatage of the network effects of having many contributors providing algorithms in a competition format. 
-                    The crowd generated algorithms they end up with, are derived by much larger teams than any hedge fund could afford on their own,  and from a comminity of highly skilled 
+		<p> The reason that  <strong>NumerAi</strong> do this is to take advantage of the network effects of having many contributors providing algorithms in a competition format. 
+                    The crowd generated algorithms they end up with, are derived by much larger teams than any hedge fund could afford on their own,  and from a community of highly skilled 
                     data scientists. Traditional algorithms used for trading were predominantly statistical models used for price prediction, but the <strong>NumerAi</strong> community  
-                    make use of the latest tools in data science an machine learning, (an area that Hedge funds are only just breaking into). <p>
-
+                    make use of the latest tools in data science and machine learning, an area that Hedge funds are only just breaking into.<p>
                 <p> The <strong>NumerAi</strong> team have incentivised the crowd to work for them in a competion based market place, and contributors compete to provide better algorithms. 
                     This costs a lot less money than hiring people to do it. Pay is in a competition format so effectively they only have to pay the winners. Also their workforce 
-                    pay to be involved, and effectively crowd fund the hedge fund. It's a very creative solution that harnesses technology and the crowd together. <p>
-
-                <p> It's a bit like <strong>Wikipedia</strong> for technical analysis and stock market prediction models, combined with <strong>Crowdfunder</strong>...  Inspired. <p>
-
-                <p> In the background it uses Cryptocurrency, Blockchain and Smart Contracts, to automatically pay everyone providing models, and to compute rankings and performance. 
-                    Contributors can make a lot of money if they are good. <p>
-
-                <p> For me this is an example of things to come, the rise in importance of algorithms and AI / automation has been much discussed. A much more important trend potentially
+                    pay to be involved, and effectively crowd fund the hedge fund. It's a very creative solution that harnesses technology and the crowd together.<p>
+                <p> It's a bit like <strong>Wikipedia</strong> for technical analysis and stock market prediction models, combined with <strong>Crowdfunder</strong>...  Inspired.<p>
+                <p> In the background it uses Cryptocurrency, Blockchain and Smart Contracts to automatically pay everyone providing models, and to compute rankings and performance. Contributors can make a lot of money if they are good.<p>
+                <p> For me this is an example of things to come, the rise in importance of algorithms and AI / automation has been much discussed. A much more important trend potentially,
                     at least in the short term, is likely to be the contributions of teams of people and computer algorithms working together... much discussed in the book <a class="link" target="_blank" href="https://www.amazon.co.uk/gp/product/1786074702/ref=as_li_tl?ie=UTF8&camp=1634&creative=6738&creativeASIN=1786074702&linkCode=as2&tag=evolvingmoney-21&linkId=0980827271bfdad9d3f2c5dc2665cb82">Superminds</a><img src="//ir-uk.amazon-adsystem.com/e/ir?t=evolvingmoney-21&l=am2&o=2&a=1786074702" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" /> 
-                    by Thomas W Malone. Numerai is precisely an example of this in action in FinTech. <p>
+                    by Thomas W Malone. Numerai is precisely an example of this in action in FinTech.<p>
 	</div>
 
 	<div class="col_4 last" style="background-color:#fff">
