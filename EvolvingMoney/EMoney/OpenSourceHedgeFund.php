5c3b82814613d2eff7b326e8a363f94b14e9d862
Jibber2000 <32431276+Jibber2000@users.noreply.github.com>
2020-01-05 19:55:17 +0000

    Create OpenSourceHedgeFund.php

---
+++ EvolvingMoney/EMoney/OpenSourceHedgeFund.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>		
+	<meta charset="UTF-8">
+       	<title>Cashflow from cashback websites</title>	
+ 	<meta name="description" content="Get Cashback on online purchases from websites and apps provided by Topcashback and Quidco. This is an Evolving Money guide to the best cashback sites and apps and details their downsides." />
+   	<meta name="keywords" content="Cashback, Quidco, TopCashback, Cashback, Cashback cards, Cashback sites, Online shopping, shopping" />
+       	<meta name="author" content="Leon Bond" />
+	<meta name="robots" content="all">
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<link href="../css/styles.css" type="text/css" rel="stylesheet" />
+	<!-- <link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" /> !-->
+	<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/960-12-col.css" />
+
+</head>
+
+<body>
+	<?php include("../includes/test-header.html");?>
+	
+<div class="row">
+
+	<div class="col_8 first" style="background-color:#fff">	
+		<br>	 
+		<h1 class="h1_article">Cashflow from cashback websites!</h1>
+		<h3>Leon Bond, 27th December 2019</h3>
+		<p><img src="https://www.evolving.money/Images/spilled_cash_EM.jpg" alt="Coins spilling out of a jar" title="Cashback websites" style="width:300px;height:auto;float: left; margin: 1%; padding: 1%;"/> </p>
+		<p>Cashback websites and their associated smartphone apps are a great way at earning money back from your online purchases! On clicking though to the retailer from either the cashback app or website, cookies track the transaction and usually a few weeks after the transaction has completed you will receive some cash back that can typically range from 1% up to 15% and sometimes even more.</p>
+		<p>If you also use a cashback credit card then you can in effect double dip to maximise cash coming back to you on your purchases.</p>
+		<h2>How do the cashback websites work</h2>
+		<p>Within the cashback website or app you can search through thousands of retailers and providers of financial products either searching by category or for a specific retailer. Once you find an offer you click on the link and are taken directly to the sellers website and purchase as normal without having to enter any referral codes. The transaction is usually seamlessly tracked in the background through the use of cookies and a few weeks later you will be able withdraw the cash as either cash into your bank account or even get a gift voucher to spend at a retailer with an inflated pay out.</p>
+		<p>Some offers are just for new customers but most allow returning customers also so you can quite quickly amass considerable cashback, particularly with any significant holiday costs (Booking.com currently pay 4% at <a class="link" href="https://www.topcashback.co.uk/ref/leonbond">TopCashback</a>) or change in mobile phone providers (Vodafone give up to £140 cash back on plans from £23 per month at <a class="link" href="https://www.quidco.com/raf/9781321/">Quidco</a>).</p>
+		<p>Most other more “day-to-day” transactions are also covered such as:</p>
+		<ul class="Glossary">
+			<li>eBay: 2.4% at <a class="link" href="https://www.quidco.com/raf/9781321/">Quidco</a> and special offer of up to 5% at <a class="link" href="https://www.topcashback.co.uk/ref/leonbond">TopCashback</a> for a limited time</li>
+			<li>Amazon:  2 to 10% at both <a class="link" href="https://www.quidco.com/raf/9781321/">Quidco</a> and <a class="link" href="https://www.topcashback.co.uk/ref/leonbond">TopCashback</a></li>
+			<li>Sainsburys online order: £1.20 at both <a class="link" href="https://www.quidco.com/raf/9781321/">Quidco</a> and <a class="link" href="https://www.topcashback.co.uk/ref/leonbond">TopCashback</a></li>
+			<li>Domino’s Pizza: £1.20 at <a class="link" href="https://www.topcashback.co.uk/ref/leonbond">TopCashback</a></li>
+		</ul>
+		<p>The two largest players in the game are TopCashback and Quidco but there are many other smaller companies who provide similar services. Both allow you to sign up with no upfront fees, although both also provide premium accounts which provide additional benefits such as boosted levels of cashback for a small fee.</p>
+		<h2>What’s in it for the cashback sites?</h2>
+		<p>The cashback sites are effectively passing on referral or affiliate fees provided by the retailers, with TopCashback and Quidco making a name for themselves for passing on the most, with claims that 100% is typically passed on. Due to their size, however, and so the significant business they put retailers way they are also paid bonuses by the retailers and so this is where they make their money.</p>
+		<h2>Quidco Vs TopCashback</h2>
+		<p>Quidco made it big slightly ahead of TopCashback and pays similar levels of cashback to it’s competitor. They both quite often have exclusive deals with retailers so it can be best to check both out to see who is offering the best rate at the time, particularly on larger transactions. Quidco does have a “price guarantee” however but if you lead a busy life then you probably won’t have time to pull them up on it…</p>
+		<p>TopCashback claim to pay out 105% of commissions generated as they pay out an element of their bonus as well so on average slightly better rates can be achieved with this site over Quidco.</p>
+		<p>They both provide a basic membership for free as well as an upgraded membership, so you can try them out for no cost, but if you do a lot of online shopping then ultimately it will likely be worthwhile to upgrade to a premium account.</p>
+		<h2>Downsides</h2>
+		<p>Sometimes transactions do not track through to payout for various reasons such as:</p>
+		<ul class="Glossary">
+			<li>If you visit the retailer through other links, then other referral cookies can be saved to your computer and so if the cashback website cookie is not the winning cookie then the cashback will be declined.</li>
+			<li>If you purchase on someone else’s online account at the retailer rather than in your own name then this can also cause issues.</li>
+		</ul>
+		<p>Most retailers provide cashback to returning customers, albeit usually at reduced rates, but some provide only for first transaction of a new customer.</p>
+		<p>Although there are thousands of retailers signed up, it may be cheaper to buy at another retailer who do not provide cashback so still ensure you shop around for the best deal. Don’t rely on the cashback paying out as a deciding factor as on rare occasions it can fail with minimal ability to chase down any rights you may have to that cashback. The cashback sites do provide facilities to dispute any unreceived cashback but unfortunately this mechanism does not always result in a resolution in your favour. The cashback website will also be out of pocket for non-payment as well so it is in their interests totry and resolve if they can.</p>
+		<h2>Sign-up bonus</h2>
+		<p>If you use my referral links below rather than signing up directly then you will also get an additional sign-up cash bonus (and I will get similar sized bonus to help pay for my musings on this website). I have already received over £600 of cashback from my own spending over last few years without religiously using the apps so hard to justify not using the apps:</p>
+		<ul class="Glossary">
+			<li>£10.00 sign up bonus for both of us if you earn £5 of other cashback at <a class="link" href="https://www.quidco.com/raf/9781321/">Quidco</a></li>
+			<li>£7.50 sign up bonus for both of us if you earn £10 of other cashback at <a class="link" href="https://www.topcashback.co.uk/ref/leonbond">TopCashback</a></li>
+		</ul>
+	</div>
+
+	<div class="col_4 last" style="background-color:#fff">
+	
+		<div>
+			<a href="https://www.evolving.money/EMoney/EBanking/challenger-comp.php"><div class="imgbox">
+				<img class="center-fit" src="../Images/Comp_chall_banks.png", alt="bank cards"></div></a>
+		
+		</div>
+	
+	</div>
+	
+	<div class="clear"></div>
+	
+
+	<div class="clear"></div>
+</div>
+	
+<?php include("../includes/footer.html");?>
+
+</body>
+</html> 
