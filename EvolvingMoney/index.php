349f90307fd32f5bb9485875f4b8b03cb7e22f47
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2020-01-06 22:40:17 +0000

    Update index.php

--- EvolvingMoney/index.php
+++ EvolvingMoney/index.php
@@ -31,6 +31,11 @@
 				      <div class="slideshow-container">
 					 	<div class="slideshow-inner">
               						<div class="mySlides fade">
+                  						<a href="https://www.evolving.money/InvIdeas/FTFC.php"> 
+					            		<div class="imgbox"><img class="center-fit" src="Images/Free_Trading.png"></div>
+
+              						</div>
+							<div class="mySlides fade">
                   					<!-- <a href="https://www.evolving.money/EMoney/EBanking/challenger-comp.php"> !-->
 								<a href="https://www.evolving.money/EMoney/EBanking/challenger-comp.php"> 
 					            		<div class="imgbox"><img class="center-fit" src="Images/Comp_chall_banks.png"></div>
