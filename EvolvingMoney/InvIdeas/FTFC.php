e9a2641eb0f3c3ef4853196a5992b74293acfd03
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2020-01-06 23:02:31 +0000

    Update FTFC.php

--- EvolvingMoney/InvIdeas/FTFC.php
+++ EvolvingMoney/InvIdeas/FTFC.php
@@ -22,8 +22,8 @@
 	<div class="col_8 first" style="background-color:#fff">	
 		<br>	 
 		<h1 class="h1_article">Free Trading WIth Free Cash!</h1>
-		<h3>Leon Bond, 18th October 2019, HSBC ETFs plc MSCI Turkey (HTRY), Price £1.69</h3>
-		<p><img src="https://www.evolving.money/Images/turkey-flag-wrinkled.jpg" alt="Wrinkled Turkish Flag" title="Turkish Delight" style="width:300px;height:auto;float: left; margin: 1%; padding: 1%;"/> </p>
+		<h3>Leon Bond, 6th January 2020</h3>
+		<p><img src="https://www.evolving.money/Images/Free_Trading_small.png" alt="Business man watering plant sprouting cash notes" title="Money does grow on trees" style="width:300px;height:auto;float: left; margin: 1%; padding: 1%;"/> </p>
 		<p>Join us on a journey of growing free cash from cashback sites and sign up bonuses on the stock market using fee free trading apps to maximise growth rates!</p> 
 
 	</div>
