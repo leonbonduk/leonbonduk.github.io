8f0ad9b963d6a29c5afd8fc6b9b2921986967d8e
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2020-01-06 01:26:33 +0000

    Update currensea_review.php

--- EvolvingMoney/EMoney/currensea_review.php
+++ EvolvingMoney/EMoney/currensea_review.php
@@ -24,7 +24,29 @@
 		<h1 class="h1_article">Seemless low FX rates with Currensea</h1>
 		<h3>Leon Bond, 5th January 2020</h3>
 		<p><img src="https://www.evolving.money/Images/Currensea.JPG" alt="Currensea debit card in circular flow between Barclays and currency sea" title="Currensea" style="width:300px;height:auto;float: left; margin: 1%; padding: 1%;"/> </p>
-		<p>Currensea...</p>
+		<p>With a plethora of app based challenger banks now doing the rounds with most offering ultra-low fx rates as one  of their main selling points versus the incumbent banks such as Barclays, HSBC, Lloyds etc on first glance it may not be apparent what the point is of new comer Currensea.</p>
+		<h2>Currensea’s offering</h2>
+		<p>Currensea does not try to offer a full replacement bank account like the other offerings. A lot of people dabble with the new challenger banks but do not fully adopt them as their main bank account. If the main reason for opening a challenger bank account is to get access to the improved fx rates and charges then it can be a bit of a phaff to top up the prepaid cards ahead of using the card on an overseas transaction, firstly having to work out approximately how much sterling equivalent will be necessary and then having to set up a bank transfer. After the transaction has completed residual cash will be left sitting on the account otherwise you will have to action a further transfer of the residual bank to your main bank account!</p>
+		<p>Currensea removes the need to top up first and instead links almost seamlessly with your bank account and instead deducts the payment as a direct debit afterwards so cutting out a lot of the hassle. They are able to do this due to the open banking regulations, so are able to access and view the balance on your bank account and so know you are good for your money ahead of the transaction. It then in effect works like an add-on to your main bank, just whip out the account for either online transactions or overseas at a retailer or cash machine and a couple of days later the payment is taken out of your bank account.</p>
+		<h2>Rates and charges</h2>
+		<p>Currensea have just come out of Beta testing and have changed their rates so that they are now competitive alongside the Challenger banks not just compared to the incumbant banks:</p>
+		<ul class="Glossary">
+			<li>Currensea: 0.5% fee on all currencies above interbank rate</li>
+			<li>Starling: 0.4% on all currencies above interbank rate</li>
+			<li>Revolut: 0% with foreign exchange desk trading hours otherwise 0.5% outside those hours</li>
+			<li>Barclays, Lloyds, HSBC etc: 2.75%+ above interbank rate</li>
+		</ul>
+		<p>Currensea charge no fee for using the card in an overseas cash machine. Most of the incumbent banks don’t charge additional fees but the challenger banks often only provide free cash withdrawal up to a certain level, with Revolut and Monese charging 2% above withdrawal’s over £200 in a month. Starling, however, has no limit on amount of cash that can withdrawn overseas.</p>
+		<p>Otherwise if Currensea is used only for foreign exchange transactions there are no other notable fees and charges.</p>
+		<h2>Is it worth it?</h2>
+		<p>So overall:</p>
+		<ul class="Glossary">
+			<li>Starling is a little cheaper than Currensea with rates 0.1% lower but then if you are not using Starling as your main bank account there is a lot more hassle on funding the account and dealing with residual funds.</li>
+			<li>Revolut can be cheaper if the transaction is within trading desk hours or if only withdrawing less than £200 from a cash machine in a month. Once you withdraw more than £200 then Currensea is significantly cheaper. Again, there is the hassle of funding the account if this is not your main account.</li>
+			<li>With typical savings of 85% over the charges and rates of Barclays, Lloyds, HSBC etc it is just a no brainer that you would not use these for overseas transactions unless you absolutely had to!</li>
+		</ul>
+		<p>It’s therefore a toss up as to what means most to you… Go for Currensea if you want substantially cheaper rates over your incumbent bank’s rates but prefer less hassle. Otherwise go for Starling for slightly better rates or Revolut for potentially zero fees (depending on the timing), but don’t mind the additional hassle of funding the account. Unless you already regularly use a challenger bank, in which case Currensea may not offer much to you except if you need to take advantage of the additional ability to withdraw fee free from a cash machine.</p>
+
 	</div>
 
 	<div class="col_4 last" style="background-color:#fff">
