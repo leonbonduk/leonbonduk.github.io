8f3b72d6c8d64a57ce96720a794ebe8f28f90d32
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2020-01-06 22:37:22 +0000

    Create FTFC.php

---
+++ EvolvingMoney/InvIdeas/FTFC.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>		
+	<meta charset="UTF-8">
+       	<title>Free Trading With Free Cash</title>	
+ 	<meta name="description" content="Join us on a journey of growing free cash from cashback sites and sign up bonuses on the stock market using fee free trading apps to maximise growth rates!" />
+   	<meta name="keywords" content="Free cash, free trading, free shares, Freetrade, free investing, money for nothing" />
+       	<meta name="author" content="Leon Bond" />
+	<meta name="robots" content="all">
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<link href="../css/styles.css" type="text/css" rel="stylesheet" />
+	<!-- <link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" /> !-->
+	<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/960-12-col.css" />
+
+</head>
+
+<body>
+	<?php include("../includes/test-header.html");?>
+	
+<div class="row">
+
+	<div class="col_8 first" style="background-color:#fff">	
+		<br>	 
+		<h1 class="h1_article">Free Trading WIth Free Cash!</h1>
+		<h3>Leon Bond, 18th October 2019, HSBC ETFs plc MSCI Turkey (HTRY), Price £1.69</h3>
+		<p><img src="https://www.evolving.money/Images/turkey-flag-wrinkled.jpg" alt="Wrinkled Turkish Flag" title="Turkish Delight" style="width:300px;height:auto;float: left; margin: 1%; padding: 1%;"/> </p>
+		<p>Join us on a journey of growing free cash from cashback sites and sign up bonuses on the stock market using fee free trading apps to maximise growth rates!</p> 
+
+	</div>
+
+	<div class="col_4 last" style="background-color:#fff">
+	
+		<div>
+			<a href="https://www.evolving.money/EMoney/EBanking/challenger-comp.php"><div class="imgbox">
+				<img class="center-fit" src="../Images/Comp_chall_banks.png", alt="bank cards"></div></a>
+		
+		</div>
+	
+	</div>
+	
+	<div class="clear"></div>
+	
+		
+	<div class="clear"></div>
+</div>
+	
+<?php include("../includes/footer.html");?>
+
+</body>
+</html> 
