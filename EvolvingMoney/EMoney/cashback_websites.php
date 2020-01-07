f4ccebef782826c2d154b8c6b8e049170a20e090
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2020-01-07 23:40:38 +0000

    Update cashback_websites.php

--- EvolvingMoney/EMoney/cashback_websites.php
+++ EvolvingMoney/EMoney/cashback_websites.php
@@ -67,6 +67,18 @@
 		
 		</div>
 	
+		<div>
+			<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+			<ins class="adsbygoogle"
+    			style="display:block; text-align:center;"
+     			data-ad-layout="in-article"
+     			data-ad-format="fluid"
+     			data-ad-client="ca-pub-3169172932709847"
+     			data-ad-slot="6547992014"></ins>
+			<script>
+     			(adsbygoogle = window.adsbygoogle || []).push({});
+			</script>
+		</div>
 	</div>
 	
 	<div class="clear"></div>
