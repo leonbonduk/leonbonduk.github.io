a1c4c692d9972fd58ff60af10d4affa57a5e7900
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2020-01-10 00:16:49 +0000

    Create Freetrade.php

---
+++ EvolvingMoney/InvIdeas/Freetrade.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>		
+	<meta charset="UTF-8">
+       	<title>Fee free trading with Freetrade</title>	
+ 	<meta name="description" content="Review of new fee free trading app Freetrade which allows trading on UK and US stocks and ETFs" />
+   	<meta name="keywords" content="Fee free trading, freetrade, freetrade.io, free shares, free investing, share dealing" />
+       	<meta name="author" content="Leon Bond" />
+	<meta name="robots" content="all">
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<link href="../css/styles.css" type="text/css" rel="stylesheet" />
+	<!-- <link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" /> !-->
+	<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/960-12-col.css" />
+
+</head>
+
+<body>
+	<?php include("../includes/test-header.html");?>
+	
+<div class="row">
+
+	<div class="col_8 first" style="background-color:#fff">	
+		<br>	 
+		<h1 class="h1_article">Fee free trading</h1>
+		<h3>Leon Bond, 9th January 2020</h3>
+		<p><img src="https://www.evolving.money/Images/Freetrade_screen.png" alt="Smart phone screen showing share price chart" title="Freetrade app screenshot" style="width:300px;height:auto;float: left; margin: 1%; padding: 1%;"/> </p>
+		<p>One of the biggest bug bears of trading are the fees charged by brokers which limit the ability to trade in small amounts or effectively trade on short timescales without significantly eroding your gains!</p>
+    <p>A typical broker fee is of the order of £10 so if you were to make a £1k trade with a one year horizon, say, buying in would effectively cost you 1% of your investment and then, assuming no movement in share price, selling out would cost a further 1%. If you were trade that £1k four times in a year instead then fees would quickly add up to 8% of the value of your capital traded and more frequent trading would be ridiculous. But of course, hopefully on most of those trades you will have made some money such that you are trading larger amounts on the later trades and so the effective cost of the fees comes down, but that is not guaranteed.</p>
+    <p>Freetrade (https://freetrade.io/), a UK based fintech start-up, have recently launched a smartphone app in last year that significantly reduces these fees, offering a basic order which is completely free! Although this executes at only 3 to 4pm each day, otherwise you can trade instantly for a paltry £1.</p>
+    <p>This is an amazing offering which is hard to match in the market place in the UK currently substantially improving the cost effectiveness of trading in smaller amounts or over shorter timescales.</p>
+    <h2>Features</h2>
+    <p>Freetrade’s main selling point are its ability to trade for free, of course, and it’s intuitive and simple to use app.</p> 
+    <p>Accounting opening via the app (either Android or Apple iOS) is also fast and straight forward and once funded you can trade within a few hours.</p>
+    <p>There are selection of over 600 UK and US stocks and ETFs available to trade currently with more added regularly based on popularity and community requests.</p>
+    <p>Support can be accessed via live chat and are responsive but unfortunately there is no ability to call or email.</p>
+    <p>The app is being actively developed but currently only has minimal features in some areas such as:<p>
+    <ul class="Glossary">
+       <li>basic charts</li>
+       <li>basic company information</li>
+       <li>funding only possible by bank transfer and Apple Pay or Google Pay</li>
+       <li>limited order types</li>
+    </ul>
+    <h2>Development Roadmap</h2> 
+    <p>Freetade has however published their development roadmap (https://trello.com/b/W15ccViA/freetrade-roadmap) of features planned to be released in the coming months and there are some quite exciting features coming up that will make trading on its platform even more worthwhile, in particular:</p>
+    <ul class="Glossary">
+        <li>Instant deposits (currently can take few hours for new funds to be applied to an account)</li>
+        <li>10x more US Stocks</li>
+        <li>Premium account with instant free trades</li>
+        <li>Fractional share trading, making trades on some of those crazily priced US shares more accessible</li>
+        <li>European stocks and eventually other overseas markets</li>
+        <li>Improved company insights</li>
+        <li>Website account</li>
+    </ul>
+    <h2>Fees</h2>
+    <p>Charges are either free or minimal and highly competitive in the market:</p>
+    <ul class="Glossary">
+        <li>asic orders fee free (order completes at set time of 3 to 4pm) so does mean that price movement could go either for or against you in the interim.  You are able to cancel the order for free before the order completes however.</li>
+        <li>Instant order £1</li>
+        <li>No inactivity or account fees on standard account</li>
+        <li>£3 monthly fee on ISA otherwise fee structure is the same as a standard account.</li>
+        <li>No withdrawal fee unless need same day for which charge is £5. Standard withdrawal can take 2 working days to clear.</li>
+        <li>Only a GBP denominated account is offered so US trades require currency conversion with spread +/-0.45%</li>
+    </ul>
+    <p>Overall  the fees are highly competitive, particularly compared  to the main players in the market, Hargreaves Lansdown charges £11.95, Interactive Investor charges £7.99 and AJ Bell charges £9.95.</p>
+    <h2>Security</h2>
+    <p>Being regulated by the Financial Conduct Authority (FCA) in UK and so covered by the FSCS scheme ensures investments up to the value of £85,000 are protected by the regulator so although Freetrade are new and  unlikely to make any significant profits for  the foreseeable future since they are heavily investing in the development of their app, you can sleep soundly in the knowledge that if they were to go bust that you would get full reimbursement of your investments up to a value of £85,000.</p>
+    <h2>Worth a dabble</h2>
+    <p>Freetrade’s features are fairly basic at the ut with minimal fees and ability to access company info  and charting from other brokers then it’s possible to leverage the best of both worlds.</p>
+    <p>I still maintain a Hargreaves Lansdown SIPP (high fees but is a legacy pension scheme from a previous employer that I haven’t got around to moving to a cheaper provider, keep waiting for time to liquate positions so transfer for minimal transaction losses but that time never seems to have arrived) which has great features for researching shares and I also have an ISA at lower fee broker AJ Bell which also has some great features, but although fees are lower than HL they are still considerably higher fees than Freetrade.</p>
+    <p>To invest in funds or in a SIPP wrapper will warrant maintaining another broker’s account anyway but the ability to trade for free with Freetrade and with the security of protection of up to £85k with the FCA means it is too much of bargain to ignore! Also the chance it offers to trade smaller  amounts and more frequently will make playing the stock market a lot more exciting.</p>
+    <p>Even though smaller and more frequent trades are now accessible the account also suits people whose strategy is a more longer term, buy and hold approach.</p>
+    <h2>Free cash and shares!</h2>
+    <p>To illustrate the effectiveness of the account you may find following my sub-blog of interest where I attempt to grow free cash in my Freetrade account: <a href="https://www.evolving.money/InvIdeas/FTFC.php">Free Trading with Free Cash blog</a></p>
+    <p>If you’re interested in opening a Freetade account then they regularly provide referral offers where we both receive a random UK or US share to the value of up t o £200! If you are seriously interested in opening an account then drop me an email at following link and I will email back with a referral link if I have one available: <a class="link" href="mailto:leon@evolving.money">leon@evolving.money</a></p>
+
+	</div>
+
+	<div class="col_4 last" style="background-color:#fff">
+	
+		<div>
+			<a href="https://www.evolving.money/EMoney/EBanking/challenger-comp.php"><div class="imgbox">
+				<img class="center-fit" src="../Images/Comp_chall_banks.png", alt="bank cards"></div></a>
+		
+		</div>
+	
+		
+		<div class="vertical_ad">
+			<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+			<!-- Native_vertical_banner -->
+			<ins class="adsbygoogle"
+     			style="display:block"
+     			data-ad-client="ca-pub-3169172932709847"
+     			data-ad-slot="1100347920"
+     			data-ad-format="auto"
+     			data-full-width-responsive="true"></ins>
+			<script>
+     			(adsbygoogle = window.adsbygoogle || []).push({});
+			</script>
+		</div>
+	</div>
+	
+	<div class="clear"></div>
+	
+
+	<div class="clear"></div>
+</div>
+	
+<?php include("../includes/footer.html");?>
+
+</body>
+</html> 
