16221d56f5e79db4279f5ba0ab5bad21549dabc6
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2020-01-15 23:57:56 +0000

    Update challenger-comp2.php

--- EvolvingMoney/EMoney/EBanking/challenger-comp2.php
+++ EvolvingMoney/EMoney/EBanking/challenger-comp2.php
@@ -24,8 +24,10 @@
 <div>
 	<div class="row">
 		<div class="col_12 first" style="background-color:#fff">
-			<div class="imgbox zoom">
+			<div class ="zoom">
+			     <div class="imgbox">
     				<img class="left-fit-page-heading" src="https://www.evolving.money/Images/Banking_logo.GIF" alt="Banking.Money">
+			</div>
 			</div>
 		</div>
 	</div> 
