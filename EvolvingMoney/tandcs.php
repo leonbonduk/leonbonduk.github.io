d8cea4fa6842ca8eedc27c2637fec6308f9779af
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2020-01-18 21:53:08 +0000

    Create tandcs.php

---
+++ EvolvingMoney/tandcs.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+       	<title>Evolving Money - Terms and Conditions</title>
+   	<meta name="description" content="Terms and Conditions of using Evolving Money website" />
+       	<meta name="keywords" content="Terms and conditions" />
+       	<meta name="author"content="Leon Bond" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="robots" content="all">
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<link href="css/styles.css" type="text/css" rel="stylesheet" />
+	<!-- <link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" /> !-->
+	<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/960-12-col.css" />
+	
+</head>
+<body>
+	
+	<?php include("includes/test-header.html");?>
+
+<div>
+	<div class="row">
+		<div class="col_9 first" style="background-color:#fff">
+			<h1>Terms and Conditions</h1>
+      <p> On this site we try and provide a different take on you standard investments tipping site. The world is evolving fast in terms of investing online and online currency transactions! We hope you enjoy this ecllectic mix of contrarian and macro economic investment opportunties mixed in with some reasoned cryptocurrency plays and other new opprtunites coming out of the fintech space. We may also throw in some low risk ways to manage or grow your wealth that we come across and believe worth sharing. <p> 
+			<p> It is truely an exciting time in the world of money finance. There is a sense that change is on the horizon, and that the future is uncertain. Well the future is what we make it, and crisis is opportunity. What is certainly true, is that the world is changing fast, and the world is a very different place today, compared to when much of our current legacy financial system was architected. The ways we do business is changing fast, who we do business is changing faster still. Our methods and systems for payments, investments, and rights of ownership will change too... in deed the very nature of money may change alongside. 
+			<p> We are here to be your guides to the world of new types of investments, payments technologies, and even changes in money itself. More than that we are here to break it down by region, different parts of the world are doing different things, innovation tends to happen at the edges and move to the centre. We aim to be at the frontier giving you the story as it unfolds.<br></p>
+			
+		</div>
+
+		<div class="col_3 last" style="background-color:#fff">
+			<div class="imgbox"><img class="center-fit" src="Images/EM_circ_logo.jpg"></div>
+		</div>
+
+	</div>
+
+	<div class="clear"></div>
+
+</div>
+
+<?php include("includes/footer.html");?>
+
+</body>
+</html>
