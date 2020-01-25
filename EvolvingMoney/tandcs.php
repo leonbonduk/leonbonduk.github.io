fbe5ae0daf79fcc9b706be4d94f9910192c13c0e
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2020-01-25 22:22:12 +0000

    Update tandcs.php

--- EvolvingMoney/tandcs.php
+++ EvolvingMoney/tandcs.php
@@ -22,10 +22,91 @@
 	<div class="row">
 		<div class="col_9 first" style="background-color:#fff">
 			<h1>Terms and Conditions</h1>
-      <p> On this site we try and provide a different take on you standard investments tipping site. The world is evolving fast in terms of investing online and online currency transactions! We hope you enjoy this ecllectic mix of contrarian and macro economic investment opportunties mixed in with some reasoned cryptocurrency plays and other new opprtunites coming out of the fintech space. We may also throw in some low risk ways to manage or grow your wealth that we come across and believe worth sharing. <p> 
-			<p> It is truely an exciting time in the world of money finance. There is a sense that change is on the horizon, and that the future is uncertain. Well the future is what we make it, and crisis is opportunity. What is certainly true, is that the world is changing fast, and the world is a very different place today, compared to when much of our current legacy financial system was architected. The ways we do business is changing fast, who we do business is changing faster still. Our methods and systems for payments, investments, and rights of ownership will change too... in deed the very nature of money may change alongside. 
-			<p> We are here to be your guides to the world of new types of investments, payments technologies, and even changes in money itself. More than that we are here to break it down by region, different parts of the world are doing different things, innovation tends to happen at the edges and move to the centre. We aim to be at the frontier giving you the story as it unfolds.<br></p>
-			
+			<h1>Terms & Conditions</h1>
+			<p>The Evolving Money website and services are only provided for your general information and use and are not intended to cater for your particular requirements. Evolving Money and its staff <!--, and Evolving Money Limited (the “Company) !--> do not accept any liability for any loss suffered by any reader or user of our website as a result on any investment decision.</p>
+			<h2>General Terms and Conditions of Website Use</h2>
+			<p>These General Terms and Conditions, together with the documents referred to below (the “Agreement”), tell you the terms and conditions on which you may make use of www.evolving.money (the “Website”). If you do not agree to the terms and conditions of this Agreement you must not use the Website. The provisions of this Agreement do not affect your statutory rights as a consumer.<p>
+			<h2>Information about us</h2>
+			<!-- <p>The Website is operated by Evolving Money Limited. This company is registered in England and Wales under company number xxxxxx and with their registered offices and trading address at xxxx.</p> !-->
+			<p>The Website is currently operated as incorpated business run by a few enthusiastic bloggers.</p>
+			<p>Our preferred means of communication is by email. Should you wish to email us, you can do so by using our contact details <a href="https://www.evoling.money/contact.php">here</a>.<!-- For all postal communications with us kindly address any correspondence to our trading address as stated above.!--></p>
+			<h2>Registration and Access to the Websites</h2>
+			<p>We permit you to access the Website on a temporary basis but we reserve the right to withdraw or amend the services and information available on the Website (collectively the “Services”) and/or cancel any subscription without notice. The final decision rests with us<!--the Company.!--></p>
+			<p>Access to certain services is free.</p>
+			<h2>Information about you and your visits to the Websites</h2>
+			<p>We process information about you in accordance with our Privacy Policy. You consent to such processing and you confirm that all data provided by you is accurate.<p>
+			<h2>Investment Warning</h2>
+			<p>The Websites and the Services are:</p>
+			<ul>
+				<li>only provided for you</li>
+				<li>not intended to be relied upon by you in making or not making any investment decisions.</li>
+			</ul>
+			<p>If in doubt please seek independent financial advice.</p>
+			<p>The price of shares and investments and the income derived from them can go down as well as up, and investors may not get back the amount they invested. Past performance cannot be relied upon as a guide to future performance.</p>
+			<h2>Private Investor (non-professional)<h2>
+			<p>You agree that in entering into this Agreement you will at all times be acting as a private investor (non-professional) and not in the course of any employment, trade or profession which you may carry on.</p>
+			<p>For regulatory purposes, you are a private investor if you meet all of the following criteria for the entire term of your subscription.</p>
+			<ul>
+				<li>You are subscribing to the service in a personal capacity.</li>
+				<li>You are not currently registered or qualified as a professional securities trader or investment adviser with any national or state exchange, regulatory authority, professional association or recognised professional body.</li>
+				<li>You do not currently act in any capacity as an investment advisor, whether or not you have at some time been qualified to do so.</li>
+				<li>You use the service solely in relation to the management of your personal funds and not as a trader or an advisor to the public or to the investment of corporate funds.</li>
+				<li>You do not distribute, republish or otherwise provide any data from the service to any third party in any manner.</li>
+				<li>The data change is designed to cover individuals and will be maintained in the customer’s name and not in the company name – the exception to this would be a share club operating on a non-professional basis.</li>
+			</ul>
+			<p>You are not a private investor if you do not meet any one of the criteria outlined above.</p>
+			<h2>Rights in this Website</h2> 
+			<p>We own or are the authorised licensee of the copyrights and other intellectual property rights in the Website. The Website is protected by copyright laws and treaties around the world.
+			<p>The Website may not be reproduced, copied, downloaded, used, linked to, or exploited in any way except as specifically provided and permitted by this Agreement or as expressly agreed in writing by us.
+			<p>Subject to the terms of this Agreement, we hereby grant you a non-exclusive, non-transferable license to use the Website solely for your own personal or internal purposes. You acknowledge that the Services, and your use and interpretation of such services, requires skill and judgement for which you shall be solely responsible.</p>
+			<p>You further acknowledge that you shall be solely responsible for all opinions, forecasts, recommendations and other comments made or action taken by you based wholly or partly on the Services.</p>
+			<p>You may print extracts of materials from the Website in hard copy solely for your own personal purposes, provided that such materials are attributed to “Master Investor” and any copyright or other proprietary notices and disclaimers applied by us are retained. You may not otherwise copy the Website to any third party.</p>
+			<p>We reserve the right to suspend your access to any part of the Website should we become aware of any regulation, claim, condition or restriction in any part of the world which requires us to do so or which exposes us to any claim of liability or claim if we do not do so.</p>
+			<p>You shall promptly notify us of any unauthorised use of the Website or any actual or suspected infringement of any of the intellectual property rights in the Website of which you become aware.</p>
+			<h2>Linking</h2>
+			<p>You may link to the pages of the Website, provided you do so in a way that is fair and legal and which does not damage our reputation or take advantage of it. In particular, you must not establish a link in such a way as to suggest any form of association, approval or endorsement on our part where none exists. The Website must not be framed on any other website, nor should you establish a link to any part of the Website other than the home page. We reserve the right to withdraw linking permission without notice.</p>
+			<p>The Website contains links to various websites and other resources provided by third parties. These links are provided for your information only. We have no control over the contents of those websites or resources and accept no responsibility for them or for any loss or damage that you may suffer in connection with your use of them.</p>
+			<h2>Liability & Disclaimer</h2>
+			<p>The Website is supplied to you on an “as is” and an “if available” basis and we expressly disclaim and exclude all warranties, conditions, representations and terms, whether express or implied by statute, common law or otherwise with respect to the Website including, without limitation, as to the accuracy or completeness of the Website, as to whether the Website is up to date. We make no warranty that the Website is free from infection by viruses or anything else that has contaminating or destructive properties.</p>
+			<p>We hereby exclude all liability for any claim, loss, demands or damages of any kind whatsoever (whether such claims, loss, demands or damages were foreseeable, known or otherwise) arising out of or in connection with the use or inability to use the Website, including without limitation, indirect or consequential loss or damage; loss of actual or anticipated profits (including loss of profits on contracts); loss of revenue; loss of business; loss of opportunity; loss of anticipated savings; loss of goodwill; loss of reputation; loss of damage to or corruption of data; loss of use of money or otherwise, and whether or not advised of the possibility of such claim, loss demand or damages and whether arising in tort (including negligence), contract or otherwise to the fullest extent to which such liability may be excluded or avoided by law.</p>
+			<p>Nothing in this agreement excludes or limits our liability for (i) death or personal injury caused by our negligence (or that of our employees, agents or directors); (ii) fraud; or (iii) any liability which may not be limited or excluded by law.</p>
+			<p>You warrant and undertake not to use the Website in connection with any unlawful or illegal purpose, or so as to bring the Company and their businesses, products or services into disrepute. You also warrant not to use the site with any automated page retrieval tool.</p>
+			<p>You agree to indemnify us against any and all claims, losses, liabilities, damages and expenses (including legal fees) arising out of your misuse of the Website.</p>
+			<p>We may terminate this Agreement:</p>
+			<ul>
+				<li>immediately, upon notice in writing, in the event that you are in breach of this Agreement; or</li>
+				<li>upon not less than one month’s notice in writing at any time.</li>
+			</ul>
+			<p>The decision by either party to terminate this Agreement shall not limit any other rights or remedies it may have, or shall not affect any accrued rights or obligations arising before the date of termination.</p>
+			<p>Upon termination of this Agreement for any reason (including termination of any free trial), you shall immediately discontinue use of the Website. The restrictions set out in this Agreement shall survive the termination of this Agreement for any reason.</p>
+			<h2>Governing Law</h2>
+			<p>This Agreement shall be construed in accordance with and governed by English law and the parties shall submit to the exclusive jurisdiction of the English courts.<p>
+			<h2>User Content</h2>
+			<p>Users of our Website and Facebook page (https://www.facebook.com/110666326994953) have the opportunity to post certain content, such as comments (“User Comments”). User Comments do not necessarily reflect the views of the Company, and the Website<!--Company!--> does not guarantee the accuracy, truthfulness or reliability of any User Comment, nor endorse any opinions expressed in any User Comment. The Website<!--Company!--> assumes no responsibility or liability for or in connection with any User Comment. You are solely responsible for any User Content you post on the Website, and once posted or transmitted, cannot always be edited or withdrawn. All User Comments must comply with the Website's<!--Company’s!--> Content Submission Policy below.</p>
+			<h2>Content Submission Policy</h2>
+			<p>The Website<!--Company!--> encourages User Comments on the Website and Facebook page. User Comments are subject to the following rules: Stay on topic and be respectful of others. Never submit anything unlawful, threatening, abusive, harassing, obscene, defamatory, any material that would violate the law or the rights of others, or any material that you know or think might be false, inaccurate or misleading. Please do not post “ad hominem” or personal attacks against other users or the Company’s employees. Although you are welcome to reference and link to other blogs and web sites in your comment, commercial solicitations or advertising for other personal or commercial blogs and websites are not permitted. Repeatedly making the same post in multiple places is not allowed, and links to obscene or offensive sites are strictly prohibited. When in doubt, remember, the Website and our Facebook page are public forums. Use the same judgment and care you would when standing in front of an audience of thousands.</p>
+			<p>By submitting any User Content, you represent and warrant that:</p>
+			<ul>
+				<li>you are the sole author and owner of the intellectual property rights thereto;</li>
+				<li>all “moral rights” that you may have in such content have been voluntarily waived by you;</li>
+				<li>all content that you post is accurate;</li>
+				<li>you are at least 13 years old;</li>
+				<li>use of the content you supply does not violate these Terms and Conditions and will not cause injury to any person or entity.</li>
+			</ul>
+			<p>You further agree and warrant that you shall not submit any User Content:</p>
+			<ul>
+				<li>that is known by you to be false, inaccurate or misleading;</li>
+				<li>that infringes any third party’s copyright, patent, trademark, trade secret or other proprietary rights or rights of publicity or privacy;</li>
+				<li>that violates any law, statute, ordinance or regulation (including, but not limited to, those governing export control, consumer protection, unfair competition, anti-discrimination or false advertising);</li>
+				<li>that is, or may reasonably be considered to be, defamatory, libelous, hateful, racially or religiously biased or offensive, unlawfully threatening or unlawfully harassing to any individual, partnership or corporation;</li>
+				<li>for which you were compensated or granted any consideration by any third party;</li>
+				<li>that includes any information that references other websites, addresses, email addresses, contact information or phone numbers;</li>
+				<li>that contains any computer viruses, worms or other potentially damaging computer programs or files;</li>
+				<li>that advertises any product or service other than Master Investor without our prior written consent.</li>
+				</ul>
+			<p>The Website<!--Company!--> may, but has not obligation to, monitor and/or moderate User Content. All User Content that you submit may be used in the Website<!--Company!-->’s sole discretion. The Website<!--Company!--> reserves the right to change, condense or delete any User Content that in the Website<!--Company!-->’s sole opinion violates this Comment Submission Policy, the rights of others or any applicable law, or is in any way harmful, objectionable or otherwise inappropriate. The Website<!--Company!--> also has the right (but not the obligation) to block a user’s right to post User Comments to the Website or Facebook page.</p>
+			<p>By submitting a User Comment, you grant the Website<!--Company!--> a perpetual, irrevocable, royalty-free, transferable right (but not the obligation) and license to use, copy, modify, delete in its entirety, adapt, publish, translate, create derivative works from and/or sell and/or distribute such content and/or incorporate such content into any form, medium or technology throughout the world without compensation to you.</p>
+			<p>None of the User Content that you submit shall be subject to any obligation of confidence on the part of the Company, its agents, subsidiaries, affiliates, partners or third party service providers and their respective directors, officers and employees.</p>		
 		</div>
 
 		<div class="col_3 last" style="background-color:#fff">
