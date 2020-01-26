c1076bcbb1bef5a2cad5ae5d7e5a09fcc4f17389
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2020-01-26 22:25:05 +0000

    Update symbiont.php

--- EvolvingMoney/EMoney/symbiont.php
+++ EvolvingMoney/EMoney/symbiont.php
@@ -21,32 +21,23 @@
 
 	<div class="col_8 first" style="background-color:#fff">	
 		<br>	 
-		<h1 class="h1_article">Smart Securities and Inteligent Markets!</h1>
+		<h1 class="h1_article">Smart Securities and Intelligent Markets!</h1>
 		<h3>James Day, 26th January 2020</h3>
 		<p><img src="https://www.evolving.money/Images/Symbiont.jpg" alt="Symbiont logo" title="Symbiont" style="width:300px;height:auto;float: left; margin: 1%; padding: 1%;"/> </p>
-		<h2> <strong> Symbiont</strong> </h2>
-		
-		<p> Symbiont are key player binging innovation to finance. Their main objective is to use smart contracts and blockchain in finance; 
+		<h2><strong>Symbiont</strong> </h2>
+		<p>Symbiont are a key player binging innovation to finance. Their main objective is to use smart contracts and blockchain in finance; 
 	 	     and they look specifically at securities bonds and equities.<p>
-
-		<p> From their perspective, <strong> within 20 years financial services will be software </strong>. This is a reality of game theory... 
+		<p>From their perspective, <strong> within 20 years financial services will be software</strong>. This is a reality of game theory... 
 		    because some players are adopting the approach, and benefits of adoption are so high, all will have to adopt to compete. <p>
-		
-		<p> The <strong> Symbiont </strong> team have some key players from Wall Street such Caitlin Long, 20 years on Wall Street, including a spell in Morgan Stanley.<p>
-
-                <p> She sees the current status as a <strong> Kodak moment in finance </strong>...“Kodak didn’t understand that the business model was obsolete and did not make the pivot.”<p>
-
-                <p> There are 3 big clearing banking centres, UK, USA, Japan, if any of these parties were to adopt a blockchain currency solutions, payments systems would change for ever.<p>
-
-                <p> Symbiont are part of the story of blockchain and smart contract adoption in finance. Their platform seeks to track ownership of securities, bonds and equities on their business focussed blockchain.
-                    Blockchains have significant advantages over older technologies such as central servers and distributed databases; They are hack tolerant, and provide a perfect audit trail. 
-                    Information can be appended, but the past can’t be edited. <p>
-
-                <p> Smart contract elements allow automation of services, obsoleting many tasks done manually in traditional financial services organisations.<p>
-
-                <p> When we give money to banks, we don’t own our money; similar with equities we institutions to own things our behalf. Historically people held their money, they held their title deeds themselves.
+		<p>The <strong> Symbiont </strong> team have some key players from Wall Street such as Caitlin Long, 20 years on Wall Street, including a spell in Morgan Stanley.<p>
+                <p>She sees the current status as a <strong> Kodak moment in finance</strong>... “Kodak didn’t understand that the business model was obsolete and did not make the pivot.”<p>
+                <p>There are 3 big clearing banking centres, UK, USA, Japan, if any of these parties were to adopt a blockchain currency solutions, payments systems would change for ever.<p>
+                <p>Symbiont are part of the story of blockchain and smart contract adoption in finance. Their platform seeks to track ownership of securities, bonds and equities on their business focused blockchain.
+                    Blockchains have significant advantages over older technologies such as central servers and distributed databases; they are hack tolerant, and provide a perfect audit trail. 
+                    Information can be appended, but the past can’t be edited.<p>
+                <p>Smart contract elements allow automation of services, making many tasks done manually in traditional financial services organisations obsolete.<p>
+                <p>When we give money to banks, we don’t own our money; similar with equities we let institutions own things on our behalf. Historically people held their money and title deeds themselves.
                     Blockchain adoption may swing the pendulum back using technology to allow personal ownership, while maintaining, even exceeding utility benefits we have got from using banks and financial institutions.<p>
-
 	</div>
 
 	<div class="col_4 last" style="background-color:#fff">
