52e1b29fa73819981b84c818ea898aab6b08e2dc
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2020-01-26 23:10:35 +0000

    Create emoney-blog.php

---
+++ EvolvingMoney/EMoney/emoney-blog.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+	<meta charset="utf-8" />
+       	<title>Evolving Money: E-Money Blog</title>
+        <meta name="description" content="Guide to the latest news in the world of E-Money such as fintech, blockchain and crypto" />
+       	<meta name="keywords" content="fintech, crypto, crypto currency" />
+       	<meta name="author" content="James Day" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="robots" content="all" />
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<meta name="msvalidate.01" content="3443AD6F8D78FC033BB9F11D3C8D9931" />
+	<script src="https://evolving.money/table-nodejs/slideshow.js"></script> <!-- SLideshow js -->
+	<script data-ad-client="ca-pub-3169172932709847" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<link rel="canonical" href="https://www.evolving.money/">
+	<link href="css/styles.css" type="text/css" rel="stylesheet" />
+	<!-- <link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" /> !-->
+	<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/960-12-col.css" />
+	<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/slideshow.css" />
+	
+</head>
+<body>
+	
+	<?php include("includes/test-header.html");?>
+<div>	
+
+ 		<div class="row">
+							      					      
+				<div style="border-bottom-width: 5px; border-bottom-style: solid; border-bottom-color: #274168;">
+					<div class="col_int_12">
+						<h1><strong>E-Money:</strong></h1>
+					</div>
+					
+					<div class="clear"></div>
+					<a href="https://www.evolving.money/EMoney/what-is-emoney.php">
+						<div class="col_int_6">
+							<div class="imgbox"><img class="center-fit" src="Images/EMoney.jpg" alt="Bitcoin and  bank notes on a keyboard"></div>
+							<h2 class="article-link-header">What is E-Money?</h2>
+						</div>
+					</a>
+					
+					<a href="https://www.evolving.money/EMoney/OpenSourceHedgeFund.php">
+						<div class="col_int_6">
+							<div class="imgbox"><img class="center-fit" src="Images/numerai.jpg" alt="Hexagonal maze symbol atop three skyscrapers"></div>
+							<h2 class="article-link-header">Open Source Hedge Funds!</h2>
+						</div>
+					</a>
+					
+					<a href="https://www.evolving.money/EMoney/symbiont.php">
+						<div class="col_int_6">
+							<div class="imgbox"><img class="center-fit" src="Images/Symbiont.jpg" alt="Crowds of people connected by lines of light"></div>
+							<h2 class="article-link-header">Smart Securities and Intelligent Markets</h2>
+						</div>
+					</a>
+					
+					<a href="https://www.sportpursuit.com/join/4FD75C24DAE7F8">
+						<div class="col_int_6">
+							<div class="imgbox"><img class="center-fit" src="Images/Sportpursuit.jpg" alt="Two hands exchanging a euro and dollar note"></div>
+							<h2 class="article-link-header">Sport flash sales site, selling the best sports & outdoor brands at up to 70% off RRP</h2>
+						</div>
+					</a>
+					
+					<div class="clear"></div>
+				</div>
+				
+						
+					<div class="col_int_6">
+						<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+						<ins class="adsbygoogle"
+     						style="display:block; text-align:center;"
+     						data-ad-layout="in-article"
+     						data-ad-format="fluid"
+     						data-ad-client="ca-pub-3169172932709847"
+     						data-ad-slot="1508607694"></ins>
+						<script>
+     						(adsbygoogle = window.adsbygoogle || []).push({});
+						</script>
+					</div>
+					
+					<div class="col_int_6">
+						<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+						<ins class="adsbygoogle"
+     						style="display:block; text-align:center;"
+     						data-ad-layout="in-article"
+     						data-ad-format="fluid"
+     						data-ad-client="ca-pub-3169172932709847"
+     						data-ad-slot="1508607694"></ins>
+						<script>
+     						(adsbygoogle = window.adsbygoogle || []).push({});
+						</script>
+					</div>
+					
+				</div>
+			</div> <!-- End of col_8 !-->
+			
+			<div class="col_4 last" style="background-color:#fff">
+				<div class="imgbox"><img class="center-fit" src="Images/EM_circ_logo.jpg" alt="Evolvinmg Money text surrounded by circle of financial symbols"></div>
+				
+				<!-- <div>
+					<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+					<ins class="adsbygoogle"
+     					style="display:block; text-align:center;"
+     					data-ad-layout="in-article"
+     					data-ad-format="fluid"
+     					data-ad-client="ca-pub-3169172932709847"
+     					data-ad-slot="6547992014"></ins>
+					<script>
+     					(adsbygoogle = window.adsbygoogle || []).push({});
+					</script>
+				</div>
+				
+				<div>
+					<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+					<ins class="adsbygoogle"
+     					style="display:block; text-align:center;"
+     					data-ad-layout="in-article"
+     					data-ad-format="fluid"
+     					data-ad-client="ca-pub-3169172932709847"
+     					data-ad-slot="1508607694"></ins>
+					<script>
+     					(adsbygoogle = window.adsbygoogle || []).push({});
+					</script>
+				</div> !-->
+				
+				<div class="vertical_ad">
+					<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+					<!-- Native_vertical_banner -->
+					<ins class="adsbygoogle"
+     					style="display:block"
+     					data-ad-client="ca-pub-3169172932709847"
+     					data-ad-slot="1100347920"
+     					data-ad-format="auto"
+     					data-full-width-responsive="true"></ins>
+					<script>
+     					(adsbygoogle = window.adsbygoogle || []).push({});
+					</script>
+				</div>
+			</div> <!-- End of col_4 !-->
+				
+		<div class="clear"></div>
+				
+		</div> <!-- End of row !-->
+	
+		<div class="clear"></div>
+</div>			
+
+<?php include("includes/footer.html");?>
+
+</body>
+</html>
