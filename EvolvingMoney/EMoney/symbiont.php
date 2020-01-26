4f319761936c61606e90319c6c1f8cfdb20b6ecf
Jibber2000 <32431276+Jibber2000@users.noreply.github.com>
2020-01-26 18:38:13 +0000

    Update symbiont.php

--- EvolvingMoney/EMoney/symbiont.php
+++ EvolvingMoney/EMoney/symbiont.php
@@ -1,1 +1,81 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>		
+	<meta charset="UTF-8">
+       	<title>Smart Securities & Intelligent Markets </title>	
+ 	<meta name="description" content="summary." />
+   	<meta name="keywords" content="Symbiont, Smart Securities, eMoney, fintech" />
+       	<meta name="author" content="James Day" />
+	<meta name="robots" content="all">
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<link href="../css/styles.css" type="text/css" rel="stylesheet" />
+	<!-- <link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" /> !-->
+	<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/960-12-col.css" />
+
+</head>
+
+<body>
+	<?php include("../includes/test-header.html");?>
+	
+<div class="row">
+
+	<div class="col_8 first" style="background-color:#fff">	
+		<br>	 
+		<h1 class="h1_article">Open Source Hedge Funds!</h1>
+		<h3>James Day, 26th January 2020</h3>
+		<p><img src="https://www.evolving.money/Images/numerai.jpg" alt="Numerai hexagonal logo at top of three skyscrapers" title="Numerai" style="width:300px;height:auto;float: left; margin: 1%; padding: 1%;"/> </p>
+		<h2> <strong>NumerAi</strong> Smart Securities & Intelligent Markets </h2>
+                <p> Symbiont are key player binging innovation to finance. Their main objective is to use smart contracts and blockchain in finance; and they look specifically at securities bonds and equities.
+                    From their perspective, within 20 years financial services will be software. This is a reality of game theory... because some players are adopting the approach, and benefits of adoption are so high, all will have to adopt to compete. 
+                    The Symbiont team have some key players from Wall Street such Caitlin Long, 20 years on Wall Street, including a spell in Morgan Stanley.<p>
+
+                <p> She sees the current status as Kodak moment in finance...“Kodak didn’t understand that the business model was obsolete and did not make the pivot.”<p>
+
+                <p> There are 3 big clearing banking centres, UK, USA, Japan, if any of these parties were to adopt a blockchain currency solutions, payments systems would change for ever.<p>
+
+                <p> Symbiont are part of the story of blockchain and smart contract adoption in finance. Their platform seeks to track ownership of securities, bonds and equities on their business focussed blockchain.
+                    Blockchains have significant advantages over older technologies such as central servers and distributed databases; They are hack tolerant, and provide a perfect audit trail. 
+                    Information can be appended, but the past can’t be edited. <p>
+
+                <p> Smart contract elements allow automation of services, obsoleting many tasks done manually in traditional financial services organisations.<p>
+
+                <p> When we give money to banks, we don’t own our money; similar with equities we institutions to own things our behalf. Historically people held their money, they held their title deeds themselves.
+                    Blockchain adoption may swing the pendulum back using technology to allow personal ownership, while maintaining, even exceeding utility benefits we have got from using banks and financial institutions.<p>
+
+	</div>
+
+	<div class="col_4 last" style="background-color:#fff">
+	
+		<div>
+			<a href="https://www.evolving.money/EMoney/EBanking/challenger-comp.php"><div class="imgbox">
+				<img class="center-fit" src="../Images/Comp_chall_banks.png", alt="bank cards"></div></a>
+		
+		</div>
+	
+		
+		<div class="vertical_ad">
+			<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+			<!-- Native_vertical_banner -->
+			<ins class="adsbygoogle"
+     			style="display:block"
+     			data-ad-client="ca-pub-3169172932709847"
+     			data-ad-slot="1100347920"
+     			data-ad-format="auto"
+     			data-full-width-responsive="true"></ins>
+			<script>
+     			(adsbygoogle = window.adsbygoogle || []).push({});
+			</script>
+		</div>
+	</div>
+	
+	<div class="clear"></div>
+	
+
+	<div class="clear"></div>
+</div>
+	
+<?php include("../includes/footer.html");?>
+
+</body>
+</html> 
 
