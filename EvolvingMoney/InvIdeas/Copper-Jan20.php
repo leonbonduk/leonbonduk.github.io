9a76c2e5c706e61125b23e248978e4e6a1fa417f
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2020-01-29 00:32:55 +0000

    Create Copper-Jan20.php

---
+++ EvolvingMoney/InvIdeas/Copper-Jan20.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>		
+	<meta charset="UTF-8">
+       	<title>Evolving Money - Has copper caught a cold?</title>	
+ 	<meta name="description" content="Has the coronavirus presented a buying opportunity for copper?" />
+   	<meta name="keywords" content="Copper, etf, contrarian, buying opportunity, emerging markets, markets, commodities, Wisdomtree Copper, COPA" />
+       	<meta name="author" content="Leon Bond" /> 
+	<meta name="robots" content="all">
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<link href="../css/styles.css" type="text/css" rel="stylesheet" />
+	<!-- <link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" /> !-->
+	<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/960-12-col.css" />
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-157104566-1"></script>
+	<script>
+  	window.dataLayer = window.dataLayer || [];
+  	function gtag(){dataLayer.push(arguments);}
+  	gtag('js', new Date());
+  	gtag('config', 'UA-157104566-1');
+	</script>
+  
+</head>
+
+<body>
+	<?php include("../includes/test-header.html");?>
+	
+<div class="row">
+
+	<div class="col_8 first" style="background-color:#fff">	
+		<br>	 
+		<h1 class="h1_article">Has copper caught a cold?</h1>
+		<h3>Leon Bond, 28th January 2020, Wisdomtree Copper ETF (COPA), Price $23.84</h3>
+		<p><img src="https://www.evolving.money/Images/copper_EM.jpg" alt="Messy pile of scrap copper pipes" title="Turkish Delight" style="width:300px;height:auto;float: left; margin: 1%; padding: 1%;"/> </p>
+		<p>Since Turkey's incursion into Syria a few days ago  Turkey's stock market has fallen to fresh lows with the MSCI Turkey index down 18% since 30th September! It remains 13% higher than the lows seen in May this year however.</p> 
+		<p>With a ceasefire now negotiated by Trump as of today, and the Kurds having allowed the Syrian government forces up to protect the border the markets could now drift back up to their highs seen just a couple of weeks ago.</p>
+		<h2>Other market details</h2>
+		<p>Turkey's market was already very cheap before this retracement, due in the most part to the political turmoil of the Turkish president's regime, President Erdoğan. The Turkish MSCI index was following simlar market trends to the general MSCI EMerging Markets Index from 2004 right up to 2016. But since shortly before the attempted coup in 2016, however, the Turkish market has diverged considerably from the rest of the emerging markets, diverging by about 50%.</p>
+		<p>At 30th September the MSCI Turkey index had a P/E ratio of 8.2 and a forward P/E ratio of 6.3! This compares to the MSCI Emerging Markets index which had P/E ratio of 13.2 and a forward P/E ratio of 11.9. Therefore the Turkish market is trading at approximately half the value of other emerging markets...</p>
+		<h2>High risk opportunity</h2>
+		<p>Although this is obviously a high risk play given the current Syrian situation, the market valuations provide some comfort that even if the short term bet does not pan out, if you can hold for longer term then this should at least return to slightly more sensible valuations, although coming fully back in line with the other emerging markets is unlikely for the foreseeable future until the internal political situation in Turkey settles down also.</p>
+	</div>
+
+	<div class="col_4 last" style="background-color:#fff">
+			
+		<div>
+			<p>Latest ETF price:</p>
+		</div>
+	
+		<div class="chart_container">
+			<div class="chart_chart">
+<!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container">
+  <div class="tradingview-widget-container__widget"></div>
+  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/markets/futures/" rel="noopener" target="_blank"><span class="blue-text">Commodities</span></a> by TradingView</div>
+  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-market-overview.js" async>
+  {
+  "colorTheme": "light",
+  "dateRange": "12m",
+  "showChart": true,
+  "locale": "en",
+  "width": "100%",
+  "height": "100%",
+  "largeChartUrl": "",
+  "isTransparent": false,
+  "plotLineColorGrowing": "rgba(33, 150, 243, 1)",
+  "plotLineColorFalling": "rgba(33, 150, 243, 1)",
+  "gridLineColor": "rgba(233, 233, 234, 1)",
+  "scaleFontColor": "rgba(120, 123, 134, 1)",
+  "belowLineFillColorGrowing": "rgba(33, 150, 243, 0.12)",
+  "belowLineFillColorFalling": "rgba(33, 150, 243, 0.12)",
+  "symbolActiveColor": "rgba(33, 150, 243, 0.12)",
+  "tabs": [
+    {
+      "title": "Commodities",
+      "symbols": [
+        {
+          "s": "LSE:COPA"
+        }
+      ],
+      "originalTitle": "Commodities"
+    }
+  ]
+}
+  </script>
+</div>
+<!-- TradingView Widget END -->
+			</div>
+		</div>
+	
+		<div>
+			<a href="https://www.evolving.money/EMoney/EBanking/challenger-comp.php"><div class="imgbox">
+				<img class="center-fit" src="../Images/Comp_chall_banks.png", alt="bank cards"></div></a>
+		
+		</div>
+    
+    <div>
+			<a href="https://www.evolving.money/InvIdeas/FTFC.php"><div class="imgbox">
+				<img class="center-fit" src="../Images/Free_Trading.png", alt="Suited man watering a money tree"></div></a>
+		
+		</div>
+	
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+			<!-- Native_vertical_banner -->
+			<ins class="adsbygoogle"
+     			style="display:block"
+     			data-ad-client="ca-pub-3169172932709847"
+     			data-ad-slot="1100347920"
+     			data-ad-format="auto"
+     			data-full-width-responsive="true"></ins>
+			<script>
+     			(adsbygoogle = window.adsbygoogle || []).push({});
+			</script>
+      
+	</div>
+	
+	<div class="clear"></div>
+	
+	<div class="col_8 first" style="background-color:#8a8a8a" >
+<p>How to trade:<br><br>As this impact in the copper price appears to be more on the price of copper rather than any impact currently on copper miner's share prices the best and cheapest way to play such a short term market opportunity like this is via an ETF, which are usually passively managed and so have low management fees. As they are traded on a stock exchange they usually also have a smaller spread when buying or selling compared to a standard unit trust fund. It is also best to make sure a reputable provider is choosen to ensure counter party risk is minimised, i.e. risk that they will go bust is minimised. I have decided to take a small stake in WisdomTree's Copper ETF trading under the ticker COPA which attempts to replciate the performance in the copper price by tracking the Bloomberg Copper Subindex through the use of swaps with the investment banks of Citigroup and Merrill Lynch and charges a management fee of only 0.5%.<br><br>Synthetic replication is riskier than through buying the commodity directly but with commidities it is harder and more expensive to hold the asset directly than with equities in an ETF. As the conterparties are reputable and with strong credit ratings the risk should still be fairly minimal however.<br><br>You should be able to buy this ETC through most online brokers in a similar manner to a normal company share, just search for the ticker COPA on your broker's trading screen.<br><br>I have onlt taken a small stake currently to see how the coroavirus pans out in next few days and so potnetially buy more if price falls any further.  
+</p>
+	</div>
+	
+	<div class="clear"></div>
+</div>
+	
+<?php include("../includes/footer.html");?>
+
+</body>
+</html> 
