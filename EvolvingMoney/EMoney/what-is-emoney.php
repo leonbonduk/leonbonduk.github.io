ab4ab723f2339ae794f8d9de05b889159b9368b0
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2020-01-30 00:46:20 +0000

    Update what-is-emoney.php

--- EvolvingMoney/EMoney/what-is-emoney.php
+++ EvolvingMoney/EMoney/what-is-emoney.php
@@ -10,6 +10,15 @@
 		<meta name="viewport" content="width=device-width,initial-scale=1" />
 		<link href="https://www.evolving.money/css/styles.css" type="text/css" rel="stylesheet" />
 		<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/960-12-col.css" />
+	
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-157104566-1"></script>
+		<script>
+  		window.dataLayer = window.dataLayer || [];
+  		function gtag(){dataLayer.push(arguments);}
+  		gtag('js', new Date());
+  		gtag('config', 'UA-157104566-1');
+		</script>
 </head>
 
 
