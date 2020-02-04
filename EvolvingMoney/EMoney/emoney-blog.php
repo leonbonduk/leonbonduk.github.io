1e43447b17b98d5be7a1bc46a0ae35848d2477ea
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2020-02-04 21:02:27 +0000

    Update emoney-blog.php

--- EvolvingMoney/EMoney/emoney-blog.php
+++ EvolvingMoney/EMoney/emoney-blog.php
@@ -42,6 +42,13 @@
 						</div>
 					</a>
 					
+					<a href="https://www.evolving.money/EMoney/blockchain-in-renewable-energy.php">
+						<div class="col_int_6">
+							<div class="imgbox"><img class="center-fit" src="https://www.evolving.money/Images/Wepower.png" alt="Wepower blockchain and wind turbines" title="Wepower"></div>
+							<h2 class="article-link-header">Smart payments, smarter markets and cleaner energy … the emerging story of blockchain in renewable energy markets</h2>
+						</div>
+					</a>
+					
 					<a href="https://www.evolving.money/EMoney/OpenSourceHedgeFund.php">
 						<div class="col_int_6">
 							<div class="imgbox"><img class="center-fit" src="../Images/numerai.jpg" alt="Hexagonal maze symbol atop three skyscrapers"></div>
