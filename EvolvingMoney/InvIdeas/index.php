2dac4bfbc20dcf6c300708d42e34088cc04ea4f0
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2020-02-04 22:14:56 +0000

    Update index.php

--- EvolvingMoney/InvIdeas/index.php
+++ EvolvingMoney/InvIdeas/index.php
@@ -12,12 +12,21 @@
 		<link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" />
 		<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/960-12-col.css" />
 
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-157104566-1"></script>
+	<script>
+  	window.dataLayer = window.dataLayer || [];
+  	function gtag(){dataLayer.push(arguments);}
+  	gtag('js', new Date());
+  	gtag('config', 'UA-157104566-1');
+	</script>
+	
 <body>
 
 	<?php include("../includes/test-header.html");?>
 
 <!-- <div>
-	<div class="row" margin 0 0;>
+	<div class="row">
 		<div class="col_12 first" style="background-color:#fff">
 			<div class="imgbox"><img class="left-fit-page-heading" src="https://www.evolving.money/Images/Investments_logo.gif">
 		</div>
@@ -27,60 +36,97 @@
 <div class="clear"></div>
 
 <div class="row">
-	<div class="col_9 first" style="background-color:#fff">	 
-   		<p>Latest investment ideas:</p>
-   
-   		<ul>			 
-     			<li><a href="Turkey-Oct19.php">Turkish Delight - 17th Oct 2019</a></li>
-       			<li><a href="BT-Sept19.php">BT a contrarian buy? - 2nd Sept 2019</a></li>		 
-     			<li>Coming...</li>		
-   		</ul>
-		<br>
-	</div>
+					
+			<div class="col_8 first" >
+				<div style="border-bottom-width: 5px; border-bottom-style: solid; border-bottom-color: #FF9300;">
+					<a href="https://www.evolving.money/InvIdeas/Copper-Jan20.php">
+						<div class="col_int_6">
+							<div class="imgbox"><img class="center-fit-rounded-top" src="Images/copper_EM.jpg" alt="A messy pile of scrap copper pipes"></div>
+							<h2 class="article-link-header">Has copper caught a cold?</h2>
+						</div>
+					</a>
+					
+					<a href="https://www.evolving.money/InvIdeas/FTFC.php">
+						<div class="col_int_6">
+							<div class="imgbox"><img class="center-fit-rounded-top" src="Images/Free_Trading_small_EM.png" alt="Suited man watering a money tree"></div>
+							<h2 class="article-link-header">Fee free trading with free cash blog</h2>
+						</div>
+					</a>
+					
+					<a href="https://www.evolving.money/InvIdeas/Turkey-Oct19.php">
+						<div class="col_int_6">
+							<div class="imgbox"><img class="center-fit-rounded-top" src="Images/turkey-flag-wrinkled.jpg" alt="Wrinkled Turkish flag"></div>
+							<h2 class="article-link-header">Potential opportunity to buy into a cheap Turkish market?</h2>
+						</div>
+					</a>
+									       
+					<a href="https://www.evolving.money/InvIdeas/BT-Sept19.php">
+						<div class="col_int_6">
+							<div class="imgbox"><img class="center-fit-rounded-top" src="Images/small_phone_boxes.jpg" alt="Red phone box"></div>
+							<h2 class="article-link-header">Is BT a contrarian buy?<br></h2>
+						</div>
+					</a>
 
-	<div class="col_3 last" style="background-color:#fff">
-		<div class="imgbox"><img class="center-fit" src="https://www.evolving.money/Images/EM_circ_logo.jpg"></div>
-	</div>
-
-</div>
-
-<div class="clear"></div> !-->
-	
-<div>	
-
- 		<div class="row">
-			<div class="col_12 first">
-				<div class="imgbox"><img class="left-fit-page-heading" src="https://www.evolving.money/Images/Investments_logo.gif" alt="Investments.Money"></div>
-				<p>Our latest investments tips, a collection of contrarian and macro-economic investment plays:</p>
-			</div>
+					
+					<div class="clear"></div>
+				</div>
+				
+						
+					<div class="col_int_6">
+						<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+						<ins class="adsbygoogle"
+     						style="display:block; text-align:center;"
+     						data-ad-layout="in-article"
+     						data-ad-format="fluid"
+     						data-ad-client="ca-pub-3169172932709847"
+     						data-ad-slot="1508607694"></ins>
+						<script>
+     						(adsbygoogle = window.adsbygoogle || []).push({});
+						</script>
+					</div>
+					
+					<div class="col_int_6">
+						<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+						<ins class="adsbygoogle"
+     						style="display:block; text-align:center;"
+     						data-ad-layout="in-article"
+     						data-ad-format="fluid"
+     						data-ad-client="ca-pub-3169172932709847"
+     						data-ad-slot="1508607694"></ins>
+						<script>
+     						(adsbygoogle = window.adsbygoogle || []).push({});
+						</script>
+					</div>
+					
+				</div>
+			</div> <!-- End of col_8 !-->
 			
-		</div>
+			<div class="col_4 last" style="background-color:#fff">
+				<div class="imgbox"><img class="center-fit" src="../Images/EM_circ_logo.jpg" alt="Evolving Money text surrounded by circle of financial symbols"></div>
+				
+				
+				<div class="vertical_ad">
+					<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+					<!-- Native_vertical_banner -->
+					<ins class="adsbygoogle"
+     					style="display:block"
+     					data-ad-client="ca-pub-3169172932709847"
+     					data-ad-slot="1100347920"
+     					data-ad-format="auto"
+     					data-full-width-responsive="true"></ins>
+					<script>
+     					(adsbygoogle = window.adsbygoogle || []).push({});
+					</script>
+				</div>
+			</div> <!-- End of col_4 !-->
 				
 		<div class="clear"></div>
-		
- 		<div class="row">
-
-				<a href="https://www.evolving.money/InvIdeas/Turkey-Oct19.php">
-					<div class="col_4 first">
-						<div class="imgbox"><img class="center-fit" src="https://www.evolving.money/Images/turkey-flag-wrinkled.jpg" alt="Wrinkled Turkish flag"></div>
-						<h2 class="article-link-header">Potential opportunity to buy into a cheap Turkish market?</h2>
-					</div>
-				</a>
-									       
-				<a href="https://www.evolving.money/InvIdeas/BT-Sept19.php">
-					<div class="col_4">
-						<div class="imgbox"><img class="center-fit" src="https://www.evolving.money/Images/small_phone_boxes.jpg" alt="Two red phone boxes"></div>
-						<h2 class="article-link-header">Is BT a contrarian buy?</h2>
-					</div>
-				</a>
-			
-				<div class="col_4 last" style="background-color:#fff"><div class="imgbox"><img class="center-fit" src="https://www.evolving.money/Images/EM_circ_logo.jpg" alt="Evolving Money logo"></div>
-	
-			
-		</div>
+				
+		</div> <!-- End of row !-->
 	
 		<div class="clear"></div>
-</div>		
+</div>		       
+					
 					
 <?php include("../includes/footer.html");?>
 	
