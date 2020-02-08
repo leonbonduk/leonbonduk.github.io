c6f833d91b6bd0f23c19a618891b4a91bae2afe9
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2020-02-08 21:17:42 +0000

    Update blockchain-in-EV-charging.php

--- EvolvingMoney/EMoney/EV-blockchain-charging/blockchain-in-EV-charging.php
+++ EvolvingMoney/EMoney/EV-blockchain-charging/blockchain-in-EV-charging.php
@@ -2,14 +2,13 @@
 <html lang="en" class="no-js">
 <head>		
 	<meta charset="UTF-8">
-       	<title>Can blockchain ease range anxiety in electric vehicles with
-smart charging payments?</title>	
+       	<title>Can blockchain ease range anxiety in electric vehicles with smart charging payments?</title>	
  	<meta name="description" content="summary." />
    	<meta name="keywords" content="Numerai, EV charging payments, eMoney, blockchain, payments systems, fintech" />
        	<meta name="author" content="James Day" />
 	<meta name="robots" content="all">
 	<meta name="viewport" content="width=device-width,initial-scale=1" />
-	<link href="../css/styles.css" type="text/css" rel="stylesheet" />
+	<link href="../../css/styles.css" type="text/css" rel="stylesheet" />
 	<!-- <link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" /> !-->
 	<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/960-12-col.css" />
 
@@ -26,57 +25,39 @@ smart charging payments?</title>
 </head>
 
 <body>
-	<?php include("../includes/test-header.html");?>
+	<?php include("../../includes/test-header.html");?>
 	
 <div class="row">
 
 	<div class="col_8 first" style="background-color:#fff">	
 		<br>	 
-		<h1 class="h1_article"></h1>
+		<h1 class="h1_article">Can blockchain ease range anxiety in electric vehicles with smart charging payments?</h1>
 		<h3>James Day, 8th February 2020</h3>
-		<p><img class="article-img-fit" src="https://www.evolving.money/Images/numerai.jpg" alt="Numerai hexagonal logo at top of three skyscrapers" title="Numerai"> </p>
-		<h2> EURELECTRIC </h2>
-		
-		
-<p> There has been a growing discussion about a potential role for <strong>blockchain</strong>, in assisting payments for charging in electric vehicles. There have been a number of commentators on the subject including academic researchers and discussion panels, before we get into all that it’s best to start by understanding the problem with the existing charging infrastructure for electric vehicles.</p>
+		<p><img class="article-img-fit" src="https://www.evolving.money/Images/electric_car.png" alt="Electric car with winding cable and plug runnic past renewable enery sources" title="Electric Car - https://www.freepik.com/free-photos-vectors/car">Car vector created by freepik - www.freepik.com"> </p>
+		<h2>EURELECTRIC</h2>
+		<p>There has been a growing discussion about a potential role for <strong>blockchain</strong>, in assisting payments for charging in electric vehicles. There have been a number of commentators on the subject including academic researchers and discussion panels, before we get into all that it’s best to start by understanding the problem with the existing charging infrastructure for electric vehicles.</p>
+		<p>Charging electric vehicles is typically a lot cheaper, than buying diesel and petrol, so some may ask what is the problem. Well, there is a potential problem and there are also solutions…</p>
+		<p>Charging electric vehicles is typically cheapest, when you charge at home over night. Once you have driven somewhere, maybe to work as a daily commute or something else a longer trip, you are beholden to whatever prices exist at the available charging stations, and you may not have the range to shop around.</p>
+		<p>How pricing of petrol and diesel works on fuel station forecourt is a familiar topic; one we navigate daily. Fuel on motorways is expensive, fuel at supermarkets is cheap, and there are geographic variations; but generally if there are a lot of fuel stations near each other prices are cheaper, and you typically have sufficient range and time to shop around.</p>
+		<p>If you are driving an electric vehicle and have to charge up away from home the options are more limited. Typically you have to charge with whatever option exists and with whatever price is offered, as there are limited options to get somewhere else. This all ties into the topic of range anxiety in electric vehicles.</p>
+		<p>It is however a bit strange, when we buy electricity at home we are all tied into the same grid, all producers feed the same grid, and the user gets to choose their provider and price, you get to shop around for providers, (albeit many of us are bad at shopping around and pay more than we should, but nevertheless).</p>
+		<p>The <strong>blockchain use-case</strong> is essentially that smart contracts can be used to <strong>autonomously shop for the best prices</strong> from providers, and the ledger can provide transparency in the form of an in-tamperable record of what’s happened. This could bring much better prices for consumers.</p>
+		<p>The benefits don’t stop with consumers though, the charging station operators can benefit from common payment mechanisms and standardised pricing and increased trust, factor crucial to improved branding and popularity. From the electricity providers perspective, the benefits exist in terms of faster electric vehicle adoption, and essentially a growth in consumer demand for their product. From the electric vehicle manufacturers perspective,  it is similar, they get to benefit from faster adoption rates, and a reduction in the range anxiety that is holding back adoption of their products.</p>
+		<p>There has been interesting research in the blockchain use case by the <strong>University of Waterloo</strong>. [Christian Gorenflo Waterloo’s David R Cheriton School of Computer Science]</p>
 
-<p> Charging electric vehicles is typically a lot cheaper, than buying diesel and petrol, so some may ask what is the problem. Well, there is a potential problem and there are also solutions…</p>
-
-<p> Charging electric vehicles is typically cheapest, when you charge at home over night. Once you have driven somewhere, maybe to work as a daily commute or something else a longer trip, you are beholden to whatever prices exist at the available charging stations, and you may not have the range to shop around.</p>
-	
-<p> How pricing of petrol and diesel works on fuel station forecourt is a familiar topic; one we navigate daily. Fuel on motorways is expensive, fuel at supermarkets is cheap, and there are geographic variations; but generally if there are a lot of fuel stations near each other prices are cheaper, and you typically have sufficient range and time to shop around.</p>
-
-<p> If you are driving an electric vehicle and have to charge up away from home the options are more limited. Typically you have to charge with whatever option exists and with whatever price is offered, as there are limited options to get somewhere else. This all ties into the topic of range anxiety in electric vehicles.</p>
-
-<p> It is however a bit strange, when we buy electricity at home we are all tied into the same grid, all producers feed the same grid, and the user gets to choose their provider and price, you get to shop around for providers, (albeit many of us are bad at shopping around and pay more than we should, but nevertheless).</p>
-
-<p> The <strong>blockchain use-case</strong> is essentially that smart contracts can be used to <strong>autonomously shop for the best prices</strong> from providers, and the ledger can provide transparency in the form of an in-tamperable record of what’s happened. This could bring much better prices for consumers.</p>
-
-<p> The benefits don’t stop with consumers though, the charging station operators can benefit from common payment mechanisms and standardised pricing and increased trust, factor crucial to improved branding and popularity. From the electricity providers perspective, the benefits exist in terms of faster electric vehicle adoption, and essentially a growth in consumer demand for their product. From the electric vehicle manufacturers perspective,  it is similar, they get to benefit from faster adoption rates, and a reduction in the range anxiety that is holding back adoption of their products.</p>
-
-<p> There has been interesting research in the blockchain use case by the <strong>University of Waterloo</strong>.</p>
-[Christian Gorenflo Waterloo’s David R Cheriton School of Computer Science]
-
-<p> There are also now discussion groups working in this topic, in particular <strong>EURELECTRIC</strong>, where you can find a wealth of information about the topic.</p>
-
-<p> https://www.eurelectric.org/news/eurelectric-launches-expert-discussion-platform-on-blockchain </p>
- 
+		<p>There are also now discussion groups working in this topic, in particular <a class="link" href="https://www.eurelectric.org/news/eurelectric-launches-expert-discussion-platform-on-blockchain">EURELECTRIC</strong></a>, where you can find a wealth of information about the topic.</p>
 	</div>
-
 	<div class="col_4 last" style="background-color:#fff">
-	
 		<div>
 			<a href="https://www.evolving.money/EMoney/EBanking/challenger-comp.php"><div class="imgbox">
-				<img class="center-fit" src="../Images/Comp_chall_banks.png", alt="bank cards"></div></a>
+				<img class="center-fit" src="../../Images/Comp_chall_banks.png", alt="bank cards"></div></a>
 		</div>
-		
-		
+				
     		<div>
 			<a href="https://www.evolving.money/InvIdeas/FTFC.php"><div class="imgbox">
-				<img class="center-fit" src="../Images/Free_Trading.png", alt="Suited man watering a money tree"></div></a>
+				<img class="center-fit" src="../../Images/Free_Trading.png", alt="Suited man watering a money tree"></div></a>
 		</div>
-	
-		
+			
 		<div class="vertical_ad">
 			<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 			<!-- Native_vertical_banner -->
@@ -94,11 +75,10 @@ smart charging payments?</title>
 	
 	<div class="clear"></div>
 	
-
 	<div class="clear"></div>
 </div>
 	
-<?php include("../includes/footer.html");?>
+<?php include("../../includes/footer.html");?>
 
 </body>
 </html> 
