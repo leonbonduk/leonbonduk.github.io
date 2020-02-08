294febe88a20c4b1863bb1afdc5e56ef59e5a5f0
Jibber2000 <32431276+Jibber2000@users.noreply.github.com>
2020-02-08 17:47:45 +0000

    Create blockchain-in-EV-charging.php

---
+++ EvolvingMoney/EMoney/EV-blockchain-charging/blockchain-in-EV-charging.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>		
+	<meta charset="UTF-8">
+       	<title>Open Source Hedge Funds</title>	
+ 	<meta name="description" content="summary." />
+   	<meta name="keywords" content="Numerai, Open Source Hedge Fund, eMoney, technical analysis, payments systems, fintech" />
+       	<meta name="author" content="James Day" />
+	<meta name="robots" content="all">
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<link href="../css/styles.css" type="text/css" rel="stylesheet" />
+	<!-- <link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" /> !-->
+	<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/960-12-col.css" />
+
+	
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-157104566-1"></script>
+	<script>
+  	window.dataLayer = window.dataLayer || [];
+  	function gtag(){dataLayer.push(arguments);}
+  	gtag('js', new Date());
+  	gtag('config', 'UA-157104566-1');
+	</script>
+	
+</head>
+
+<body>
+	<?php include("../includes/test-header.html");?>
+	
+<div class="row">
+
+	<div class="col_8 first" style="background-color:#fff">	
+		<br>	 
+		<h1 class="h1_article">Open Source Hedge Funds!</h1>
+		<h3>James Day, 5th January 2020</h3>
+		<p><img class="article-img-fit" src="https://www.evolving.money/Images/numerai.jpg" alt="Numerai hexagonal logo at top of three skyscrapers" title="Numerai"> </p>
+		<h2> <strong>NumerAi</strong> the open-source hedge fund </h2>
+                <p> Open source is a term we hear from software, where the source code is freely available, but what does this have to do with hedge funds? <p>
+                <p> In quant hedge funds teams generate models for technical analysis of stock prices. Algorithms tend to do the larger proportion of the trading rather than a human trader.
+                    These algorithms are kept closely guarded, and are not something a normal hedge fund would share, but in the case of NumerAI, it is precisely these algorithms
+                    that are shared. <p>		 
+		<p> The reason that  <strong>NumerAi</strong> do this is to take advantage of the network effects of having many contributors providing algorithms in a competition format. 
+                    The crowd generated algorithms they end up with, are derived by much larger teams than any hedge fund could afford on their own,  and from a community of highly skilled 
+                    data scientists. Traditional algorithms used for trading were predominantly statistical models used for price prediction, but the <strong>NumerAi</strong> community  
+                    make use of the latest tools in data science and machine learning, an area that Hedge funds are only just breaking into.<p>
+                <p> The <strong>NumerAi</strong> team have incentivised the crowd to work for them in a competion based market place, and contributors compete to provide better algorithms. 
+                    This costs a lot less money than hiring people to do it. Pay is in a competition format so effectively they only have to pay the winners. Also their workforce 
+                    pay to be involved, and effectively crowd fund the hedge fund. It's a very creative solution that harnesses technology and the crowd together.<p>
+                <p> It's a bit like <strong>Wikipedia</strong> for technical analysis and stock market prediction models, combined with <strong>Crowdfunder</strong>...  Inspired.<p>
+                <p> In the background it uses Cryptocurrency, Blockchain and Smart Contracts to automatically pay everyone providing models, and to compute rankings and performance. Contributors can make a lot of money if they are good.<p>
+                <p> For me this is an example of things to come, the rise in importance of algorithms and AI / automation has been much discussed. A much more important trend potentially,
+                    at least in the short term, is likely to be the contributions of teams of people and computer algorithms working together... much discussed in the book <a class="link" target="_blank" href="https://www.amazon.co.uk/gp/product/1786074702/ref=as_li_tl?ie=UTF8&camp=1634&creative=6738&creativeASIN=1786074702&linkCode=as2&tag=evolvingmoney-21&linkId=0980827271bfdad9d3f2c5dc2665cb82">Superminds</a><img src="//ir-uk.amazon-adsystem.com/e/ir?t=evolvingmoney-21&l=am2&o=2&a=1786074702" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" /> 
+                    by Thomas W Malone. Numerai is precisely an example of this in action in FinTech.<p>
+	</div>
+
+	<div class="col_4 last" style="background-color:#fff">
+	
+		<div>
+			<a href="https://www.evolving.money/EMoney/EBanking/challenger-comp.php"><div class="imgbox">
+				<img class="center-fit" src="../Images/Comp_chall_banks.png", alt="bank cards"></div></a>
+		</div>
+		
+		
+    		<div>
+			<a href="https://www.evolving.money/InvIdeas/FTFC.php"><div class="imgbox">
+				<img class="center-fit" src="../Images/Free_Trading.png", alt="Suited man watering a money tree"></div></a>
+		</div>
+	
+		
+		<div class="vertical_ad">
+			<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+			<!-- Native_vertical_banner -->
+			<ins class="adsbygoogle"
+     			style="display:block"
+     			data-ad-client="ca-pub-3169172932709847"
+     			data-ad-slot="1100347920"
+     			data-ad-format="auto"
+     			data-full-width-responsive="true"></ins>
+			<script>
+     			(adsbygoogle = window.adsbygoogle || []).push({});
+			</script>
+		</div>
+	</div>
+	
+	<div class="clear"></div>
+	
+
+	<div class="clear"></div>
+</div>
+	
+<?php include("../includes/footer.html");?>
+
+</body>
+</html> 
