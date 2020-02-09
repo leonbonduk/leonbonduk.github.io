21a3581cc416f9dbde657820a0381ab09a99b024
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2020-02-09 20:18:53 +0000

    Update what-is-emoney.php

--- EvolvingMoney/EMoney/what-is-emoney.php
+++ EvolvingMoney/EMoney/what-is-emoney.php
@@ -44,7 +44,7 @@
 			e-commerce "frictionless" (<strong>Paypal, Amazon, Alibaba</strong>) have slaughtered the competition. It hasn't stopped with <strong>Amazon</strong> and <strong>Paypal</strong>, innovations continue. Payments systems like <strong>Transferwise</strong> 
 			compete with <strong>Western Union</strong>;  even <strong>Uber</strong> and <strong>AirBnB</strong> can in some sense be considered as frictionless payments systems. </p>		 
 			<p> The nature of the e-commerce market is changing, and the money we use may very well change alongside it.</p>
-			<p> One of the biggest changes on the horizon, is that currently the are roughly 3.2 billion people who are internet connected, but that number is predicted to double 
+			<p> One of the biggest changes on the horizon, is that currently there are roughly 3.2 billion people who are internet connected, but that number is predicted to double 
 			over the next 5-10 years. Many of these new entrants to global e-commerce are currently unbanked/underbanked and based in the developing world. They may choose to get bank accounts and credit cards, and setup paypal; but if it's easier and cheaper to do something else, then up to half of those on the internet could be doing something new in the future. </p>
 			<p> If that something new is easier and less hassle than what we have now, we may all end up using it in the future.<p>
 			<p> The other side of this story, is that we know where would be a good place to look for where new innovation is taking hold, emerging markets.</p>
