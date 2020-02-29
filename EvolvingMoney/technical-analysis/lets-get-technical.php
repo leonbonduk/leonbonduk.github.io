7f0c71b3bc6d438be9013c0ef64c4f34a418b28c
Jibber2000 <32431276+Jibber2000@users.noreply.github.com>
2020-02-29 14:16:19 +0000

    Update lets-get-technical.php

--- EvolvingMoney/technical-analysis/lets-get-technical.php
+++ EvolvingMoney/technical-analysis/lets-get-technical.php
@@ -1,1 +1,85 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+		<title>Evolving Money - What is E-Money</title>
+       		<meta name="description" content="Introduction to evolving nature of money due to online technology" />
+       		<meta name="keywords" content="emoney, new, crypto, banking" />
+       		<meta name="author" content="James Day" />
+		<meta name="robots" content="all">
+		<meta name="viewport" content="width=device-width,initial-scale=1" />
+		<link href="https://www.evolving.money/css/styles.css" type="text/css" rel="stylesheet" />
+		<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/960-12-col.css" />
+	
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-157104566-1"></script>
+		<script>
+  		window.dataLayer = window.dataLayer || [];
+  		function gtag(){dataLayer.push(arguments);}
+  		gtag('js', new Date());
+  		gtag('config', 'UA-157104566-1');
+		</script>
+</head>
+
+
+<body>
+
+	<?php include("../includes/test-header.html");?>
+
+<div>
+	<div class="row" style="margin: 0 0;">
+		<div class="col_12 first" style="background-color:#fff">
+			<div class="imgbox">
+    				<img class="left-fit-page-heading" src="https://www.evolving.money//Images/EMoney_logo.GIF" alt="EMoney title in blue and orange">
+			</div>
+		</div>
+	</div> 
+
+	<div class="clear"></div>
+
+	<div class="row">
+		<div class="col_8 first" style="background-color:#fff">
+
+			<p> One of the most exciting and least understood areas in finance is online purchasing and how it's still changing. Commerce has moved online, and those that have made 
+			e-commerce "frictionless" (<strong>Paypal, Amazon, Alibaba</strong>) have slaughtered the competition. It hasn't stopped with <strong>Amazon</strong> and <strong>Paypal</strong>, innovations continue. Payments systems like <strong>Transferwise</strong> 
+			compete with <strong>Western Union</strong>;  even <strong>Uber</strong> and <strong>AirBnB</strong> can in some sense be considered as frictionless payments systems. </p>		 
+			<p> The nature of the e-commerce market is changing, and the money we use may very well change alongside it.</p>
+			<p> One of the biggest changes on the horizon, is that currently there are roughly 3.2 billion people who are internet connected, but that number is predicted to double 
+			over the next 5-10 years. Many of these new entrants to global e-commerce are currently unbanked/underbanked and based in the developing world. They may choose to get bank accounts and credit cards, and setup paypal; but if it's easier and cheaper to do something else, then up to half of those on the internet could be doing something new in the future. </p>
+			<p> If that something new is easier and less hassle than what we have now, we may all end up using it in the future.<p>
+			<p> The other side of this story, is that we know where would be a good place to look for where new innovation is taking hold, emerging markets.</p>
+			<p> It's a strange thought, but in manufacturing it's a story that's played out before, riches to rust belt for some, and new wealth to others. In a similar way, 
+			finance that doesn't sufficiently innovate, could suffer a similar fate.</p>
+
+		</div>
+
+		<div class="col_4 last" style="background-color:#fff">
+			<div class="imgbox"><img class="center-fit" src="https://www.evolving.money/Images/EM_circ_logo.jpg" alt="Evolving Money totle in a circle of finacial logos"></div>
+		
+		
+			<div class="vertical_ad">
+				<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+				<!-- Native_vertical_banner -->
+				<ins class="adsbygoogle"
+     				style="display:block"
+     				data-ad-client="ca-pub-3169172932709847"
+     				data-ad-slot="1100347920"
+     				data-ad-format="auto"
+     				data-full-width-responsive="true"></ins>
+				<script>
+     				(adsbygoogle = window.adsbygoogle || []).push({});
+				</script>
+			</div>
+		</div>
+
+	</div>
+
+	<div class="clear"></div>
+
+</div>
+
+<?php include("../includes/footer.html");?>
+
+</body>
+</html>
