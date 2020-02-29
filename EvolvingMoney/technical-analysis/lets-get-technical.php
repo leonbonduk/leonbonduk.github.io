8724dba2dda8a4f5f14b513e4486a137dae90d79
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2020-02-29 15:09:26 +0000

    Update lets-get-technical.php

--- EvolvingMoney/technical-analysis/lets-get-technical.php
+++ EvolvingMoney/technical-analysis/lets-get-technical.php
@@ -41,17 +41,12 @@
 	<div class="row">
 		<div class="col_8 first" style="background-color:#fff">
 
-<p> A key objective of trading of stocks is to make money on the trades; either through appreciation (buying low and selling high), or by shorting (essentially a placing a kind of bet that the value of a stock will fall). </p>
-
-<p> Over time people have developed strategies to help them decide which stocks to trade and when to trade them; strategies that aim to increase the probability of making a successful trade. This subject is an art and a science and debates rage as to whether the underlying philosophy is valid, however there are people who use these strategies and <strong> beat the market <strong>, (make more money that if they just bought an held stock over time). There are also people who don’t… </p>
-
-<p> One of the key techniques is called <strong> Charting </strong> or <strong> Technical Analysis </strong>. The underlying philosophy of why it can work, is that even though the future is unwritten, people tend to behave in the same way and make similar decisions, when presented with the same kinds of price information. The technique aims to look at past price data, and establish patterns in that data that enable the trader to produce forecasts of where the stock price is more likely to go next, and this helps the trader to make more effective trades when the predictions from the technique are good.</p>
-
-<p> In this blog section we are going to look at charting and technical analysis, present the techniques and the terminology, and build some trading models. The performance of the models over past price data sets will be looked at; we can look at what makes a good model and why...</p>
-
-<p>Things will start simply with some <strong> basic models using moving averages and momentum </strong> to make trade decisions, then we will start to look at some more advanced techniques, where we get the computer to make the model for us using <strong> different ML techniques </strong>.</p>
-
-<p> It’s an interesting area, join us and see if we can <strong> out compute the market </strong>. </p>
+<p>A key objective of trading of stocks is to make money on the trades; either through appreciation (buying low and selling high), or by shorting (essentially a placing a kind of bet that the value of a stock will fall).</p>
+<p>Over time people have developed strategies to help them decide which stocks to trade and when to trade them; strategies that aim to increase the probability of making a successful trade. This subject is an art as well as a science and debates rage as to whether the underlying philosophy is valid, however there are people who use these strategies and <strong>beat the market<strong>, making more money than if they just bought and held the stock over time. There are also people who don’t…</p>
+<p>One of the key techniques is called <strong> Charting </strong> or <strong> Technical Analysis </strong>. The underlying philosophy of why it can work, is that even though the future is unwritten, people tend to behave in the same way and make similar decisions, when presented with the same kinds of price information. The technique aims to look at past price data, and establish patterns in that data that enable the trader to produce forecasts of where the stock price is more likely to go next, and this helps the trader to make more effective trades when the predictions from the technique are good.</p>
+<p>In this blog section we are going to look at charting and technical analysis, present the techniques and the terminology, and build some trading models. The performance of the models over past price data sets will be looked at; we can look at what makes a good model and why...</p>
+<p>Things will start simply with some <strong>basic models using moving averages and momentum</strong> to make trading decisions, then we will start to look at some more advanced techniques, where we get the computer to make the model for us using <strong>different ML techniques</strong>.</p>
+<p>It’s an interesting area, join us and see if we can <strong>out compute the market</strong>.</p>
 
 		</div>
 
