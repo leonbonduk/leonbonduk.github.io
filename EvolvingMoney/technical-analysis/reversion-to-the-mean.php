f8177296bee9f166cb183bcb5cebdd5a088a1ed2
Jibber2000 <32431276+Jibber2000@users.noreply.github.com>
2020-06-18 21:29:55 +0100

    Create reversion-to-the-mean.php

---
+++ EvolvingMoney/technical-analysis/reversion-to-the-mean.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+		<title>Reversion to the mean</title>
+       		<meta name="description" content="Introduction to charting technical analysis and price prediction" />
+       		<meta name="keywords" content=", technical-analysis, banking, trading" />
+       		<meta name="author" content="James Day" />
+		<meta name="robots" content="all">
+		<meta name="viewport" content="width=device-width,initial-scale=1" />
+		<link href="https://www.evolving.money/css/styles.css" type="text/css" rel="stylesheet" />
+		<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/960-12-col.css" />
+	
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-157104566-1"></script>
+		<script>
+  		window.dataLayer = window.dataLayer || [];
+  		function gtag(){dataLayer.push(arguments);}
+  		gtag('js', new Date());
+  		gtag('config', 'UA-157104566-1');
+		</script>
+</head>
+
+
+<body>
+
+	<?php include("../includes/test-header.html");?>
+
+<div>
+	<div class="row">
+		<div class="col_8 first" style="background-color:#fff">
+<h1 class="h1_article">Reversion to the mean</h1>
+
+		</div>
+
+		<div class="col_4 last" style="background-color:#fff">
+			<div class="imgbox"><img class="center-fit" src="https://www.evolving.money/Images/EM_circ_logo.jpg" alt="Evolving Money totle in a circle of finacial logos"></div>
+		
+		
+			<div class="vertical_ad">
+				<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+				<!-- Native_vertical_banner -->
+				<ins class="adsbygoogle"
+     				style="display:block"
+     				data-ad-client="ca-pub-3169172932709847"
+     				data-ad-slot="1100347920"
+     				data-ad-format="auto"
+     				data-full-width-responsive="true"></ins>
+				<script>
+     				(adsbygoogle = window.adsbygoogle || []).push({});
+				</script>
+			</div>
+		</div>
+
+	</div>
+
+	<div class="clear"></div>
+
+</div>
+
+<?php include("../includes/footer.html");?>
+
+</body>
+</html>
