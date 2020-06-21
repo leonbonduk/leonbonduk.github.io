fa86313bd5e09bfcbc060e39646f1fc9562000ba
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2020-06-21 23:14:02 +0100

    Update remittance-blockchain.php

--- EvolvingMoney/EMoney/remittance-blockchain.php
+++ EvolvingMoney/EMoney/remittance-blockchain.php
@@ -33,167 +33,142 @@
 		<br>	 
 		<h1 class="h1_article">Blockchain Remittance, locked down and here to stay?</h1>
 		<h3>James, 21st June 2020</h3>
-<p><img class="article-img-fit" src="https://www.evolving.money/Images/Smartphone_handshake.jpg" alt="Two smartphones shaking hands" title="Two smartphones shaking hands"> </p>
-<section>
+		<p><img class="article-img-fit" src="https://www.evolving.money/Images/Smartphone_handshake.jpg" alt="Two smartphones shaking hands" title="Two smartphones shaking hands"> </p>
+
+		<section>
+			<p>The rise of globalisation has been accompanied by a big growth in migrant workers. We can think of places like Dubai with may international migrant workers in the building and construction sector. There are examples everywhere, large global cities have better opportunities and many be people pursuing careers work abroad to take advantage of better job opportunities. It's become very common to work abroad for a while and see the world.</p>
+			<p>The payments people send home from abroad are called <strong> remittance payments </strong>. There are lots of different options for sending money. Directly sending money between two banks in different countries can be expensive, you have transfer fees and exchange rates to pay.</p>
+			<p>There has long been a view that blockchain based and other innovative web based electronic payments systems would and could offer a competitive advantage in remittance payments, allowing people to move money across boarders at lower cost.</p>
+			<p><strong>Currently globalisation is taking a different flavour with a pandemic, & rolling global lock-down </strong>,</p> 
+			<p>The crisis brings some interesting changes to market conditions. Firstly air travel is likely to be a rarer event going forward. We may also see changes in behaviour of the migrant labour force in terms of their willingness to stay abroad or take jobs abroad in the future.</p>
+			<p>If people are less willing to travel, we may well see more of a shift to <strong>"international work at a distance"</strong>. </p>
+			<p>For many cash will be tighter than before, but business must go on, <strong>"the machine can't stay stopped"</strong>... a few key questions that many people may be asking going forward...  </p>
+		</section>
+
+		<section>
+			<ul>
+				<ol>How do I continue to keep doing business globally, against a backdrop of reduced international travel, and tightening budgets?</ol>
+				<ol>How do I make efficiencies in day to day business activities to maintain my profitability?</ol>
+				<ol>How do I get new global business if I can't travel very easily?</ol>
+			</ul>
+		</section>
+
+		<section>
+			<p>We can't answer all the questions in this segment, but the efficiency question is a good one to start with.</p>
+			<p>There are many options for remittance payments, we have looked at some of the more popular options that exist for remittance.</p>
+			<br>
+			<br>
+			<heading>
+				<h2><u>Conventional fintech cross boarder payments solutions:</u></h2>
+			</heading>
+
+			<heading>
+				<h3>Inter-Bank Transfer</h3>
+			</heading>
+				<p>The conventional way of transferring money to someone in another country is to make an international bank transfer from your bank to their bank. The issue with this is that the fees tend to be high, and companies have arisen to provide more cost effective alternatives</p>
+			<heading>
+				<h3>Money transfer company</h3>
+			</heading>
+
+			<table style = "width:100%">
+				<tr>
+					<th> <u> Company </u> </th>
+					<th> <u> Product overview </u> </th>
+					<th> <u> Date founded </u> </th>
+				</tr>
+				<tr>
+					<th> Western Union </th>
+					<th> Online transfer, in shop transfer. </th>
+					<th> 1851 </th>
+				</tr>
+
+				<tr>
+					<th> Moneygram </th>
+					<th> Online transfer, in shop transfer. </th>
+					<th> 1940 </th>
+				</tr>
+
+				<tr>
+					<th> MoneyCorp </th>
+					<th> Online transfer, in shop transfer. </th>
+					<th> 1962 </th>
+				</tr>
+
+				<tr>
+					<th> TransferWise </th>
+					<th> Online transfer only (phone app and web app) </th>
+					<th> 2011 </th>
+				</tr>
+
+				<tr>
+					<th> Remitly </th>
+					<th> Online transfer only (phone app and web app) </th>
+					<th> 2011 </th>
+				</tr>
+			</table>
+
+			<style>
+				table, th, td {
+ 				border: 1px solid black;
+				}
+			</style>
+			<br>
+			<br>
+			<p><strong>Western Union </strong> has been around a long time, and is one of the most common remittance payments methods. People can transfer money even without a bank in store. It also has an online presence. <strong>Transferwise</strong> and <strong>Remitly</strong> are more recent online only offerings. They brought very competitive pricing from the different ways in which they operate. The healthy competition has meant all platforms tend to offer good online prices that are generally cheaper than bank transfers. It's worth comparing and checking though... where you are sending the money too can really make a difference on the different platforms.</p>
+			<heading>
+				<h2><u>Emergent fintech cross boarder payments solutions:</u></h2>
+			</heading>
+
+			<table style = "width:100%">
+				<tr>
+					<th> <u> Company </u> </th>
+					<th> <u> Product overview </u> </th>
+					<th> <u> Date founded </u> </th>
+				</tr>
+
+				<tr>
+					<th> BitPesa </th>
+					<th> Online transfer and text and email payments </th>
+					<th> 2013 </th>
+				</tr>
+
+				<tr>
+					<th> WeSwap </th>
+					<th> Online transfer only (phone app and web app) </th>
+					<th> 2013 </th>
+				</tr>
+
+				<tr>
+					<th> Revolut </th>
+					<th> Online transfer only (phone app and web app) </th>
+					<th> 2015 </th>
+				</tr>
+
+			</table>
+
+			<style>
+				table, th, td {
+				border: 1px solid black;
+				}
+			</style>
+
+			<p>Blockchain enabled systems are a much more recent addition. They are not yet mainstream but offer a strong competitive edge in niece markets. In parts of the world where the traditional banking system is not strong, the blockchains can often be the cheapest (and sometimes only) solution. The underlying infrastructure required is a mobile phone and open source software. It's a great solution because of the low infrastructure requirement, and minimal need for staff or training to provide these systems.</p>
+			<p>Web and phone applications provide a glossy front end to payments over the blockchain, the users don't even really need to know that blockchain is used. <strong> BitPesa </strong> has a very strong presence in central Africa, and is leading the way in terms of innovation in Fintech in that emergent market.</p>
+
+		</section> 
+	</div>
 	
-<p> The rise of globalisation has been accompanied by a big growth in migrant workers. We can think of places like Dubai with may international migrant workers in the building and construction sector. There are examples everywhere, large global cities have better opportunities and many be people pursuing careers work abroad to take advantage of better job opportunities. It's become very common to work abroad for a while and see the world. </p>
-
-<p>The payments people send home from abroad are called <strong> remittance payments </strong>. There are lots of different options for sending money. Directly sending money between two banks in different countries can be expensive, you have transfer fees and exchange rates to pay. </p>
-
-<p> There has long been a view that blockchain based and other innovative web based electronic payments systems would and could offer a competitive advantage in remittance payments, allowing people to move money across boarders at lower cost.</p>
-
-<p> <strong> Currently globalisation is taking a different flavour with a pandemic, & rolling global lock-down </strong>,</p> 
-
-<p> The crisis brings some interesting changes to market conditions. Firstly air travel is likely to be a rarer event going forward. We may also see changes in behaviour of the migrant labour force in terms of their willingness to stay abroad or take jobs abroad in the future. </p>
-
-<p>If people are less willing to travel, we may well see more of a shift to <strong>"international work at a distance"</strong>. </p>
-
-<p> For many cash will be tighter than before, but business must go on, <strong>"the machine can't stay stopped"</strong>... a few key questions that many people may be asking going forward...  </p>
-
-</section>
-
-<section>
-
-<ul>
-<ol>How do I continue to keep doing business globally, against a backdrop of reduced international travel, and tightening budgets?</ol>
-<ol>How do I make efficiencies in day to day business activities to maintain my profitability?</ol>
-<ol>How do I get new global business if I can't travel very easily?</ol>
-</ul>
-</section>
-
-<section>
-<p> We can't answer all the questions in this segment, but the efficiency question is a good one to start with. </p>
-
-<pp>There are many options for remittance payments, we have looked at some of the more popular options that exist for remittance. </p>
-
-<br/>
-<br/>
-
-
-<heading>
-<h2><u>Conventional fintech cross boarder payments solutions:</u></h2>
-</heading>
-
-<heading>
-<h3>Inter-Bank Transfer</h3>
-</heading>
-<pp>The conventional way of transferring money to someone in another country is to make an international bank transfer from your bank to their bank. The issue with this is that the fees tend to be high, and companies have arisen to provide more cost effective alternatives</p>
-
-<heading>
-<h3>Money transfer company</h3>
-</heading>
-
-<table style = "width:100%">
-
-<tr>
-<th> <u> Company </u> </th>
-<th> <u> Product overview </u> </th>
-<th> <u> Date founded </u> </th>
-</tr>
-
-<tr>
-<th> Western Union </th>
-<th> Online transfer, in shop transfer. </th>
-<th> 1851 </th>
-</tr>
-
-<tr>
-<th> Moneygram </th>
-<th> Online transfer, in shop transfer. </th>
-<th> 1940 </th>
-</tr>
-
-
-
-<tr>
-<th> MoneyCorp </th>
-<th> Online transfer, in shop transfer. </th>
-<th> 1962 </th>
-</tr>
-
-<tr>
-<th> TransferWise </th>
-<th> Online transfer only (phone app and web app) </th>
-<th> 2011 </th>
-</tr>
-
-<tr>
-<th> Remitly </th>
-<th> Online transfer only (phone app and web app) </th>
-<th> 2011 </th>
-</tr>
-
-
-</table>
-
-<style>
-table, th, td {
- border: 1px solid black;
-}
-</style>
-
-<br/>
-<br/>
-
-<p> <strong> Western Union </strong> has been around a long time, and is one of the most common remittance payments methods. People can transfer money even without a bank in store. It also has an online presence. <strong>Transferwise</strong> and <strong>Remitly</strong> are more recent online only offerings. They brought very competitive pricing from the different ways in which they operate. The healthy competition has meant all platforms tend to offer good online prices that are generally cheaper than bank transfers. It's worth comparing and checking though... where you are sending the money too can really make a difference on the different platforms. </p>
-<heading>
-<h2><u>Emergent fintech cross boarder payments solutions:</u></h2>
-</heading>
-
-<table style = "width:100%">
-
-<tr>
-<th> <u> Company </u> </th>
-<th> <u> Product overview </u> </th>
-<th> <u> Date founded </u> </th>
-</tr>
-
-<tr>
-<th> BitPesa </th>
-<th> Online transfer and text and email payments </th>
-<th> 2013 </th>
-</tr>
-
-
-<tr>
-<th> WeSwap </th>
-<th> Online transfer only (phone app and web app) </th>
-<th> 2013 </th>
-</tr>
-
-<tr>
-<th> Revolut </th>
-<th> Online transfer only (phone app and web app) </th>
-<th> 2015 </th>
-</tr>
-
-</table>
-
-<style>
-table, th, td {
- border: 1px solid black;
-}
-</style>
-
-<p>Blockchain enabled systems are a much more recent addition. They are not yet mainstream but offer a strong competitive edge in niece markets. In parts of the world where the traditional banking system is not strong, the blockchains can often be the cheapest (and sometimes only) solution. The underlying infrastructure required is a mobile phone and open source software. It's a great solution because of the low infrastructure requirement, and minimal need for staff or training to provide these systems.</p>
-
-<p>Web and phone applications provide a glossy front end to payments over the blockchain, the users don't even really need to know that blockchain is used. <strong> BitPesa </strong> has a very strong presence in central Africa, and is leading the way in terms of innovation in Fintech in that emergent market. </p>
-
-</section> 
 	<div class="col_4 last" style="background-color:#fff">
-	
 		<div>
 			<a href="https://www.evolving.money/EMoney/EBanking/challenger-comp.php"><div class="imgbox">
 				<img class="center-fit" src="../Images/Comp_chall_banks.png", alt="bank cards"></div></a>
 		</div>
-		
-		
+				
     		<div>
 			<a href="https://www.evolving.money/InvIdeas/FTFC.php"><div class="imgbox">
 				<img class="center-fit" src="../Images/Free_Trading.png", alt="Suited man watering a money tree"></div></a>
 		</div>
-	
-		
+			
 		<div class="vertical_ad">
 			<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 			<!-- Native_vertical_banner -->
