8890a71049424a7794244ab095a7fa9280d81c7a
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2020-06-21 22:00:03 +0100

    Update remittance-blockchain.php

--- EvolvingMoney/EMoney/remittance-blockchain.php
+++ EvolvingMoney/EMoney/remittance-blockchain.php
@@ -32,28 +32,20 @@
 	<div class="col_8 first" style="background-color:#fff">	
 		<br>	 
 		<h1 class="h1_article">Blockchain Remittance, locked down and here to stay?</h1>
-		<h3>James Day, 21st June 2020</h3>
-
+		<h3>James, 21st June 2020</h3>
+<p><img class="article-img-fit" src="https://www.evolving.money/Images/Smartphone_handshake.jpg " alt="Two smartphones shaking hands" title="Two smartphones shaking hands"> </p>
 <section>
 	
 <p> The rise of globalisation has been accompanied by a big growth in migrant workers. We can think of places like Dubai with may international migrant workers in building and construction sector. There are examples everywhere, large global cities have better opportunities and many be people pursuing careers work abroad to take advantage of better job opportunities. It's become very common to work abroad for a while and see the world. </p>
-
 <p>The payments people send home from abroad are called <strong> remittance payments </strong>. There are lots of different options for sending money. Directly sending money between two banks in different countries can be expensive, you have transfer fees and exchange rates to pay. </p>
-
 <p> There has long been a view that blockchain based and other innovative web based electronic payments systems would and could offer a competitive advantage in remittance payments, allowing people to move money across boarders at lower cost.</p>
-
 <p> <strong> Currently globalisation is taking a different flavour with a pandemic, & rolling global lock-down </strong>,</p> 
-
 <p> The crisis brings some interesting changes to market conditions. Firstly air travel is likely to be a rarer event going forward. We may also see changes in behaviour of the migrant labour force in terms of their willingness to stay abroad or take jobs abroad in the future. </p>
-
 <p>If people are less willing to travel, we may well see more of a shift to <strong>"international work at a distance"</strong>. </p>
-
 <p> For many cash will be tighter than before, but business must go on, <strong>"the machine can't stay stopped"</strong>... a few key questions that many people may be asking going forward...  </p>
-
 </section>
 
 <section>
-
 <ul>
 <ol>How do I continue to keep doing business globally, against a backdrop of reduced international travel, and tightening budgets?</ol>
 <ol>How do I make efficiencies in day to day business activities to maintain my profitability?</ol>
@@ -63,13 +55,9 @@
 
 <section>
 <p> We can't answer all the questions in this segment, but the efficiency question is a good one to start with. </p>
-
-<pp>There are many options for remittance payments, we have looked at some of the more popular options that exist for remittance. </p>
-
+<p>There are many options for remittance payments, we have looked at some of the more popular options that exist for remittance. </p>
 <br/>
 <br/>
-
-
 <heading>
 <h2><u>Conventional fintech cross boarder payments solutions:</u></h2>
 </heading>
@@ -77,12 +65,10 @@
 <heading>
 <h3>Inter-Bank Transfer</h3>
 </heading>
-<pp>The conventional way of transferring money to someone in another country is to make an international bank transfer from your bank to their bank. The issue with this is that the fees tend to be high, and companies have arisen to provide more cost effective alternatives</p>
-
+<p>The conventional way of transferring money to someone in another country is to make an international bank transfer from your bank to their bank. The issue with this is that the fees tend to be high, and companies have arisen to provide more cost effective alternatives</p>
 <heading>
 <h3>Money transfer company</h3>
 </heading>
-
 <table style = "width:100%">
 
 <tr>
@@ -103,8 +89,6 @@
 <th> 1940 </th>
 </tr>
 
-
-
 <tr>
 <th> MoneyCorp </th>
 <th> - </th>
@@ -123,7 +107,6 @@
 <th> 2011 </th>
 </tr>
 
-
 </table>
 
 <style>
@@ -131,10 +114,8 @@ table, th, td {
  border: 1px solid black;
 }
 </style>
-
 <br/>
 <br/>
-
 <p> Western Union has been around a long time, and is one of the most common remittance payments methods. People can transfer money even without a bank in store. It also has an online presence. Transferwise and Remitly are more recent online only offerings. They brought very competitive pricing from the different ways in which the operate. The healthy competition has meant all platforms tend to offer good online prices that are generally cheaper than bank transfers. It's worth comparing an checking though, where you are sending the money can really make a difference on the different platforms. 
 
 <heading>
