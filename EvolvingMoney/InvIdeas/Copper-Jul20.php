9f6fba6f6599369c782847bf922df8eaa5741d17
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2020-07-06 13:14:05 +0100

    Create Copper-Jul20.php

---
+++ EvolvingMoney/InvIdeas/Copper-Jul20.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>		
+	<meta charset="UTF-8">
+       	<title>Ok! So a black swan came along and gave copper a bad case of man flu!</title>	
+ 	<meta name="description" content="Sell-off and quick rebound in the price of copper due to the coronavirus but is there potential further opportunity?" />
+   	<meta name="keywords" content="Copper, etf, contrarian, buying opportunity, emerging markets, markets, commodities, WisdomTree Copper, COPA" />
+       	<meta name="author" content="Leon Bond" /> 
+	<meta name="robots" content="all">
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<link href="../css/styles.css" type="text/css" rel="stylesheet" />
+	<!-- <link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" /> !-->
+	<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/960-12-col.css" />
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-157104566-1"></script>
+	<script>
+  	window.dataLayer = window.dataLayer || [];
+  	function gtag(){dataLayer.push(arguments);}
+  	gtag('js', new Date());
+  	gtag('config', 'UA-157104566-1');
+	</script>
+  
+</head>
+
+<body>
+	<?php include("../includes/test-header.html");?>
+	
+<div class="row">
+
+	<div class="col_8 first" style="background-color:#fff">	
+		<br>	 
+		<h1 class="h1_article">Ok! So a black swan came along and gave copper a bad case of man flu!</h1>
+		<h3>Leon, 5th July 2020, Wisdomtree Copper ETF (COPA), Price $24.62</h3>
+		<p><img class="article-img-fit" src="https://www.evolving.money/Images/copperman_EM.jpg" alt="Metal statue of man with head in his hands" title="Metal man with man flu"> </p>
+    <p>I completely misjudged the severity of the impact of covid-19 and so it’s impact on the world economy back at the end of January!</p>
+    <p>On 28th Jan copper was trading at $2.58 /lb but although the price increased throughout February and early March it dramatically fell from 6th March as the world went into lockdown to a low on 23rd March of $2.10/lb (COPA $19.185)!</p>
+    <p>From that date, however, the copper price has rocketed back, like a man recovering from a severe case of man flu and now stands at $2.71/ lb as of 3rd July, 5% above the price on the original mistimed article. This has probably been driven by several reasons I will briefly discuss…</p>
+    <h2>Supply</h2>
+    <p>Copper stocks measured by the Shanghai Futures exchange rose from 124k tonnes at the start of the year to 380k tonnes in March, although seasonally this happens over the Chinese new year, it will have also been significantly driven by the slow down caused by Covid-19 resulting in an oversupply of Copper during that period. Now however, stock levels are back down below the position at start of the year and now run at 114k tonnes as the Chinese economy has roared back to life.</p>
+    <p>As discussed in the previous article, despite Chinese warehouses filling up throughout February and March due to factory output stalling under lockdown conditions, supply constraints from already depleting South American copper mines will have been exacerbated by fears of temporary reductions in supply in the short term caused by the pandemic ravaging through those countries. The pandemic will likely also restrict future supply due to probable delayed investment in new mining projects given the current uncertain situation.</p>
+    <h2>Demand</h2>
+    <p>The strength of the rebound in the Chinese economy of course has taken everyone by surprise. This has been helped by the Chinese government increasing support to the construction industry issuing substantial bonds of 2.6trillion Yuan (circa £300bn) in local and central government bonds in order to do so.</p>
+    <p>Fitch forecasts China will see flat growth in copper consumption in 2020 which is a great improvement over the fears of a contraction due to Covid-19, especially in light of the falling supply and so should help tighten the market due to supply and demand mechanics</p>
+    <p>Even before the current crisis it was thought that demand for copper would increase due to the requirements in the manufacture of electric cars and renewable energy generation. Before the crisis McKinsey was forecasting demand could increase by up to 43% in the next 15 years!</p>
+    <p>There are now musings from and external calls on various governments that they should take the opportunity of the current economic downturn to stimulate their economies through further investment in both these areas. Therefore as these countries also come out of lockdown this could boost demand even further in the coming months, squeezing the copper price to even greater heights.</p>
+    <h2>Opportunity</h2>
+    <p>Currently I am back in profit now that price has returned above that of my previous article and will continue to hold, closely monitoring how the market is behaving in the coming weeks to ensure supply and demand mechanics continue to play out as I hope. I invested via an exchange traded fund (“ETF”) called WisdomTree Copper as described in the how to trade box below. There are however several other ETF products available from WisdomTree, not just a pure replication of the copper price:</p>
+    <ul class="Glossary">
+      <li>•	Normal replication of the copper price: ticker LSE:COPA</li>
+      <li>•	A more leveraged position can be taken so as to magnify the effect of any price changes but this can be either to your advantage on the copper price increasing or magnify your losses if the price were to fall: ticker LSE:LCOP for 2x leverage or LSE:3HCl for 3x leverage</li>
+      <li>•	If you however feel the market is going to fall you could utilise a short version of the fund to gain from any fall in the price of copper: ticker LSE:SCOP</li>
+      <li>•	Or again a leveraged version of short to magnify the effect of the falling price is available: ticker LSE:3HCS </li>
+  	<div style="background-color:#8a8a8a" >
+			<p><strong>How to trade:</strong><br><br>As this impact in the copper price appears to be more on the price of copper rather than any impact currently on copper miner's share prices the best and cheapest way to play such a short term market opportunity like this is via an ETC (an Exchange Traded Commodity which works in a similar manner to an Exchange Traded Fund or ETF), which are usually passively managed and so have low management fees. As they are traded on a stock exchange they usually also have a smaller spread when buying or selling compared to a standard unit trust fund. It is also best to make sure a reputable provider is choosen to ensure counter party risk is minimised, i.e. risk that they will go bust is minimised. I have decided to take a small stake in WisdomTree's Copper ETC trading under the ticker COPA which attempts to replciate the performance in the copper price by tracking the Bloomberg Copper Subindex through the use of swaps with the investment banks of Citigroup and Merrill Lynch and charges a management fee of only 0.5%.<br><br>Synthetic replication is riskier than through buying the commodity directly but with commidities it is harder and more expensive to hold the asset directly than with equities in an ETF. As the conterparties are reputable and with strong credit ratings the risk should still be fairly minimal however.<br><br>You should be able to buy this ETC through most online brokers in a similar manner to a normal company share, just search for the ticker COPA on your broker's trading screen.<br>  
+			</p>
+		</div>
+	
+	</div>
+
+	<div class="col_4 last" style="background-color:#fff">
+			
+		<div>
+			<p>Latest ETF price:</p>
+		</div>
+	
+		<div class="chart_container">
+			<div class="chart_chart">
+<!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container">
+  <div class="tradingview-widget-container__widget"></div>
+  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/markets/futures/" rel="noopener" target="_blank"><span class="blue-text">Commodities</span></a> by TradingView</div>
+  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-market-overview.js" async>
+  {
+  "colorTheme": "light",
+  "dateRange": "12m",
+  "showChart": true,
+  "locale": "en",
+  "width": "100%",
+  "height": "100%",
+  "largeChartUrl": "",
+  "isTransparent": false,
+  "plotLineColorGrowing": "rgba(33, 150, 243, 1)",
+  "plotLineColorFalling": "rgba(33, 150, 243, 1)",
+  "gridLineColor": "rgba(233, 233, 234, 1)",
+  "scaleFontColor": "rgba(120, 123, 134, 1)",
+  "belowLineFillColorGrowing": "rgba(33, 150, 243, 0.12)",
+  "belowLineFillColorFalling": "rgba(33, 150, 243, 0.12)",
+  "symbolActiveColor": "rgba(33, 150, 243, 0.12)",
+  "tabs": [
+    {
+      "title": "Commodities",
+      "symbols": [
+        {
+          "s": "LSE:COPA"
+        }
+      ],
+      "originalTitle": "Commodities"
+    }
+  ]
+}
+  </script>
+</div>
+<!-- TradingView Widget END -->
+			</div>
+		</div>
+	
+		<div>
+			<a href="https://www.evolving.money/EMoney/EBanking/challenger-comp.php"><div class="imgbox">
+				<img class="center-fit" src="../Images/Comp_chall_banks.png", alt="bank cards"></div></a>
+		
+		</div>
+    
+    <div>
+			<a href="https://www.evolving.money/InvIdeas/FTFC.php"><div class="imgbox">
+				<img class="center-fit" src="../Images/Free_Trading.png", alt="Suited man watering a money tree"></div></a>
+		
+		</div>
+	
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+			<!-- Native_vertical_banner -->
+			<ins class="adsbygoogle"
+     			style="display:block"
+     			data-ad-client="ca-pub-3169172932709847"
+     			data-ad-slot="1100347920"
+     			data-ad-format="auto"
+     			data-full-width-responsive="true"></ins>
+			<script>
+     			(adsbygoogle = window.adsbygoogle || []).push({});
+			</script>
+      
+	</div>
+	
+	<div class="clear"></div>
+	
+	
+	
+	<div class="clear"></div>
+</div>
+	
+<?php include("../includes/footer.html");?>
+
+</body>
+</html> 
