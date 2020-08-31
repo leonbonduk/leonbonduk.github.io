c32be2cf3adfa35267a9228823571319ce38f15a
leonbonduk <48361937+leonbonduk@users.noreply.github.com>
2020-08-31 15:08:46 +0100

    Create PropertyPartner.php

---
+++ EvolvingMoney/InvIdeas/PropertyPartner.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>		
+	<meta charset="UTF-8">
+       	<title>Property crowdfunding with Property Partner</title>	
+ 	<meta name="description" content="Dabble in but-to-let property investing with out the hassles and in tax efficient structure through the crow funding webiste Property Partner" />
+   	<meta name="keywords" content="Property Invetsment, Buy-to-let, buy to let, crowdfunding, Property Partner" />
+       	<meta name="author" content="Leon Bond" />
+	<meta name="robots" content="all">
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<script data-ad-client="ca-pub-3169172932709847" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<link href="../css/styles.css" type="text/css" rel="stylesheet" />
+	<!-- <link rel="stylesheet" href="https://www.evolving.money/css/new-header.css" /> !-->
+	<link rel="stylesheet" type="text/css" href="https://www.evolving.money/css/960-12-col.css" />
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-157104566-1"></script>
+	<script>
+  	window.dataLayer = window.dataLayer || [];
+  	function gtag(){dataLayer.push(arguments);}
+  	gtag('js', new Date());
+  	gtag('config', 'UA-157104566-1');
+	</script>
+	
+</head>
+
+<body>
+	<?php include("../includes/test-header.html");?>
+	
+<div class="row">
+
+	<div class="col_8 first" style="background-color:#fff">	
+		<br>	 
+		<h1 class="h1_article">Property crowdfunding with Property Partner</h1>
+		<h3>Leon, 31st August 2020</h3>
+		<p><img class="article-img-fit" src="https://www.evolving.money/Images/spilled_cash_EM.jpg" alt="Coins spilling out of a jar" title="Cashback websites"> </p>
+		<p>A lot of people have a dream of building a property empire but at the start are daunted by the stresses of choosing a property, finding enough funds to put a deposit down, dealing with, finding and then placating tenants etc. It does not help also that the government have made it a lot less tax efficient to own second properties directly, reducing the tax deductibility of mortgage interest and hiking stamp duty. But on the other end of the spectrum investing into a property fund is so far removed that most of the excitement of investing into property has been removed.</p>
+		<p>There is however a middle ground of online property crowdfunding platforms, such as Property Partner, whereby you can invest in smaller lump sums with multiple other investors, choosing specific individual properties where you are able to pick and choose to invest into each property based on which investment cases you believe will give the best return. The platform then manages both the investment process and the ongoing maintenance and administration of the property. Recent tax changes are also circumvented as the properties are owned by an incorporated company, you buy shares in the company but effectively own the same proportion of the property as shares in the company. Such a structure then avoids the restrictions on mortgage interest relieve of direct ownership of a buy-to-let property.</p>
+    <p>You do, however, sacrifice some element of control such as not being able to choose tenants or have a say in whether or not to make repairs to a property (which are usually funded by a provision deducted from the rental income).</p>
+    <p>But on the plus side there are also many advantages such as it being possible to liquidate your investment much quicker than a normal property due to a secondary market that is available on the platform where fellow investors can buy your shares off you.</p>
+		<h2>Investments available</h2>
+		<p>Property Partner provides a range of potential properties across the UK including residential houses, blocks of flats, student properties, commercial properties as well as the chance to invest in short term, hight interest development loans (typically at a rate of 10%) for property developers. It is also possible to invest via a managed investment plan which spreads your funds over multiple properties for you to spread your risk. An ISA wrapper is also available for investing into the property loans.</p>
+    <p>Whilst there are some houses on the platform, most investments available are multi-tenanted properties which I prefer as investment in such properties helps minimise the impact on income received of any tenant void periods.</p>
+    <p>Although dividends have been frozen currently due to the current uncertainty with the Covid situation (and payment holidays have also been negotiated with the banks on interest payments) dividends received typically ranged from 3% to 7%. There is then also the potential for any capital gains…</p>
+		<h2>Mechanics of the platform</h2>
+    <ol class="Glossary">
+      <li>Property Partner identifies a property and negotiates terms and an offer price with the seller.</li>
+      <li>They then list the potential property on the platform to seek out investors to purchase the property.</li>
+      <li>Investors offer funds on a first come first served basis until 100% of the investment is funded</li>
+      <li>The property is then purchased by a newly incorporated company that is solely dedicated to the ownership of that one property, and the investors take shares in that company.</li>
+      <li>Most properties are already tenanted but Property Partner takes on maintenance arrangement for the property and subsequent administration of the tenants.</li>
+      <li>Rental income net of any running expenses and funds held back as a provision for rainy day expenses are paid out as monthly dividends.</li>
+      <li>Any capital gains are reflected in the valuation movement and can be crystallised by selling in the online market place or waiting for the 5 year life of the investment to run its course at which point the shareholders vote as to whether to sell out in the open market at which point the shares are either offered on the platform to other investors or the property is sold off platform.</li>
+      <li>Exiting the investment can happen in one of two ways:</li>
+        <ol class="Glossary">
+          <li>Either use the secondary market to set a price at which you wish to sell out, setting either a low price for a quick sale or higher price if you do not mind biding your time.</li>
+          <li>At the end of the 5-year investment cycle for the property shareholders vote on whether they wish to retain the investment. The platform will then attempt to complete any sell out action on the secondary market otherwise the entire property will be sold on the open market if a majority wish to sell out.</li>
+        </ol>
+     </ol>
+
+
+
+
+
+<ul class="Glossary">
+			<li>eBay: 2.4% at <a class="link" href="https://www.quidco.com/raf/9781321/">Quidco</a> and special offer of up to 5% at <a class="link" href="https://www.topcashback.co.uk/ref/leonbond">TopCashback</a> for a limited time</li>
+			<li>Amazon:  2 to 10% at both <a class="link" href="https://www.quidco.com/raf/9781321/">Quidco</a> and <a class="link" href="https://www.topcashback.co.uk/ref/leonbond">TopCashback</a></li>
+			<li>Sainsburys online order: £1.20 at both <a class="link" href="https://www.quidco.com/raf/9781321/">Quidco</a> and <a class="link" href="https://www.topcashback.co.uk/ref/leonbond">TopCashback</a></li>
+			<li>Domino’s Pizza: £1.20 at <a class="link" href="https://www.topcashback.co.uk/ref/leonbond">TopCashback</a></li>
+		</ul>
+		<p>The two largest players in the game are TopCashback and Quidco but there are many other smaller companies who provide similar services. Both allow you to sign up with no upfront fees, although both also provide premium accounts which provide additional benefits such as boosted levels of cashback for a small fee.</p>
+		<h2>What’s in it for the cashback sites?</h2>
+		<p>The cashback sites are effectively passing on referral or affiliate fees provided by the retailers, with TopCashback and Quidco making a name for themselves for passing on the most, with claims that 100% is typically passed on. Due to their size, however, and so the significant business they put retailers way they are also paid bonuses by the retailers and so this is where they make their money.</p>
+		<h2>Quidco Vs TopCashback</h2>
+		<p>Quidco made it big slightly ahead of TopCashback and pays similar levels of cashback to it’s competitor. They both quite often have exclusive deals with retailers so it can be best to check both out to see who is offering the best rate at the time, particularly on larger transactions. Quidco does have a “price guarantee” however but if you lead a busy life then you probably won’t have time to pull them up on it…</p>
+		<p>TopCashback claim to pay out 105% of commissions generated as they pay out an element of their bonus as well so on average slightly better rates can be achieved with this site over Quidco.</p>
+		<p>They both provide a basic membership for free as well as an upgraded membership, so you can try them out for no cost, but if you do a lot of online shopping then ultimately it will likely be worthwhile to upgrade to a premium account.</p>
+		<h2>Downsides</h2>
+		<p>Sometimes transactions do not track through to payout for various reasons such as:</p>
+		<ul class="Glossary">
+			<li>If you visit the retailer through other links, then other referral cookies can be saved to your computer and so if the cashback website cookie is not the winning cookie then the cashback will be declined.</li>
+			<li>If you purchase on someone else’s online account at the retailer rather than in your own name then this can also cause issues.</li>
+		</ul>
+		<p>Most retailers provide cashback to returning customers, albeit usually at reduced rates, but some provide only for first transaction of a new customer.</p>
+		<p>Although there are thousands of retailers signed up, it may be cheaper to buy at another retailer who do not provide cashback so still ensure you shop around for the best deal. Don’t rely on the cashback paying out as a deciding factor as on rare occasions it can fail with minimal ability to chase down any rights you may have to that cashback. The cashback sites do provide facilities to dispute any unreceived cashback but unfortunately this mechanism does not always result in a resolution in your favour. The cashback website will also be out of pocket for non-payment as well so it is in their interests totry and resolve if they can.</p>
+		<h2>Sign-up bonus</h2>
+		<p>If you use my referral links below rather than signing up directly then you will also get an additional sign-up cash bonus (and I will get similar sized bonus to help pay for my musings on this website). I have already received over £600 of cashback from my own spending over last few years without religiously using the apps so hard to justify not using the apps:</p>
+		<ul class="Glossary">
+			<li>£10.00 sign up bonus for both of us if you earn £5 of other cashback at <a class="link" href="https://www.quidco.com/raf/9781321/">Quidco</a></li>
+			<li>£7.50 sign up bonus for both of us if you earn £10 of other cashback at <a class="link" href="https://www.topcashback.co.uk/ref/leonbond">TopCashback</a></li>
+		</ul>
+	</div>
+
+	<div class="col_4 last" style="background-color:#fff">
+	
+		<div class="vertical_ad">
+			<a href="https://www.evolving.money/EMoney/EBanking/challenger-comp.php"><div class="imgbox">
+				<img class="center-fit" src="../Images/Comp_chall_banks.png", alt="bank cards"></div></a>
+		
+		</div>
+	
+		<div>
+			<a href="https://www.evolving.money/InvIdeas/FTFC.php"><div class="imgbox">
+				<img class="center-fit" src="../Images/Free_Trading.png", alt="Suited man watering a money tree"></div></a>
+		</div>
+		
+		<div class="vertical_ad">
+			 
+			 <!--
+			<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+			<ins class="adsbygoogle"
+    			style="display:block; text-align:center;"
+     			data-ad-layout="in-article"
+     			data-ad-format="fluid"
+     			data-ad-client="ca-pub-3169172932709847"
+     			data-ad-slot="6547992014"></ins>
+			<script>
+     			(adsbygoogle = window.adsbygoogle || []).push({});
+			</script> !-->
+			 
+			<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+			<!-- Native_vertical_banner -->
+			<ins class="adsbygoogle"
+     			style="display:block"
+     			data-ad-client="ca-pub-3169172932709847"
+     			data-ad-slot="1100347920"
+     			data-ad-format="auto"
+     			data-full-width-responsive="true"></ins>
+			<script>
+     			(adsbygoogle = window.adsbygoogle || []).push({});
+			</script>
+			 
+			<!--  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+			Native_vertical_banner_fixed 
+			<ins class="adsbygoogle"
+     			style="display:inline-block;width:450px;height:1200px"
+    			data-ad-client="ca-pub-3169172932709847"
+     			data-ad-slot="1100347920"></ins>
+			<script>
+     			(adsbygoogle = window.adsbygoogle || []).push({});
+			</script> !-->
+			 
+	
+		</div>
+	</div>
+	
+	<div class="clear"></div>
+	
+
+	<div class="clear"></div>
+</div>
+	
+<?php include("../includes/footer.html");?>
+
+</body>
+</html> 
